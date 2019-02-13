2de03b4fad41cbd8c94fc6c61d04181f7afeca8b
SICOT Jean Vivien <jv.sicot@gt-logistics.fr>
2019-02-13 17:57:47 +0100

    merch 13/02 noBug

--- WiiStock/src/Entity/Utilisateurs.php
+++ WiiStock/src/Entity/Utilisateurs.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -9,7 +7,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UtilisateursRepository")
  * @UniqueEntity(fields="email", message="Cette adresse email est déjà utilisée.")
@@ -23,185 +20,150 @@ class Utilisateurs implements UserInterface, EquatableInterface
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      */
     private $username;
-
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\NotBlank()
      * @Assert\Email()
      */
     private $email;
-
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $password;
-
     /**
      * @Assert\NotBlank()
      * @Assert\Length(min=8, max=4096)
      * @Assert\Regex(pattern="/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).*$/", message="Doit contenir au moins une majuscule, une minuscule, un symbole, et un nombre.")
      */
     private $plainPassword;
-
     /**
      * @ORM\Column(type="array")
      */
     private $roles;
-
-
-
     private $salt;
-
     /**
      * @ORM\Column(type="datetime", nullable=true)
      */
     private $lastLogin;
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Receptions", mappedBy="utilisateur")
      */
     private $receptions;
-
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Demande", mappedBy="utilisateur")
      */
     private $demandes;
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Alerte", mappedBy="AlerteUtilisateur")
      */
     private $UtilisateurAlertes;
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Collecte", mappedBy="demandeur")
      */
     private $collectes;
-
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Preparation", mappedBy="Utilisateur")
      */
     private $preparations;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Livraison", mappedBy="utilisateur")
+     */
+    private $livraisons;
+    
     public function __construct()
     {
         $this->receptions = new ArrayCollection();
         $this->demandes = new ArrayCollection();
         $this->UtilisateurAlertes = new ArrayCollection();
         $this->collectes = new ArrayCollection();
-        $this->livraisons = new ArrayCollection();
         $this->preparations = new ArrayCollection();
+        $this->livraisons = new ArrayCollection();
     }
-
     public function getId()
     {
         return $this->id;
     }
-
     public function getUsername() : ? string
     {
         return $this->username;
     }
-
     public function setUsername(string $username) : self
     {
         $this->username = $username;
-
         return $this;
     }
-
     public function getEmail() : ? string
     {
         return $this->email;
     }
-
     public function setEmail(string $email) : self
     {
         $this->email = $email;
-
         return $this;
     }
-
     public function getPassword() : ? string
     {
         return $this->password;
     }
-
     public function setPassword(string $password) : self
     {
         $this->password = $password;
-
         return $this;
     }
-
     public function getPlainPassword()
     {
         return $this->plainPassword;
     }
-
     public function setPlainPassword($password)
     {
         $this->plainPassword = $password;
     }
-
     public function getSalt()
     {
         // you *may* need a real salt depending on your encoder
         // see section on salt below
         return null;
     }
-
     public function getRoles()
     {
         return $this->roles;
     }
-
     public function setRoles(array $roles) : self
     {
         $this->roles = $roles;
-
         return $this;
     }
-
     public function eraseCredentials()
     {
     }
-
     public function isEqualTo(UserInterface $user)
     {
         if (!$user instanceof Utilisateurs) {
             return false;
         }
-
         if ($this->password !== $user->getPassword()) {
             return false;
         }
-
         if ($this->email !== $user->getEmail()) {
             return false;
         }
-
         return true;
     }
-
     public function getLastLogin() : ? \DateTimeInterface
     {
         return $this->lastLogin;
     }
-
     public function setLastLogin(? \DateTimeInterface $lastLogin) : self
     {
         $this->lastLogin = $lastLogin;
-
         return $this;
     }
-
     /**
      * @return Collection|Receptions[]
      */
@@ -209,17 +171,14 @@ class Utilisateurs implements UserInterface, EquatableInterface
     {
         return $this->receptions;
     }
-
     public function addReception(Receptions $reception) : self
     {
         if (!$this->receptions->contains($reception)) {
             $this->receptions[] = $reception;
             $reception->setUtilisateur($this);
         }
-
         return $this;
     }
-
     public function removeReception(Receptions $reception) : self
     {
         if ($this->receptions->contains($reception)) {
@@ -229,15 +188,12 @@ class Utilisateurs implements UserInterface, EquatableInterface
                 $reception->setUtilisateur(null);
             }
         }
-
         return $this;
     }
-
     public function __toString()
     {
         return $this->username;
     }
-
     /**
      * @return Collection|Demande[]
      */
@@ -245,17 +201,14 @@ class Utilisateurs implements UserInterface, EquatableInterface
     {
         return $this->demandes;
     }
-
     public function addDemande(Demande $demande) : self
     {
         if (!$this->demandes->contains($demande)) {
             $this->demandes[] = $demande;
             $demande->setUtilisateur($this);
         }
-
         return $this;
     }
-
     public function removeDemande(Demande $demande) : self
     {
         if ($this->demandes->contains($demande)) {
@@ -265,10 +218,8 @@ class Utilisateurs implements UserInterface, EquatableInterface
                 $demande->setUtilisateur(null);
             }
         }
-
         return $this;
     }
-
     /**
      * @return Collection|Alerte[]
      */
@@ -276,17 +227,14 @@ class Utilisateurs implements UserInterface, EquatableInterface
     {
         return $this->UtilisateurAlertes;
     }
-
     public function addUtilisateurAlerte(Alerte $utilisateurAlerte) : self
     {
         if (!$this->UtilisateurAlertes->contains($utilisateurAlerte)) {
             $this->UtilisateurAlertes[] = $utilisateurAlerte;
             $utilisateurAlerte->setAlerteUtilisateur($this);
         }
-
         return $this;
     }
-
     public function removeUtilisateurAlerte(Alerte $utilisateurAlerte) : self
     {
         if ($this->UtilisateurAlertes->contains($utilisateurAlerte)) {
@@ -296,10 +244,8 @@ class Utilisateurs implements UserInterface, EquatableInterface
                 $utilisateurAlerte->setAlerteUtilisateur(null);
             }
         }
-
         return $this;
     }
-
     /**
      * @return Collection|Collecte[]
      */
@@ -307,17 +253,14 @@ class Utilisateurs implements UserInterface, EquatableInterface
     {
         return $this->collectes;
     }
-
     public function addCollecte(Collecte $collecte) : self
     {
         if (!$this->collectes->contains($collecte)) {
             $this->collectes[] = $collecte;
             $collecte->setDemandeur($this);
         }
-
         return $this;
     }
-
     public function removeCollecte(Collecte $collecte) : self
     {
         if ($this->collectes->contains($collecte)) {
@@ -327,10 +270,8 @@ class Utilisateurs implements UserInterface, EquatableInterface
                 $collecte->setDemandeur(null);
             }
         }
-
         return $this;
     }
-
     /**
      * @return Collection|Preparation[]
      */
@@ -338,17 +279,14 @@ class Utilisateurs implements UserInterface, EquatableInterface
     {
         return $this->preparations;
     }
-
     public function addPreparation(Preparation $preparation) : self
     {
         if (!$this->preparations->contains($preparation)) {
             $this->preparations[] = $preparation;
             $preparation->setUtilisateur($this);
         }
-
         return $this;
     }
-
     public function removePreparation(Preparation $preparation) : self
     {
         if ($this->preparations->contains($preparation)) {
@@ -358,9 +296,37 @@ class Utilisateurs implements UserInterface, EquatableInterface
                 $preparation->setUtilisateur(null);
             }
         }
+        return $this;
+    }
+
+    /**
+     * @return Collection|Livraison[]
+     */
+    public function getLivraisons(): Collection
+    {
+        return $this->livraisons;
+    }
+
+    public function addLivraison(Livraison $livraison): self
+    {
+        if (!$this->livraisons->contains($livraison)) {
+            $this->livraisons[] = $livraison;
+            $livraison->setUtilisateur($this);
+        }
 
         return $this;
     }
 
+    public function removeLivraison(Livraison $livraison): self
+    {
+        if ($this->livraisons->contains($livraison)) {
+            $this->livraisons->removeElement($livraison);
+            // set the owning side to null (unless already changed)
+            if ($livraison->getUtilisateur() === $this) {
+                $livraison->setUtilisateur(null);
+            }
+        }
 
+        return $this;
+    }
 }
