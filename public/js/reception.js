89fd50ba0d2cd22910ea30814c63ce4ccb8c6876
SICOT Jean Vivien <jv.sicot@gt-logistics.fr>
2019-02-22 17:02:03 +0100

    kjnkju

--- public/js/reception.js
+++ public/js/reception.js
@@ -6,48 +6,9 @@ function addArticle(){
     console.log("patatete");
 }
 
-InitialiserModal
+let modal = document.getElementById("dataModalCenter");; 
+let submit ;
+let url ;
 
-/**
- * Initialise une fenêtre modale
- * 
- * @param {Document} modal la fenêtre modale selectionnée : document.getElementById("modal").
- * @param {Document} submit le bouton qui va envoyé les données au controller via Ajax.
- * @param {string} path le chemin pris pour envoyé les données.
- * 
- */
-function InitialiserModal(modal, submit, path) {
-    submit.addEventListener("click", function () 
-    {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () 
-        {
-            if (this.readyState == 4 && this.status == 200)
-            {
-                data = JSON.parse(this.responseText);
-                table.ajax.reload(function( json ) 
-                {
-                    $('#myInput').val( json.lastInput );
-                    
-                    if(data.redirect)
-                    {
-                        window.location.href = data.redirect;
-                    }
-                });
-            }
-        };
+InitialiserModal(modal, submit, url )
 
-        let inputs = modal.querySelectorAll(".data"); // On récupère toutes les données qui nous intéresse avec le querySelectorAll
-        let Data = []; // Tableau de données
-
-        inputs.forEach(input => { 
-            Data.push({
-                [input.name]: input.value
-            });
-        });
-
-        Json = JSON.stringify(Data); // On transforme les données en JSON
-        xhttp.open("POST", path, true);
-        xhttp.send(Json);
-    });
-}
--- public/js/script-wiilog.js
+++ public/js/script-wiilog.js
@@ -4,83 +4,46 @@ $(document).ready(function () {
 
 
 
-//Fonction de traitement des donnees post ajax pour un tableau
-function traitementDataArticle(reponse, id_table) {
-    $(id_table).children().remove();
-    let articles = JSON.parse(JSON.parse(reponse));
-    var refTable = document.getElementById(id_table)
-    if (articles !== "patate") {
-        console.log(articles)
-
-        var rowCount = refTable.rows.length;
-        articles.forEach(function (article) {
-            var nouvelleLigne = refTable.insertRow();
-
-            var nouvelleCellule = nouvelleLigne.insertCell(0);
-            var nouveauTexte = document.createTextNode(article.nom);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(1);
-            var nouveauTexte = document.createTextNode(article.statut);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(2);
-            var nouveauTexte = document.createTextNode(article.etat);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(3);
-            var nouveauTexte = document.createTextNode(article.refArticle);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(4);
-            var nouveauTexte = document.createTextNode(article.position);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(5);
-            var nouveauTexte = document.createTextNode(article.direction);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var nouvelleCellule = nouvelleLigne.insertCell(6);
-            var nouveauTexte = document.createTextNode(article.quantite);
-            nouvelleCellule.appendChild(nouveauTexte);
-
-            var urlShow = 'http://localhost/WiiStock/WiiStock/public/index.php/articles/show/' + article.id;
-            var urlEdit = 'http://localhost/WiiStock/WiiStock/public/index.php/articles/edite/' + article.id;
-            var nouvelleCellule = nouvelleLigne.insertCell(7);
-            var a = document.createElement("a");
-            a.className = 'btn btn-xs btn-default command-edit';
-            a.setAttribute('href', urlShow);
-            var i = document.createElement("i");
-            i.className = 'fas fa-eye fa-2x';
-            nouvelleCellule.appendChild(a).appendChild(i);
-            var a = document.createElement("a");
-            a.className = 'btn btn-xs btn-default command-edit';
-            a.setAttribute('href', urlEdit);
-            var i = document.createElement("i");
-            i.className = 'fas fa-pencil-alt fa-2x';
-            nouvelleCellule.appendChild(a).appendChild(i);
-        });
-    }else {
-        var nouvelleLigne = refTable.insertRow();
-        var nouvelleCellule = nouvelleLigne.insertCell(0);
-        var nouveauTexte = document.createTextNode("aucun résultat");
-        nouvelleCellule.appendChild(nouveauTexte);
-    }
-}
-
-//Fonction de recherche par input => str = valeur de l'input + url = url du traitement en php
-function showHint(str, url) {
-    if (str.length > 0) {
-        var xmlhttp = new XMLHttpRequest()
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                traitementDataArticle(this.responseText)
+/**
+ * Initialise une fenêtre modale
+ * 
+ * @param {Document} modal la fenêtre modale selectionnée : document.getElementById("modal").
+ * @param {Document} submit le bouton qui va envoyé les données au controller via Ajax.
+ * @param {string} path le chemin pris pour envoyé les données.
+ * 
+ */
+function InitialiserModal(modal, submit, path) {
+    submit.addEventListener("click", function () 
+    {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () 
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                data = JSON.parse(this.responseText);
+                table.ajax.reload(function( json ) 
+                {
+                    $('#myInput').val( json.lastInput );
+                    
+                    if(data.redirect)
+                    {
+                        window.location.href = data.redirect;
+                    }
+                });
             }
-        }
-        var myJSON = JSON.stringify(str)
-        xmlhttp.open("POST", url, true)
-        xmlhttp.send(myJSON)
-    }
+        };
+
+        let inputs = modal.querySelectorAll(".data"); // On récupère toutes les données qui nous intéresse avec le querySelectorAll
+        let Data = []; // Tableau de données
+
+        inputs.forEach(input => { 
+            Data.push({
+                [input.name]: input.value
+            });
+        });
+
+        Json = JSON.stringify(Data); // On transforme les données en JSON
+        xhttp.open("POST", path, true);
+        xhttp.send(Json);
+    });
 }
-
-
