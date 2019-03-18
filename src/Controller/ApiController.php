20866263d7ec7a868a8458c826c3965a4ea74098
c.gazaniol <JauneCitr0n!>
2019-03-18 16:11:58 +0100

    debug connection

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -91,7 +91,7 @@ class ApiController extends FOSRestController implements ClassResourceInterface
             $response->headers->set('Access-Control-Allow-Methods', 'POST, GET');
 
             $user = $this->utilisateurRepository->findOneBy(['username' => $data['login']]);
-            $this->successData['data'] = $user; //TODO
+            $this->successData['data'] = $data; //TODO
 
             if ($user !== null) {
                 if ($this->passwordEncoder->isPasswordValid($user, $data['password'])) {
