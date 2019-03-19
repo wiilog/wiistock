da2df2112edb2278c10efb1a525ac65654ed4b11
SICOT Jean Vivien <jv.sicot@gt-logistics.fr>
2019-03-19 19:09:30 +0100

    unique RefArticle/fournisseur

--- public/js/emplacement.js
+++ public/js/emplacement.js
@@ -1,7 +1,7 @@
 var pathEmplacement = Routing.generate("emplacement_api", true);
 var tableEmplacement = $('#tableEmplacement_id').DataTable({
     "language": {
-        "url": "/js/i18n/dataTableLanguage.json"
+        "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
     },
     ajax:{
         "url": pathEmplacement,
