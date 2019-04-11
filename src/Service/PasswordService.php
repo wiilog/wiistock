3655cc132637e5c06a5f44f8871b0dc672a5eb11
SICOT Jean Vivien <jv.sicot@gt-logistics.fr>
2019-04-11 17:12:36 +0200

    WIIS-275

--- src/Service/PasswordService.php
+++ src/Service/PasswordService.php
@@ -50,8 +50,8 @@ class PasswordService
         $this->entityManager = $entityManager;
         $this->passwordEncoder = $passwordEncoder;
         $this->utilisateurRepository = $utilisateurRepository;
-        $this->username = 'admin@wiilog.fr'; // TODO
-        $this->password = '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'; // TODO
+        $this->username = 'jeanvivien@wiilog.fr'; // TODO
+        $this->password = 'Kellhus16^^'; // TODO
         $this->transport = (new Swift_SmtpTransport('smtp.sendgrid.net', 465, 'ssl'))
             ->setUsername($this->username)
             ->setPassword($this->password);
