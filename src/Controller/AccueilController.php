e12ffb1d0c5556ca71a6c01e88ff260b9c705533
Matteo <matteo.hevin@wiilog.fr>
2019-04-12 10:59:40 +0200

    WIIS-277 Mise en place de l'envoi d'avertissement de seuils

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,37 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\SeuilAlerteService;
-
-
-
-use App\Repository\DemandeRepository;
-use App\Repository\ReferenceArticleRepository;
-use App\Repository\LigneArticleRepository;
-use App\Repository\StatutRepository;
 use App\Repository\EmplacementRepository;
-use App\Repository\UtilisateurRepository;
-
-
-
 use App\Repository\AlerteRepository;
 
 /**
  * @Route("/accueil")
  */
-
 class AccueilController extends AbstractController
 {
-
-     /**
+    /**
      * @var AlerteRepository
      */
     private $alerteRepository;
 
-     /**
+    /**
      * @var SeuilAlerteServic
      */
     private $seuilAlerteService;
@@ -43,7 +29,7 @@ class AccueilController extends AbstractController
      */
     private $emplacementRepository;
 
-    public function __construct(SeuilAlerteService $seuilAlerteService,AlerteRepository $alerteRepository, EmplacementRepository $emplacementRepository)
+    public function __construct(SeuilAlerteService $seuilAlerteService, AlerteRepository $alerteRepository, EmplacementRepository $emplacementRepository)
     {
         $this->alerteRepository = $alerteRepository;
         $this->emplacementRepository = $emplacementRepository;
@@ -54,12 +40,13 @@ class AccueilController extends AbstractController
      * @Route("/", name="accueil", methods={"GET"})
      */
     public function index(): Response
-    {  
+    {
         $nbAlerte = $this->seuilAlerteService->thresholdReaches();
+        $this->seuilAlerteService->warnUsers();
 
         return $this->render('accueil/index.html.twig', [
             'nbAlerte' => $nbAlerte,
-            'emplacements'=> $this->emplacementRepository->findAll(),
+            'emplacements' => $this->emplacementRepository->findAll(),
         ]);
     }
 }
--- src/Service/SeuilAlerteService.php
+++ src/Service/SeuilAlerteService.php
@@ -3,17 +3,10 @@
  * Created by VisualStudioCode.
  * User: jv.Sicot
  * Date: 03/04/2019
- * Time: 15:09
+ * Time: 15:09.
  */
 
 namespace App\Service;
-
-
-use App\Entity\Article;
-use App\Entity\ReferenceArticle;
-use App\Entity\ValeurChampsLibre;
-
-use App\Service\RefArticleDataService;
 
 use App\Repository\ArticleRepository;
 use App\Repository\ArticleFournisseurRepository;
@@ -24,12 +17,11 @@ use App\Repository\StatutRepository;
 use App\Repository\TypeRepository;
 use App\Repository\ValeurChampsLibreRepository;
 use App\Repository\AlerteRepository;
-
-
+use App\Repository\UtilisateurRepository;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
-
+use Swift_SmtpTransport;
+use Swift_Mailer;
 
 class SeuilAlerteService
 {
@@ -58,6 +50,11 @@ class SeuilAlerteService
      */
     private $statutRepository;
 
+    /*
+     * @var StatutRepository
+     */
+    private $utilisateurRepository;
+
     /**
      * @var ValeurChampsLibreRepository
      */
@@ -82,7 +79,7 @@ class SeuilAlerteService
      * @var RefArticleDataService
      */
     private $refArticleDataService;
-    
+
     /**
      * @var AlerteRepository
      */
@@ -93,10 +90,28 @@ class SeuilAlerteService
      */
     private $user;
 
+    /**
+     * @var Swift_SmtpTransport
+     */
+    private $transport;
+
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var password
+     */
+    private $username;
+
+    /**
+     * @var password
+     */
+    private $password;
+
     private $em;
 
-
-    public function __construct(AlerteRepository $alerteRepository ,RefArticleDataService $refArticleDataService, ArticleRepository $articleRepository, ArticleFournisseurRepository $articleFournisseurRepository, TypeRepository  $typeRepository, StatutRepository $statutRepository, EntityManagerInterface $em, ValeurChampsLibreRepository $valeurChampsLibreRepository, ReferenceArticleRepository $referenceArticleRepository, ChampsLibreRepository $champsLibreRepository, FilterRepository $filterRepository, \Twig_Environment $templating, TokenStorageInterface $tokenStorage)
+    public function __construct(AlerteRepository $alerteRepository, RefArticleDataService $refArticleDataService, ArticleRepository $articleRepository, ArticleFournisseurRepository $articleFournisseurRepository, TypeRepository  $typeRepository, StatutRepository $statutRepository, EntityManagerInterface $em, ValeurChampsLibreRepository $valeurChampsLibreRepository, ReferenceArticleRepository $referenceArticleRepository, ChampsLibreRepository $champsLibreRepository, FilterRepository $filterRepository, \Twig_Environment $templating, TokenStorageInterface $tokenStorage, UtilisateurRepository $utilisateurRepository)
     {
         $this->referenceArticleRepository = $referenceArticleRepository;
         $this->articleRepository = $articleRepository;
@@ -108,13 +123,21 @@ class SeuilAlerteService
         $this->refArticleDataService = $refArticleDataService;
         $this->typeRepository = $typeRepository;
         $this->alerteRepository = $alerteRepository;
+        $this->utilisateurRepository = $utilisateurRepository;
         $this->templating = $templating;
         $this->user = $tokenStorage->getToken()->getUser();
         $this->em = $em;
+        $this->username = 'admin@wiilog.fr'; // TODO
+        $this->password = 'Kellhus16^^'; // TODO
+        $this->transport = (new Swift_SmtpTransport('smtp.sendgrid.net', 465, 'ssl'))
+            ->setUsername($this->username)
+            ->setPassword($this->password);
+        $this->mailer = (new Swift_Mailer($this->transport));
     }
 
     /**
      * @return array
+     *
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
@@ -127,15 +150,35 @@ class SeuilAlerteService
             $quantiteAR = $this->referenceArticleRepository->getQuantiteStockById($seuil->getAlerteRefArticle()->getId());
             if ($seuil->getAlerteSeuil() > $quantiteAR) {
                 $seuil->setSeuilAtteint(true);
-                $nbSeuilAtteint++ ;
+                ++$nbSeuilAtteint;
             } else {
                 $seuil->getSeuilAtteint(false);
             }
-            
         }
         $entityManager = $this->em;
         $entityManager->flush();
-        
+
         return $nbSeuilAtteint;
+    }
+
+    public function warnUsers()
+    {
+        $seuils = $this->alerteRepository->findAll();
+        foreach ($seuils as $seuil) {
+            $quantiteAR = $this->referenceArticleRepository->getQuantiteStockById($seuil->getAlerteRefArticle()->getId());
+            if ($seuil->getAlerteSeuil() > $quantiteAR) {
+                $user = $this->utilisateurRepository->find($seuil->getAlerteUtilisateur()->getId());
+                $this->sendWarning($user->getEmail(), $seuil);
+            }
+        }
+    }
+
+    public function sendWarning($to, $alerte)
+    {
+        $message = (new \Swift_Message('Oubli de mot de passe Wiilog.'))
+            ->setFrom([$this->username => 'L\'équipe de Wiilog.'])
+            ->setTo($to)
+            ->setBody('Vous avez une alerte de seuil concernant l\'article '.$alerte->getAlerteRefArticle()->getLibelle().'!');
+        $this->mailer->send($message);
     }
 }
