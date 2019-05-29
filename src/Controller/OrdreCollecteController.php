f53683cb843280b75a9fbe85ebf5c7f5d0cbc5e3
c.gazaniol <JauneCitron!>
2019-05-29 12:18:50 +0200

    WIIS-597: tentative debug avec return après redirect

--- src/Controller/OrdreCollecteController.php
+++ src/Controller/OrdreCollecteController.php
@@ -351,10 +351,10 @@ class OrdreCollecteController extends AbstractController
             if (!$this->userService->hasRightFunction(Menu::COLLECTE, Action::CREATE_EDIT)) {
                 return $this->redirectToRoute('access_denied');
             }
-            
+
             $ordreCollecte = $this->ordreCollecteRepository->find($data['collecte']);
             $collecte = $ordreCollecte->getDemandeCollecte();
-            
+
             $collecte
                 ->setStatut($this->statutRepository->findOneByCategorieAndStatut(Collecte::CATEGORIE, Collecte::STATUS_BROUILLON));
 
@@ -364,6 +364,7 @@ class OrdreCollecteController extends AbstractController
             $data = [
                 'redirect' => $this->generateUrl('ordre_collecte_index'),
             ];
+
             return new JsonResponse($data);
         }
         throw new NotFoundHttpException('404');
