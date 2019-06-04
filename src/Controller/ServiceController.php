111b181a87fb22bd43bfcadcf856559704ef8e5a
SANZ Silvia <s.sanz@gt-logistics.fr>
2019-06-04 09:25:48 +0200

    hotfix getStatuy en getStatut

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -238,7 +238,7 @@ class ServiceController extends AbstractController
 
             if (
                 !$this->userService->hasRightFunction(Menu::MANUT, Action::LIST)
-                && ($service->getStatuy()->getNom() === Service::STATUT_BROUILLON)
+                && ($service->getStatut()->getNom() === Service::STATUT_BROUILLON)
             ) {
                 return $this->redirectToRoute('access_denied');
             }
