f2543a63ed4f97da99b954e65ec35810f9929f53
clara tuco <clara.tuco@wiilog.fr>
2019-07-31 12:14:38 +0200

    WIIS-787 : creation entité prefixe_nom_demande

---
+++ src/Entity/PrefixeNomDemande.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PrefixeNomDemandeRepository")
+ */
+class PrefixeNomDemande
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typeDemandeAssociee;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prefixe;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTypeDemandeAssociee(): ?string
+    {
+        return $this->typeDemandeAssociee;
+    }
+
+    public function setTypeDemandeAssociee(string $typeDemandeAssociee): self
+    {
+        $this->typeDemandeAssociee = $typeDemandeAssociee;
+
+        return $this;
+    }
+
+    public function getPrefixe(): ?string
+    {
+        return $this->prefixe;
+    }
+
+    public function setPrefixe(?string $prefixe): self
+    {
+        $this->prefixe = $prefixe;
+
+        return $this;
+    }
+}
