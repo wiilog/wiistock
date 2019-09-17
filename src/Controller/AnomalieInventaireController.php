ae2f0456c4cb238c419626c0500a84c2c8697683
h.mangematin <hugo.mangematin@wiilog.fr>
2019-09-17 11:50:02 +0200

    WIIS-945 create 3 new template/controller [INVENTAIRE]

---
+++ src/Controller/AnomalieInventaireController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Action;
+use App\Entity\Menu;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use App\Service\UserService;
+
+/**
+ * @Route("/anomalies_inventaire")
+ */
+class AnomalieInventaireController extends AbstractController
+{
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @Route("/", name="anomalie_inventaire_index")
+     */
+    public function index()
+    {
+        if (!$this->userService->hasRightFunction(Menu::INVENTAIRE, Action::LIST)) {
+            return $this->redirectToRoute('access_denied');
+        }
+
+        return $this->render('anomalie_inventaire/index.html.twig', [
+
+        ]);
+    }
+}
---
+++ src/Controller/SaisieInventaireController.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Action;
+use App\Entity\Menu;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use App\Service\UserService;
+
+
+/**
+ * @Route("/saisie_inventaire")
+ */
+class SaisieInventaireController extends AbstractController
+{
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @Route("/", name="saisie_inventaire_index")
+     */
+    public function index()
+    {
+        if (!$this->userService->hasRightFunction(Menu::INVENTAIRE, Action::LIST)) {
+            return $this->redirectToRoute('access_denied');
+        }
+
+        return $this->render('saisie_inventaire/index.html.twig', [
+
+        ]);
+    }
+}
