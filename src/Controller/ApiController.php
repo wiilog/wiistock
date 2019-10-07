f39ad037af1152c27a4c01316ea02fe024c2d4bf
c.gazaniol <JauneCitr0n!>
2019-10-07 11:38:14 +0200

    WIIS-944: ortho

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -921,7 +921,7 @@ class ApiController extends FOSRestController implements ClassResourceInterface
 				$s = $numberOfRowsInserted > 1 ? 's' : '';
 				$this->successDataMsg['success'] = true;
 				$this->successDataMsg['data']['status'] = ($numberOfRowsInserted === 0) ?
-					"Aucune anomalie d'inventaire à synchroniser." : $numberOfRowsInserted . ' anomalie' . $s . ' d\'inventaire synchronisé' . $s;
+					"Aucune anomalie d'inventaire à synchroniser." : $numberOfRowsInserted . ' anomalie' . $s . ' d\'inventaire synchronisée' . $s;
 			} else {
 				$this->successDataMsg['success'] = false;
 				$this->successDataMsg['msg'] = "Vous n'avez pas pu être authentifié. Veuillez vous reconnecter.";
