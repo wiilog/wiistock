67b386f6645d4d8488c49128aa3931907852cfb9
c.gazaniol <JauneCitr0n!>
2019-10-10 11:11:06 +0200

    WIIS-1006: crée emplacement à définir si n'existe pas

--- src/DataFixtures/PatchEmptyEmplacementsFixtures.php
+++ src/DataFixtures/PatchEmptyEmplacementsFixtures.php
@@ -3,6 +3,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Emplacement;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -37,27 +38,32 @@ class PatchEmptyEmplacementsFixtures extends Fixture implements FixtureGroupInte
     {
         $emplacementDef = $this->emplacementRepository->findOneByLabel('A définir');
 
-        if ($emplacementDef) {
-            $query = $this->em->createQuery(
-            /** @lang DQL */
-                "UPDATE App\Entity\ReferenceArticle ra
-            SET ra.emplacement = :emplacement
-            WHERE ra.emplacement IS NULL"
-            )->setParameter('emplacement', $emplacementDef);
+        if (!$emplacementDef) {
+        	$emplacementDef = new Emplacement();
+        	$emplacementDef->setLabel('A définir')
+				->setDescription('A définir')
+				->setIsDeliveryPoint(false)
+				->setIsActive(true);
+		}
 
-            $query->execute();
+		$query = $this->em->createQuery(
+		/** @lang DQL */
+			"UPDATE App\Entity\ReferenceArticle ra
+		SET ra.emplacement = :emplacement
+		WHERE ra.emplacement IS NULL"
+		)->setParameter('emplacement', $emplacementDef);
 
-            $query = $this->em->createQuery(
-            /** @lang DQL */
-                "UPDATE App\Entity\Article a
-            SET a.emplacement = :emplacement
-            WHERE a.emplacement IS NULL"
-            )->setParameter('emplacement', $emplacementDef);
+		$query->execute();
 
-            $query->execute();
-        } else {
-            dump('Emplacement "A DEFINIR" n\'existe pas');
-        }
+		$query = $this->em->createQuery(
+		/** @lang DQL */
+			"UPDATE App\Entity\Article a
+		SET a.emplacement = :emplacement
+		WHERE a.emplacement IS NULL"
+		)->setParameter('emplacement', $emplacementDef);
+
+		$query->execute();
+
     }
 
     public static function getGroups():array {
