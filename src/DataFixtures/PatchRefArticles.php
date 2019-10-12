73531ee77ae3a659c9135b25c5358087317876b8
h.mangematin <hugo.mangematin@wiilog.fr>
2019-10-12 12:26:31 +0200

    WIIS-1007 ADD reset du counter quand la ref de l'article change et SET ref de la refArticle associé au début.

--- src/DataFixtures/PatchRefArticles.php
+++ src/DataFixtures/PatchRefArticles.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\ReferenceArticleRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -15,25 +16,41 @@ class PatchRefArticles extends Fixture implements FixtureGroupInterface
 	 */
 	private $articleRepository;
 
+    /**
+     * @var ReferenceArticleRepository
+     */
+    private $referenceArticleRepository;
 
-	public function __construct(ArticleRepository $articleRepository)
+
+	public function __construct(ArticleRepository $articleRepository, ReferenceArticleRepository $referenceArticleRepository)
 	{
 		$this->articleRepository = $articleRepository;
+		$this->referenceArticleRepository = $referenceArticleRepository;
 	}
 
 	public function load(ObjectManager $manager)
 	{
 		// patch spécifique pour dédoublonner les références des articles
+        $date = new \DateTime('now', new \DateTimeZone('Europe/Paris'));
+        $formattedDate = $date->format('ym');
 		$doublons = $this->articleRepository->findDoublons();
-		$i = 0;
-		dump(count($doublons) . ' références en doublon.');
+		$counter = 1;
 
+		$saveLastRefToRestart = null;
 		foreach ($doublons as $doublon) {
 			$refArray = explode('-', $doublon->getReference());
-			$newRef = $refArray[0] . '-' . $i;
+			$reference = $refArray[0];
+
+			if ($reference != $saveLastRefToRestart) {
+			    $counter = 1;
+            }
+            $saveLastRefToRestart = $reference;
+
+            $formattedCounter = sprintf('%05u', $counter);
+            $referenceRef = $doublon->getArticleFournisseur()->getReferenceArticle()->getReference();
+			$newRef = $referenceRef . $formattedDate . $formattedCounter;
 			$doublon->setReference($newRef);
-			$i++;
-			dump($i);
+			$counter++;
 		}
 
 		$manager->flush();
