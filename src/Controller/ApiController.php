d689ca601c414a06c0fa7d195c7e15554cdb10f2
m.hevin <matteo.hevin@wiilog.fr>
2019-10-22 17:15:41 +0200

    added date

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -674,7 +674,7 @@ class ApiController extends FOSRestController implements ClassResourceInterface
                     $manut->getStatut()->getNom() == Livraison::STATUT_A_TRAITER
                 ) {
                     // modif de la livraison
-                    $manut->setCommentaire($manut->getCommentaire() . "\n" . $nomadUser->getUsername() . " : " . $data['commentaire']);
+                    $manut->setCommentaire($manut->getCommentaire() . "\n" . $nomadUser->getUsername() . " : " . $data['commentaire'] . " : " . date('dd/MM/yyyy h:mm:ss'));
                     $manut->setStatut($this->statutRepository->findOneByCategorieAndStatut(CategorieStatut::MANUTENTION, Manutention::STATUT_TRAITE));
                     $em->flush();
 
