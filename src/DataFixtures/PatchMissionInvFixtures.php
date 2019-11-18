fd3dafcf52339a1c84dce7ef83a0344f722ba2f9
c.gazaniol <JauneCitr0n!>
2019-11-18 10:40:33 +0100

    WIIS-1297: fixture nouvelle mission inventaire à la main

---
+++ src/DataFixtures/PatchMissionInvFixtures.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\InventoryMission;
+use App\Repository\ChampLibreRepository;
+use App\Repository\ReferenceArticleRepository;
+use App\Repository\ValeurChampLibreRepository;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+class PatchMissionInvFixtures extends Fixture implements FixtureGroupInterface
+{
+
+    /**
+     * @var ReferenceArticleRepository
+     */
+    private $referenceArticleRepository;
+
+
+    public function __construct(ReferenceArticleRepository $referenceArticleRepository)
+    {
+    	$this->referenceArticleRepository = $referenceArticleRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+    	// création mission inventaire
+    	$startDate = new \DateTime('now');
+    	$endDate = new \DateTime('2019-12-31');
+
+    	$mission = new InventoryMission();
+    	$mission
+			->setStartPrevDate($startDate)
+			->setEndPrevDate($endDate);
+    	$manager->persist($mission);
+    	$manager->flush();
+
+
+    	// ajout références à la mission
+		$path ="src/DataFixtures/Csv/inventaire.csv";
+		$file = fopen($path, "r");
+
+		$rows = [];
+		while (($data = fgetcsv($file, 1000, ";")) !== false) {
+			$rows[] = $data;
+		}
+
+		array_shift($rows); // supprime la 1è ligne d'en-têtes
+
+		foreach ($rows as $row) {
+			$ref = $this->referenceArticleRepository->findOneByReference($row[0]);
+
+			if ($ref) {
+				$mission->addRefArticle($ref);
+//				dump('réf ' . $row[0] . ' ajoutée');
+			} else {
+				dump('la réf ' . $row[0] . ' n\'existe pas');
+			}
+		}
+
+		$manager->flush();
+		fclose($file);
+	}
+
+    public static function getGroups():array {
+        return ['inventory'];
+    }
+}
