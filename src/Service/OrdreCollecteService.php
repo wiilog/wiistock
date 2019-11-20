c1c19e8a170872d5b6d327e87eab69e804d30fd0
Adrien Bounader <adrien.bounade@wiilog.fr>
2019-11-20 12:25:15 +0100

    WIIS-1212 : ajout des mouvements de stocks dans les collectes

--- src/Service/OrdreCollecteService.php
+++ src/Service/OrdreCollecteService.php
@@ -4,14 +4,20 @@ namespace App\Service;
 
 use App\Entity\Article;
 use App\Entity\Collecte;
+use App\Entity\MouvementStock;
 use App\Entity\OrdreCollecte;
 use App\Entity\Utilisateur;
 use App\Repository\CollecteReferenceRepository;
 use App\Repository\MailerServerRepository;
 use App\Repository\StatutRepository;
 
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
+use Doctrine\ORM\NonUniqueResultException;
+use Twig_Environment;
+use Twig_Error_Loader;
+use Twig_Error_Runtime;
+use Twig_Error_Syntax;
 
 class OrdreCollecteService
 {
@@ -40,7 +46,12 @@ class OrdreCollecteService
 	 */
 	private $collecteReferenceRepository;
 
-    public function __construct(MailerServerRepository $mailerServerRepository, CollecteReferenceRepository $collecteReferenceRepository, MailerService $mailerService, StatutRepository $statutRepository, EntityManagerInterface $em, \Twig_Environment $templating)
+    public function __construct(MailerServerRepository $mailerServerRepository,
+                                CollecteReferenceRepository $collecteReferenceRepository,
+                                MailerService $mailerService,
+                                StatutRepository $statutRepository,
+                                EntityManagerInterface $em,
+                                Twig_Environment $templating)
 	{
 	    $this->mailerServerRepository = $mailerServerRepository;
 		$this->templating = $templating;
@@ -50,13 +61,16 @@ class OrdreCollecteService
 		$this->collecteReferenceRepository = $collecteReferenceRepository;
 	}
 
-	/**
-	 * @param OrdreCollecte $collecte
-	 * @param Utilisateur $user
-	 * @param string $date
-	 * @throws Exception
-	 */
-	public function finishCollecte($collecte, $user, $date)
+    /**
+     * @param OrdreCollecte $collecte
+     * @param Utilisateur $user
+     * @param DateTime $date
+     * @throws NonUniqueResultException
+     * @throws Twig_Error_Loader
+     * @throws Twig_Error_Runtime
+     * @throws Twig_Error_Syntax
+     */
+	public function finishCollecte($collecte, $user, DateTime $date)
 	{
 		// on modifie le statut de l'ordre de collecte
 		$collecte
@@ -93,12 +107,30 @@ class OrdreCollecteService
 			foreach ($collecteReferences as $collecteReference) {
 				$refArticle = $collecteReference->getReferenceArticle();
 				$refArticle->setQuantiteStock($refArticle->getQuantiteStock() + $collecteReference->getQuantite());
+
+                $newMouvement = new MouvementStock();
+                $newMouvement
+                    ->setUser($user)
+                    ->setRefArticle($refArticle)
+                    ->setDate($date)
+                    ->setType(MouvementStock::TYPE_ENTREE)
+                    ->setQuantity($collecteReference->getQuantite());
+                $this->em->persist($newMouvement);
 			}
 
 			// on modifie le statut des articles liés à la collecte
 			$articles = $demandeCollecte->getArticles();
 			foreach ($articles as $article) {
 				$article->setStatut($this->statutRepository->findOneByCategorieNameAndStatutName(Article::CATEGORIE, Article::STATUT_ACTIF));
+
+                $newMouvement = new MouvementStock();
+                $newMouvement
+                    ->setUser($user)
+                    ->setRefArticle($article)
+                    ->setDate($date)
+                    ->setType(MouvementStock::TYPE_ENTREE)
+                    ->setQuantity($article->getQuantite());
+                $this->em->persist($newMouvement);
 			}
 		}
 		$this->em->flush();
