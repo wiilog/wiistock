57ff39993e83cee03693117ba7bd48fd73f144c7
m.hevin <matteo.hevin@wiilog.fr>
2019-11-28 11:18:39 +0100

    WIIS-1308 finished chart aswell

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,4 @@
+let resizeTimer;
 $('.range-buttons').hide();
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(function () {
@@ -5,63 +6,7 @@ google.charts.setOnLoadCallback(function () {
     drawAnnotations('dashboard-arrival');
 });
 
-function drawAnnotations(parent) {
-    let data = new google.visualization.DataTable();
-    let currentWeekRoute = Routing.generate(parent, true);
-    let params = {
-        'firstDay': $('#' + parent + ' > .range-buttons > .firstDay').data('day'),
-        'lastDay': $('#' + parent + ' > .range-buttons > .lastDay').data('day'),
-    };
-    $.post(currentWeekRoute, JSON.stringify(params), function (chartData) {
-        chartData.columns.forEach(column => {
-            if (column.annotation) {
-                data.addColumn({type: column.type, role: column.role});
-            } else {
-                data.addColumn(column.type, column.value);
-            }
-        });
-        for (const [key, value] of Object.entries(chartData.rows)) {
-            if (value.conform !== undefined) data.addRow(
-                [
-                    key,
-                    Number(value.count),
-                    value.conform,
-                    key + ' : ' + String(value.conform) + '%']);
-            else data.addRow([key, Number(value.count)]);
-        }
-        let options = {
-            vAxes: {
-                0: {
-                    minValue: 1,
-                    format: '#',
-                },
-                1: {
-                    maxValue: 100,
-                    minValue: 0,
-                    format: '#',
-                    gridlines: {color: 'transparent'},
-                },
-            },
-            seriesType: 'bars',
-            pointSize: 5,
-            series: {
-                1: {
-                    pointShape: 'circle',
-                    type: 'line',
-                    targetAxisIndex: 1
-                }
-            },
-            legend: {position: 'top'}
-        };
-        console.log(data);
-        let chart = new google.visualization.ColumnChart($('#' + parent + ' > .chart')[0]);
-        chart.draw(data, options);
-        $('#' + parent + ' > .range-buttons').show();
-        $('#' + parent + ' > .spinner-border').hide();
-    }, 'json');
-}
-
-let changeCurrentWeek = function (after, parent) {
+function drawAnnotations(parent, after = true, fromStart = true) {
     $('#' + parent + ' > .range-buttons').hide();
     $('#' + parent + ' > .spinner-border').show();
     let data = new google.visualization.DataTable();
@@ -69,7 +14,7 @@ let changeCurrentWeek = function (after, parent) {
     let params = {
         'firstDay': $('#' + parent + ' > .range-buttons > .firstDay').data('day'),
         'lastDay': $('#' + parent + ' > .range-buttons > .lastDay').data('day'),
-        'after': after
+        'after': (fromStart ? 'now' : after)
     };
     $.post(currentWeekRoute, JSON.stringify(params), function (chartData) {
         chartData.columns.forEach(column => {
@@ -83,34 +28,56 @@ let changeCurrentWeek = function (after, parent) {
             if (value.conform !== undefined) data.addRow(
                 [
                     key,
-                    Number(value.count),
+                    Number(value.count) !== 0 ? Number(value.count) : null,
                     value.conform,
                     key + ' : ' + String(value.conform) + '%']);
-            else data.addRow([key, Number(value.count)]);
+            else data.addRow([key, Number(value.count) !== 0 ? Number(value.count) : null]);
         }
         let options = {
             vAxes: {
                 0: {
                     minValue: 1,
                     format: '#',
+                    textStyle: {
+                        color: 'black',
+                        fontName: 'Montserrat',
+                    }
                 },
                 1: {
                     maxValue: 100,
                     minValue: 0,
                     format: '#',
                     gridlines: {color: 'transparent'},
+                    textStyle: {
+                        color: 'black',
+                        fontName: 'Montserrat',
+                    }
                 },
             },
+            hAxis: {
+                textStyle: {
+                    color: 'black',
+                    fontName: 'Montserrat',
+                }
+            },
+            interpolateNulls: true,
             seriesType: 'bars',
             pointSize: 5,
             series: {
                 1: {
                     pointShape: 'circle',
                     type: 'line',
-                    targetAxisIndex: 1
+                    targetAxisIndex: 1,
                 }
             },
-            legend: {position: 'top'}
+            legend: {
+                position: 'top',
+                textStyle: {
+                    color: 'black',
+                    fontName: 'Montserrat',
+                }
+            },
+            colors: ['#130078', 'Red'],
         };
         let chart = new google.visualization.ColumnChart($('#' + parent + ' > .chart')[0]);
         chart.draw(data, options);
@@ -121,11 +88,12 @@ let changeCurrentWeek = function (after, parent) {
         $('#' + parent + ' > .range-buttons').show();
         $('#' + parent + ' > .spinner-border').hide();
     }, 'json');
-};
+}
 
 let routeForLate = Routing.generate('api_retard', true);
 
 $('.retards-table').DataTable({
+    responsive: true,
     dom: 'ftipr',
     pageLength: 5,
     processing: true,
@@ -142,4 +110,12 @@ $('.retards-table').DataTable({
         {"data": 'time', 'name': 'delai', 'title': 'Délai'},
         {"data": 'emp', 'name': 'emp', 'title': 'Emplacement'},
     ]
+});
+
+$(window).resize(function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+        drawAnnotations('dashboard-assoc');
+        drawAnnotations('dashboard-arrival');
+    }, 250);
 });
