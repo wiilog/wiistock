d3553114475653d586d59b64a9d6124b0c220e7a
o.flifli <oussama.flifli@wiilog.fr>
2019-12-02 16:10:21 +0100

    WIIS-1353 : Oussama - fixtures

---
+++ src/DataFixtures/PatchAlerteFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Repository\ReferenceArticleRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\ReferenceArticle;
+
+class PatchAlerteFixtures extends Fixture implements FixtureGroupInterface
+{
+
+    /**
+     * @var ReferenceArticleRepository
+     */
+    private $referenceArticleRepository;
+
+    public function __construct(ReferenceArticleRepository $referenceArticleRepository)
+    {
+        $this->referenceArticleRepository = $referenceArticleRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $refArts = $this->referenceArticleRepository->findAll();
+        foreach ($refArts as $refArt)
+        {
+            $limit_warning = $refArt->getLimitWarning();
+            $refArt
+                ->setLimitWarning($refArt->getLimitSecurity())
+                ->setLimitSecurity($limit_warning);
+
+        }
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['alerte', 'fixtures'];
+    }
+}
