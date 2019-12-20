ca56630793a65f3f3642cb09d01c96bb06d76bb3
Adrien Bounader <adrien.bounade@wiilog.fr>
2019-12-20 14:43:15 +0100

    hotfix : correction easyForm pour le conditionnementArticleTemplate.html.twig

--- src/Controller/ReceptionController.php
+++ src/Controller/ReceptionController.php
@@ -957,7 +957,7 @@ class ReceptionController extends AbstractController
             $champsLibres = $this->champLibreRepository->findByTypeAndCategorieCLLabel($typeArticle, CategorieCL::ARTICLE);
             $response = new Response();
             $response->setContent($this->renderView(
-                'reception/condtionnementArticleTemplate.html.twig',
+                'reception/conditionnementArticleTemplate.html.twig',
                 [
                     'reception' => [
                         'reference' => $reference,
