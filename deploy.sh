42dc6faf764ec0ea2cb35c7aaaa4da0c52398821
root <root@vps650537.ovh.net>
2019-12-30 12:05:09 +0100

    WIIS-1289:maintenance

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#echo 'numéro de version ?'
+
+#echo '-> numéro de version ?'
 #read version
 #
 ## mise à jour numéro de version sur template
@@ -14,28 +15,42 @@
 #sed -i "4s/.*/$forthLineYaml/" config/services.yaml
 ##sed -i "5s/.*/$fifthLineYaml/" config/services.yaml
 #echo 'OK : numéros de version mis à jour sur le services.yaml'
-#
+
+## commit et push modifs version
+#git add config/services.yaml
+#git add templates/layout.html.twig
+#git commit -m "version $version"
+#git push
+#echo 'OK : commit et push modif version'
+
 ## mise à jour jira
-#read -p "mise à jour des tâches sur jira"
+#read -p "-> mise à jour des tâches sur jira"
 #echo "OK : mise à jour des tâches sur jira"
 
 # choix de l'instance
-echo 'sur quelle instance déployer ? (cl2prod, cl1rec, scs1prod, scs1rec, col1prod, col1rec, test, dev)'
+echo '-> déployer sur quelle instance ?'
 read instance
 
 case "$instance" in
   dev | test ) ip=51.77.202.108;;
-  cl2prod | cl1rec | scs1prod | scs1rec ) ip=145.239.76.51;;
-  col1prod | col1rec ) ip=51.38.34.237;;
-  * ) echo 'instances disponibles : cl2prod, cl1rec, scs1prod, scs1rec, col1prod, col1rec, test, dev';;
+  cl2-prod | cl1-rec | scs1-prod | scs1-rec ) ip=145.239.76.51;;
+  col1-prod | col1-rec ) ip=51.38.34.237;;
+  * ) echo 'instances disponibles : cl2-prod, cl1-rec, scs1-prod, scs1-rec, col1-prod, col1-rec, test, dev';exit;;
 esac
 
 # mise en maintenance
-echo "$ip"
-password=AkMHvXP7
-sshpass -p "$password" ssh -o StrictHostKeyChecking=no root@$ip
-ls
-#echo "$instance"
-#sixthLineEnv="APP_ENV=maintenance"
-#sed -i "6s/.*/$sixthLineEnv/" .env
-#echo exit
+sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
+  cd /var/www/"$instance"/WiiStock
+  sed -i "6s/.*/APP_ENV=maintenance/" .env
+EOF
+echo "OK : mise en maintenance de l'instance $instance"
+
+# sauvegarde base données
+# cacheclear
+# git pull
+# cacheclear
+# migrations
+# mise à jour base données
+# fixtures
+# fin de maintenance
+# cacheclear
