25088ddd41c81eb334d90267055f1d520ef48bf3
c.gazaniol <JauneCitr0n!>
2019-12-30 17:32:38 +0100

    WIIS-1289: optimisations modif numéros version services.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-#echo '-> numéro de version ?'
-#read version
-#
-## mise à jour numéro de version sur template
-#firstLineTwig="{% set version = '"$version"' %}"
-#sed -i "1s/.*/$firstLineTwig/" templates/layout.html.twig
-#echo 'OK : numéro de version mis à jour sur le template'
-#
-## mise à jour numéro de version sur services.yaml
-#forthLineYaml="    nomade_versions: '>="$version"'"
-#nomadeVersion=${version//\./-}
-#fifthLineYaml="    nomade_apk: 'http://wiilog.fr/dl/wiistock/app-"$nomadeVersion".apk'"
-#sed -i "4s/.*/$forthLineYaml/" config/services.yaml
-##sed -i "5s/.*/$fifthLineYaml/" config/services.yaml
-#echo 'OK : numéros de version mis à jour sur le services.yaml'
+echo '-> numéro de version web?'
+read versionWeb
+echo '-> numéro de version nomade (si différent)'
+read versionNomade
 
-## commit et push modifs version
-#git add config/services.yaml
-#git add templates/layout.html.twig
-#git commit -m "version $version"
-#git push
-#echo 'OK : commit et push modif version'
+if [ "$versionNomade" = "" ]; then
+  versionNomade=$versionWeb
+fi
 
-## mise à jour jira
-#read -p "-> mise à jour des tâches sur jira"
-#echo "OK : mise à jour des tâches sur jira"
+# mise à jour numéro de version sur template
+firstLineTwig="{% set version = '"$versionWeb"' %}"
+sed -i "1s/.*/$firstLineTwig/" templates/layout.html.twig
+echo '////////// OK : numéro de version web mis à jour sur le template //////////'
+
+# mise à jour numéro de version sur services.yaml
+formerFourthLineYaml="nomade_versions:"
+newFourthLineYaml="nomade_versions: '>="$versionNomade"'"
+sed -i "s/$formerFourthLineYaml.*/$newFourthLineYaml/" config/services.yaml
+
+versionNomadeFormatted=${versionNomade//\./-}
+formerFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock"
+newFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock\/app-$versionNomadeFormatted.apk'"
+sed -i "s/$formerFifthLineYaml.*/$newFifthLineYaml/" config/services.yaml
+
+echo '////////// OK : numéros de version nomade mis à jour sur le services.yaml //////////'
+
+# commit et push modifs version
+git add config/services.yaml
+git add templates/layout.html.twig
+git commit -m "version $version"
+git push
+echo '////////// OK : commit et push modif version //////////'
+
+# mise à jour jira
+read -p "-> mise à jour des tâches sur jira"
+echo "////////// OK : mise à jour des tâches sur jira //////////"
 
 # choix de l'instance
 printf '-> déployer sur quelle instance ?'
@@ -50,7 +60,7 @@ sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
   cd /var/www/"$instance"/WiiStock
   sed -i "6s/.*/APP_ENV=maintenance/" .env
 EOF
-printf "//////////\nOK : mise en maintenance de l'instance $instance\n//////////"
+printf "////////// OK : mise en maintenance de l'instance $instance //////////\n"
 
 # sauvegarde base données
 case "$instance" in
@@ -66,10 +76,10 @@ if [ "$dbuser" != 'noBackup' ]; then
   read -p "-> lancer la sauvegarde de la base de données ?"
   date=$(date '+%Y-%m-%d')
 #  mysqldump --host=cb249510-001.dbaas.ovh.net --user="$dbuser" --port=35403 --password="$password" "$db" > /root/db_backups/svg_"$db"_"$date".sql
-  printf "//////////\nOK : base de données $db sauvegardée\n//////////"
+  printf "////////// OK : base de données $db sauvegardée //////////\n"
 
 else
-  printf "//////////\npas de sauvegarde de base de données nécessaire\n//////////"
+  printf "////////// pas de sauvegarde de base de données nécessaire //////////\n"
 fi
 
 # git pull
@@ -80,15 +90,15 @@ read -p "-> lancer git pull + migrations + fixtures + fin de maintenance ?"
 sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
   cd /var/www/"$instance"/WiiStock
 #  git pull
-  printf "//////////\nOK : git pull effectué\n//////////"
+  printf "////////// OK : git pull effectué //////////"
   php bin/console doctrine:migrations:migrate
   php bin/console doctrine:schema:update --force
-  printf "//////////\nOK : migrations de la base effectuées\n//////////"
+  printf "////////// OK : migrations de la base effectuées //////////"
   php bin/console doctrine:fixtures:load --append --group=fixtures
-  printf "//////////\nOK : fixtures effectuées\n//////////"
+  printf "////////// OK : fixtures effectuées //////////"
   sed -i "6s/.*/APP_ENV=prod/" .env
-  printf "//////////\nOK : mise en prod de l'instance $instance\n//////////"
+  printf "////////// OK : mise en prod de l'instance $instance //////////"
   php bin/console cache:clear
   chmod 777 -R /var/www/"$instance"/WiiStock/var/cache/
-  printf "//////////\nOK : cache nettoyé\n//////////"
+  printf "////////// OK : cache nettoyé //////////"
 EOF
