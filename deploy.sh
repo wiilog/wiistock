8e4a1e11e96f2df73c5e5a33e1c5150a4b6b3259
c.gazaniol <JauneCitr0n!>
2019-12-31 10:10:05 +0100

    WIIS-1289: fixture personnalisée - tests

--- deploy.sh
+++ deploy.sh
@@ -1,91 +1,94 @@
 #!/bin/bash
 
-echo '-> numéro de version web ?'
-read versionWeb
-echo '-> numéro de version nomade (si différent)'
-read versionNomade
-
-if [ "$versionNomade" = "" ]; then
-  versionNomade=$versionWeb
-fi
-
-# mise à jour numéro de version sur template
-firstLineTwig="{% set version = '"$versionWeb"' %}"
-sed -i "1s/.*/$firstLineTwig/" templates/layout.html.twig
-echo '////////// OK : numéro de version web mis à jour sur le template //////////'
-
-# mise à jour numéro de version sur services.yaml
-formerFourthLineYaml="nomade_versions:"
-newFourthLineYaml="nomade_versions: '>="$versionNomade"'"
-sed -i "s/$formerFourthLineYaml.*/$newFourthLineYaml/" config/services.yaml
-
-versionNomadeFormatted=${versionNomade//\./-}
-formerFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock"
-newFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock\/app-$versionNomadeFormatted.apk'"
-sed -i "s/$formerFifthLineYaml.*/$newFifthLineYaml/" config/services.yaml
-
-echo '////////// OK : numéros de version nomade mis à jour sur le services.yaml //////////'
-
-# commit et push modifs version
-git add config/services.yaml
-git add templates/layout.html.twig
-git commit -m "version $version"
-git push
-echo '////////// OK : commit et push modif version //////////'
-
-# mise à jour jira
-read -p "-> pense à mettre à jour le numéro de version des tâches sur jira !"
-
-# choix de l'instance
-echo '-> déployer sur quelle instance ?'
-while true; do
-  read instance
-  case "$instance" in
-    dev | test )
-      ip=51.77.202.108
-      break;;
-    cl2-prod | cl1-rec | scs1-prod | scs1-rec )
-      ip=145.239.76.51
-      break;;
-    col1-prod | col1-rec )
-      ip=51.38.34.237
-      break;;
-    * ) echo 'instances disponibles : cl2-prod, cl1-rec, scs1-prod, scs1-rec, col1-prod, col1-rec, test, dev';;
-  esac
-done
-
-# mise en maintenance
-sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
-  cd /var/www/"$instance"/WiiStock
-  sed -i "6s/.*/APP_ENV=maintenance/" .env
-EOF
-printf "////////// OK : mise en maintenance de l'instance $instance //////////\n"
-
-# sauvegarde base données
-case "$instance" in
-  cl2-prod | scs1-prod | col1-rec)
-    db=$(awk '{ print $1 }' ./db-"$instance")
-    dbuser=$(awk ' {print $2} ' ./db-"$instance")
-    password=$(awk ' {print $3} ' ./db-"$instance");;
-  * ) dbuser='noBackup';;
-esac
-
-if [ "$dbuser" != 'noBackup' ]; then
-  read -p "-> lancer la sauvegarde de la base de données ?"
-  date=$(date '+%Y-%m-%d')
-  mysqldump --host=cb249510-001.dbaas.ovh.net --user="$dbuser" --port=35403 --password="$password" "$db" > /root/db_backups/svg_"$db"_"$date".sql
-  printf "////////// OK : base de données $db sauvegardée //////////\n"
-
-else
-  echo "////////// pas de sauvegarde de base de données nécessaire //////////"
-fi
+#echo '-> numéro de version web ?'
+#read versionWeb
+#echo '-> numéro de version nomade (si différent)'
+#read versionNomade
+#
+#if [ "$versionNomade" = "" ]; then
+#  versionNomade=$versionWeb
+#fi
+#
+## mise à jour numéro de version sur template
+#firstLineTwig="{% set version = '"$versionWeb"' %}"
+#sed -i "1s/.*/$firstLineTwig/" templates/layout.html.twig
+#echo '////////// OK : numéro de version web mis à jour sur le template //////////'
+#
+## mise à jour numéro de version sur services.yaml
+#formerFourthLineYaml="nomade_versions:"
+#newFourthLineYaml="nomade_versions: '>="$versionNomade"'"
+#sed -i "s/$formerFourthLineYaml.*/$newFourthLineYaml/" config/services.yaml
+#
+#versionNomadeFormatted=${versionNomade//\./-}
+#formerFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock"
+#newFifthLineYaml="nomade_apk: 'http:\/\/wiilog.fr\/dl\/wiistock\/app-$versionNomadeFormatted.apk'"
+#sed -i "s/$formerFifthLineYaml.*/$newFifthLineYaml/" config/services.yaml
+#
+#echo '////////// OK : numéros de version nomade mis à jour sur le services.yaml //////////'
+#
+## commit et push modifs version
+#git add config/services.yaml
+#git add templates/layout.html.twig
+#git commit -m "version $version"
+#git push
+#echo '////////// OK : commit et push modif version //////////'
+#
+## mise à jour jira
+#read -p "-> pense à mettre à jour le numéro de version des tâches sur jira !"
+#
+## choix de l'instance
+#echo '-> déployer sur quelle instance ?'
+#while true; do
+#  read instance
+#  case "$instance" in
+#    dev | test )
+#      ip=51.77.202.108
+#      break;;
+#    cl2-prod | cl1-rec | scs1-prod | scs1-rec )
+#      ip=145.239.76.51
+#      break;;
+#    col1-prod | col1-rec )
+#      ip=51.38.34.237
+#      break;;
+#    * ) echo 'instances disponibles : cl2-prod, cl1-rec, scs1-prod, scs1-rec, col1-prod, col1-rec, test, dev';;
+#  esac
+#done
+#
+## mise en maintenance
+#sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
+#  cd /var/www/"$instance"/WiiStock
+#  sed -i "6s/.*/APP_ENV=maintenance/" .env
+#EOF
+#printf "////////// OK : mise en maintenance de l'instance $instance //////////\n"
+#
+## sauvegarde base données
+#case "$instance" in
+#  cl2-prod | scs1-prod | col1-rec)
+#    db=$(awk '{ print $1 }' ./db-"$instance")
+#    dbuser=$(awk ' {print $2} ' ./db-"$instance")
+#    password=$(awk ' {print $3} ' ./db-"$instance");;
+#  * ) dbuser='noBackup';;
+#esac
+#
+#if [ "$dbuser" != 'noBackup' ]; then
+#  read -p "-> lancer la sauvegarde de la base de données ?"
+#  date=$(date '+%Y-%m-%d')
+#  mysqldump --host=cb249510-001.dbaas.ovh.net --user="$dbuser" --port=35403 --password="$password" "$db" > /root/db_backups/svg_"$db"_"$date".sql
+#  printf "////////// OK : base de données $db sauvegardée //////////\n"
+#
+#else
+#  echo "////////// pas de sauvegarde de base de données nécessaire //////////"
+#fi
 
 # git pull
 # migrations et mise à jour bdd
 # fixtures
 # fin de maintenance
 read -p "-> lancer git pull + migrations + fixtures + fin de maintenance ?"
-sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
+echo "-> lancer des fixtures supplémentaires ? (nomFixture1,nomFixture2 / n)"
+read fixtures
+
+sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@"$ip" <<EOF
   cd /var/www/"$instance"/WiiStock
   git pull
   printf "////////// OK : git pull effectué //////////"
@@ -94,6 +97,13 @@ sshpass -f pass-"$ip" ssh -o StrictHostKeyChecking=no root@$ip <<EOF
   printf "////////// OK : migrations de la base effectuées //////////"
   php bin/console doctrine:fixtures:load --append --group=fixtures
   printf "////////// OK : fixtures effectuées //////////"
+
+  if [ "$fixtures" != 'n' ]; then
+    for i in "${fixtures[@]}"; do
+      echo "$i"
+    done
+  fi
+
   sed -i "6s/.*/APP_ENV=prod/" .env
   printf "////////// OK : mise en prod de l'instance $instance //////////"
   php bin/console cache:clear
