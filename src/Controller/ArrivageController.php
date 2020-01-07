fef246a846c2b5218074580409e69b3df928eaf7
c.gazaniol <JauneCitr0n!>
2020-01-07 09:53:44 +0100

    WIIS-1365: traduction sur arrivage (arrivage+colis)

--- src/Controller/ArrivageController.php
+++ src/Controller/ArrivageController.php
@@ -43,7 +43,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 
 /**
