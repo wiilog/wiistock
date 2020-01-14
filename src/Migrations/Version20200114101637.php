23ec68ee395d0d552df3de16ab303fd53d88fc41
m.hevin <matteo.hevin@wiilog.fr>
2020-01-14 14:26:35 +0100

    WIIS-1606 implemented the migration which will take care of the article fournisseur duplicates

---
+++ src/Migrations/Version20200114101637.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200114101637 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Is used to delete the duplicates from the articleFournisseur table. It takes care of the foreign keys in the article table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        // Update article first
+        $this->addSql("
+            UPDATE article a
+            INNER JOIN article_fournisseur af ON af.id = a.article_fournisseur_id
+            SET a.article_fournisseur_id =
+            (
+                     SELECT MIN(af_second.id)
+                     FROM article_fournisseur AS af_second
+                     WHERE
+                af_second.label LIKE af.label AND
+                af_second.reference LIKE af.reference AND
+                af_second.fournisseur_id LIKE af.fournisseur_id AND
+                af_second.reference_article_id LIKE af.reference_article_id
+            )
+        ");
+
+        // Then delete unused duplicates
+        $this->addSql("
+            DELETE a FROM article_fournisseur AS a
+            LEFT OUTER JOIN
+            (
+                 SELECT
+                 MIN(af.id) as IdToKeep
+                 FROM
+                 (SELECT * FROM article_fournisseur) AS af
+                 GROUP BY
+                 af.reference_article_id,
+                 af.fournisseur_id,
+                 af.reference,
+                 af.label
+            ) AS tableToKeep ON tableToKeep.IdToKeep = a.id
+            WHERE tableToKeep.IdToKeep IS NULL
+        ");
+    }
+
+    public function down(Schema $schema) : void
+    {
+
+    }
+}
