29091f7d77e8c5f249d08354c0815fd651a16443
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-02-06 11:18:56 +0100

    WIIS-1641 : suppression du fichier custom font du git

---
+++ src/Command/UpdateCustomFontCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Command;
+
+
+use App\Service\GlobalParamService;
+use Doctrine\ORM\NonUniqueResultException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateCustomFontCommand extends Command
+{
+    private $globalParamService;
+
+    public function __construct(GlobalParamService $globalParamService) {
+        parent::__construct();
+        $this->globalParamService = $globalParamService;
+    }
+
+    protected function configure()
+    {
+		$this->setName('app:update:custom-font');
+		$this->setDescription('This commands generate the sass for custom font.');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|void
+     * @throws NonUniqueResultException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->globalParamService->generateSassFile();
+        dump('assets/sass/_customFont.sass generated !');
+    }
+}
