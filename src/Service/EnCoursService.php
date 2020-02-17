9f5f2b513e8c4d32603420487bae7ceabb7b28fd
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-02-17 16:54:58 +0100

    WIIS-1873 : Refacto calcul du delai d'un colis sur un emplacement

--- src/Service/EnCoursService.php
+++ src/Service/EnCoursService.php
@@ -13,10 +13,10 @@ use App\Repository\MouvementTracaRepository;
 use DateInterval;
 use DatePeriod;
 use DateTime;
-use DateTimeInterface;
 use DateTimeZone;
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NonUniqueResultException;
-use Doctrine\ORM\NoResultException;
 use Exception;
 
 
@@ -41,6 +41,7 @@ class EnCoursService
      * @var ColisRepository
      */
     private $colisRepository;
+    private $entityManager;
 
     private const AFTERNOON_FIRST_HOUR_INDEX = 4;
     private const AFTERNOON_LAST_HOUR_INDEX = 6;
@@ -58,7 +59,7 @@ class EnCoursService
      * @param EmplacementRepository $emplacementRepository
      * @param DaysWorkedRepository $daysRepository
      */
-    public function __construct(
+    public function __construct(EntityManagerInterface $entityManager,
         ColisRepository $colisRepository,
         MouvementTracaRepository $mouvementTracaRepository,
         EmplacementRepository $emplacementRepository,
@@ -68,6 +69,7 @@ class EnCoursService
         $this->mouvementTracaRepository = $mouvementTracaRepository;
         $this->emplacementRepository = $emplacementRepository;
         $this->daysRepository = $daysRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -141,158 +143,34 @@ class EnCoursService
     }
 
     /**
-     * @param DateTime $day the day tested
-     * @param DateTime $now now time
-     * @param DateTimeInterface $dateMvt mvt's time
-     * @return int the worked hours for the day tested
-     * @throws NonUniqueResultException
-     */
-    public function getMinutesWorkedDuringThisDay(DateTime $day, DateTime $now, DateTimeInterface $dateMvt): int
-    {
-        /**
-         * @var $dayWorked DaysWorked|null the DaysWorked entity corresponding to the day being tested
-         * @var $isToday bool if the tested day is today
-         * @var $isMvtDay bool if the tested day is the mvt's day
-         * @var $minutesWorked int the minutes worked the tested day
-         * @var $diffMinutesBetweenMvtAndNow int the difference in minutes between the mvt and now
-         * @var $dayTestedTimeInMinutes int the day tested time in minutes
-         * @var $nowTimeInMinutes int now time in minutes
-         * @var $dateMvtTimeInMinutes int mvt's time in minutes
-         */
-        $dayWorked = $this->daysRepository->findOneByDayAndWorked(strtolower($day->format('l')));
-        $isToday = ($day->format('Ymd') === $now->format('Ymd'));
-        $isMvtDay = ($day->format('Ymd') === $dateMvt->format('Ymd'));
-        $minutesWorked = 0;
-        $diffMinutesBetweenMvtAndNow = ((intval($now->diff($dateMvt)->h)) * 60) + intval($now->diff($dateMvt)->i);
-        $dayTestedTimeInMinutes = ((intval($day->format('H')) * 60) + intval($day->format('i')));
-        $nowTimeInMinutes = (intval($now->format('H')) * 60) + intval($now->format('i'));
-        $dateMvtTimeInMinutes = (intval($dateMvt->format('H')) * 60) + intval($dateMvt->format('i'));;
-        // If the day being tested is a working day (ex : Monday)
-        if ($dayWorked) {
-            /**
-             * Get the times in minutes of each point in the day
-             * @var $dayWorkedBeginOfDayInMinutes (ex : 8:00 -> 480)
-             * @var $dayWorkedEndOfDayInMinutes (ex : 18:00 -> 1080)
-             * @var $dayWorkedBeginOfBreakInMinutes (ex : 12:00 -> 720)
-             * @var $dayWorkedEndOfBreakInMinutes (ex : 14:00 -> 840)
-             **/
-            $timeArray = $this->getTimeArrayForDayWorked($dayWorked);
-            if (empty($timeArray)) return 0;
-
-            $dayWorkedBeginOfDayInMinutes = (($timeArray[self::MORNING_FIRST_HOUR_INDEX]) * 60) + $timeArray[self::MORNING_FIRST_MINUTE_INDEX];
-            $dayWorkedEndOfDayInMinutes = (($timeArray[self::AFTERNOON_LAST_HOUR_INDEX]) * 60) + $timeArray[self::AFTERNOON_LAST_MINUTE_INDEX];
-            $dayWorkedBeginOfBreakInMinutes = (($timeArray[self::MORNING_LAST_HOUR_INDEX] * 60) + $timeArray[self::MORNING_LAST_MINUTE_INDEX]);
-            $dayWorkedEndOfBreakInMinutes = (($timeArray[self::AFTERNOON_FIRST_HOUR_INDEX] * 60) + $timeArray[self::AFTERNOON_FIRST_MINUTE_INDEX]);
-
-            // If the day being tested is today
-            if ($isToday) {
-                /**
-                 * If the day being tested is also the movement's day,
-                 * which means that this day is the first and last to get tested
-                 **/
-                if ($isMvtDay) {
-                    // We add the minutes between now and the movement's time
-                    $minutesWorked += $diffMinutesBetweenMvtAndNow;
-                    /**
-                     * If we counted the break in the diff, we need to exclude it
-                     **/
-                    // Example : if mvt's time is 9:00
-                    if ($dayTestedTimeInMinutes <= $dayWorkedBeginOfBreakInMinutes) {
-                        // And now time is 15:00
-                        if ($nowTimeInMinutes >= $dayWorkedEndOfBreakInMinutes) {
-                            // We need to exclude the whole break
-                            $minutesWorked -= $this->getTimeBreakThisDayForDayWorked($dayWorked);
-                        } // And now time is 13:00
-                        else if ($nowTimeInMinutes < $dayWorkedEndOfBreakInMinutes
-                            && $nowTimeInMinutes > $dayWorkedBeginOfBreakInMinutes) {
-                            // We need to exclude the difference between 12:00 and 13:00
-                            $minutesWorked -= ($nowTimeInMinutes - $dayWorkedBeginOfBreakInMinutes);
-                        }
-                    } // Example : if the mvt's time is 13:00
-                    else if ($dayTestedTimeInMinutes <= $dayWorkedEndOfBreakInMinutes) {
-                        // And now time is 15:00
-                        if ($nowTimeInMinutes >= $dayWorkedEndOfBreakInMinutes) {
-                            // We need to exclude the difference between 14:00 and 13:00
-                            $minutesWorked -= ($dayWorkedEndOfBreakInMinutes - $dayTestedTimeInMinutes);
-                        } // And now time is 13:30
-                        else if ($nowTimeInMinutes < $dayWorkedEndOfBreakInMinutes
-                            && $nowTimeInMinutes > $dayWorkedBeginOfBreakInMinutes) {
-                            // We need to exclude the whole difference that we counted because it was in the day break
-                            $minutesWorked -= $diffMinutesBetweenMvtAndNow;
-                        }
-                    }
-                } // If the day tested is not the mvt's day
-                else {
-                    // If now time is before the break (ex : 10:30)
-                    if ($nowTimeInMinutes < $dayWorkedBeginOfBreakInMinutes) {
-                        // We only count the beginning of the day to now
-                        $minutesWorked += ($nowTimeInMinutes - $dayWorkedBeginOfDayInMinutes);
-                    } // If now time is in the break time
-                    else if ($nowTimeInMinutes < $dayWorkedEndOfBreakInMinutes) {
-                        // We only count the beginning of the day to the beginning of the break
-                        $minutesWorked += ($dayWorkedBeginOfBreakInMinutes - $dayWorkedBeginOfDayInMinutes);
-                    } else {
-                        // If now time is after the break then we can count the beginning of the day to now
-                        // excluding the break time
-                        $minutesWorked += (
-                            ($nowTimeInMinutes - $dayWorkedBeginOfDayInMinutes)
-                            -
-                            $this->getTimeBreakThisDayForDayWorked($dayWorked)
-                        );
-                    }
-                }
-            }
-            // If we are testing the day of the mvt but now today,
-            // which means that this is the first but not the last day to get tested
-            else if ($isMvtDay && !$isToday) {
-                // We count the whole time between the mvt's time and the end of day
-                $minutesWorked += (
-                    $dayWorkedEndOfDayInMinutes -
-                    $dateMvtTimeInMinutes
-                );
-                // Excluding the whole break is the mvt is before the break
-                if ($dateMvtTimeInMinutes <= $dayWorkedBeginOfBreakInMinutes) {
-                    $minutesWorked -= $this->getTimeBreakThisDayForDayWorked($dayWorked);
-                } // Or excluding the time between the mvt and the end of the break if the mvt is in the break
-                else if ($dateMvtTimeInMinutes <= $dayWorkedEndOfBreakInMinutes) {
-                    $minutesWorked -= ($dayWorkedEndOfBreakInMinutes - $dateMvtTimeInMinutes);
-                }
-            } // If the day tested is neither the mvt's day or today, we can count the whole day, excluding the break;
-            else {
-                $minutesWorked += $this->getTimeWorkedDuringThisDayForDayWorked($dayWorked);
-            }
-        }
-        return $minutesWorked;
-    }
-
-    /**
-     * @param int $minutesBetween
+     * @param DateInterval $movementAge
      * @param Emplacement $emplacement
      * @return array containing :
      * late => if the ref stayed too long on the emp
      * time => the time stayed on the emp
      */
-    public function buildDataForDatatable(int $minutesBetween, Emplacement $emplacement)
+    public function buildDataForDatatable(DateInterval $movementAge, Emplacement $emplacement)
     {
         $maxTime = $emplacement->getDateMaxTime();
         if ($maxTime) {
-            $timeHours = floor($minutesBetween / 60);
-            $timeMinutes = $minutesBetween % 60;
-            $time =
-                (($timeHours > 0)
-                    ? (($timeHours < 10 ? '0' : '') . $timeHours . ' h ')
+            $time = (
+                (($movementAge->h > 0)
+                    ? (($movementAge->h < 10 ? '0' : '') . $movementAge->h . ' h ')
                     : '')
-                . (($timeHours === 0 && $timeMinutes < 0)
+                . (($movementAge->i === 0 && $movementAge->h < 1)
                     ? '< 1 min'
-                    : ((($timeHours > 0 && $timeMinutes < 10) ? '0' : '') . $timeMinutes . ' min'));
+                    : ((($movementAge->h > 0 && $movementAge->i < 10) ? '0' : '') . $movementAge->i . ' min'))
+            );
             $maxTimeHours = intval(explode(':', $maxTime)[0]);
             $maxTimeMinutes = intval(explode(':', $maxTime)[1]);
-            $late = false;
-            if ($timeHours > $maxTimeHours) {
-                $late = true;
-            } else if (intval($timeHours) === $maxTimeHours) {
-                $late = $timeMinutes > $maxTimeMinutes;
-            }
+            $late = (
+                // true if age hour > $maxTimeHours
+                ($movementAge->h > $maxTimeHours) ||
+
+                // OR true if age hour == $maxTimeHours and age minutes > $maxTimeMinutes
+                (($movementAge->h === $maxTimeHours) && ($movementAge->i > $maxTimeMinutes))
+            );
+
             return [
                 'time' => $time,
                 'late' => $late,
@@ -301,13 +179,12 @@ class EnCoursService
         return null;
     }
 
-	/**
-	 * @param Emplacement $emplacement
-	 * @return array
-	 * @throws NonUniqueResultException
-	 * @throws NoResultException
-	 * @throws Exception
-	 */
+    /**
+     * @param Emplacement $emplacement
+     * @return array
+     * @throws DBALException
+     * @throws Exception
+     */
     public function getEnCoursForEmplacement(Emplacement $emplacement)
     {
         $success = true;
@@ -316,20 +193,16 @@ class EnCoursService
 
         foreach ($mouvements as $mouvement) {
             $dateMvt = new DateTime($mouvement->getDatetime()->format('d-m-Y H:i'), new DateTimeZone("Europe/Paris"));
-            $minutesBetween = $this->getMinutesBetween($dateMvt);
-            if (empty($minutesBetween)) {
-                $success = false;
-            } else {
-                $dataForTable = $this->buildDataForDatatable($minutesBetween, $emplacement);
-                if ($dataForTable) {
-                    $emplacementInfo[] = [
-                        'colis' => $mouvement->getColis(),
-                        'time' => $dataForTable['time'],
-                        'date' => $dateMvt->format('d/m/Y H:i:s'),
-                        'max' => $emplacement->getDateMaxTime(),
-                        'late' => $dataForTable['late']
-                    ];
-                }
+            $movementAge = $this->getTrackingMovementAge($dateMvt);
+            $dataForTable = $this->buildDataForDatatable($movementAge, $emplacement);
+            if ($dataForTable) {
+                $emplacementInfo[] = [
+                    'colis' => $mouvement->getColis(),
+                    'time' => $dataForTable['time'],
+                    'date' => $dateMvt->format('d/m/Y H:i:s'),
+                    'max' => $emplacement->getDateMaxTime(),
+                    'late' => $dataForTable['late']
+                ];
             }
         }
 
@@ -368,24 +241,100 @@ class EnCoursService
     }
 
     /**
-     * @param $dateMvt
-     * @return int
+     * @param DateTime $movementDate
+     * @return DateInterval
      * @throws Exception
      */
-    public function getMinutesBetween($dateMvt): int
-    {
-        $now = new DateTime("now", new DateTimeZone("Europe/Paris"));
-        $nowIncluding = (clone $now)->setTime(23, 59, 59);
-        $interval = DateInterval::createFromDateString('1 day');
-        $period = new DatePeriod($dateMvt, $interval, $nowIncluding);
-        $minutesBetween = 0;
-        /**
-         * @var $day DateTime
-         */
-        foreach ($period as $day) {
-            $minutesBetween += $this->getMinutesWorkedDuringThisDay($day, $now, $dateMvt);
-        }
+    public function getTrackingMovementAge(DateTime $movementDate): DateInterval {
+        $daysWorkedRepository = $this->entityManager->getRepository(DaysWorked::class);
 
-        return $minutesBetween;
+        $daysWorked = array_reduce(
+            $daysWorkedRepository->findAllOrdered(),
+            function (array $carry, DaysWorked $daysWorked) {
+                $times = $daysWorked->getTimes();
+                $worked = $daysWorked->getWorked();
+                if ($worked && !empty($times)) {
+                    $carry[strtolower($daysWorked->getDay())] = $daysWorked->getTimes();
+                }
+                return $carry;
+            },
+            []);
+
+
+        if (count($daysWorked) > 0) {
+            $now = new DateTime("now");
+            $nowIncluding = (clone $now)->setTime(23, 59, 59);
+            $interval = DateInterval::createFromDateString('1 day');
+            $period = new DatePeriod($movementDate, $interval, $nowIncluding);
+
+            $periodsWorked = [];
+            // pour chaque jour entre la date du mouvement et aujourd'hui, minimum un tour de boucle pour les mouvements du jours
+            /** @var DateTime $day */
+            foreach ($period as $day) {
+                $dayLabel = strtolower($day->format('l'));
+
+                if (isset($daysWorked[$dayLabel])) {
+                    $periodsWorked = array_merge(
+                        $periodsWorked,
+                        array_map(
+                            function (string $timePeriod) use ($now, $day, $movementDate) {
+                                // we calculate delay between two given times
+                                $times = explode('-', $timePeriod);
+
+                                $time1 = explode(':', $times[0]);
+                                $begin = (clone $day)->setTime($time1[0], $time1[1], 0);
+
+                                $time2 = explode(':', $times[1]);
+                                $end = (clone $day)->setTime($time2[0], $time2[1], 0);
+
+                                // si la date du mouvement est dans la fourchette => devient la date de begin
+                                if ($begin < $movementDate && $movementDate <= $end) {
+                                    $begin = $movementDate;
+                                }
+
+                                // si le DateTime 'now'  est dans la fourchette => devient la date de end
+                                if ($begin <= $now &&
+                                    $now < $end) {
+                                    $end = $now;
+                                }
+
+                                // Si la date du mouvement est après la fourchette de temps
+                                // ou si l'heure actuelle est avant la fourchette
+                                // on retourne un dateInterval de 0
+                                // sinon une dateInterval < 1 day
+                                return ($end < $movementDate || $now < $begin)
+                                    ? new DateInterval('P0Y')
+                                    : $begin->diff($end);
+                            },
+                            explode(';', $daysWorked[$dayLabel])
+                        )
+                    );
+                }
+            }
+
+            // on fait la somme de toutes les périodes calculés
+            $age = array_reduce(
+                $periodsWorked,
+                function (?DateInterval $carry, DateInterval $interval) {
+                    if (!isset($carry)) {
+                        return $interval;
+                    }
+                    else {
+                        $newDateInterval = new DateInterval('P0Y');
+                        $newDateInterval->h = ($carry->h + $interval->h);
+                        $newDateInterval->i = ($carry->i + $interval->i);
+                        $newDateInterval->s = ($carry->s + $interval->s);
+                        $newDateInterval->f = ($carry->f + $interval->f);
+                        return $newDateInterval;
+                    }
+                },
+                null
+            );
+        }
+        else {
+            // age null
+            $age = new DateInterval('P0Y');
+        }
+        return $age;
     }
 }
