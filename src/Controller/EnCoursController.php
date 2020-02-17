3b1318833939fcbdb3e884363504195f28285170
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-02-17 10:44:58 +0100

    WIIS-1873 : Refacto EnCoursController

--- src/Controller/EnCoursController.php
+++ src/Controller/EnCoursController.php
@@ -4,97 +4,61 @@
 namespace App\Controller;
 
 use App\Entity\Action;
+use App\Entity\DaysWorked;
+use App\Entity\Emplacement;
 use App\Entity\Menu;
-use App\Repository\DaysWorkedRepository;
-use App\Repository\MouvementTracaRepository;
-use App\Repository\EmplacementRepository;
-
+use App\Entity\MouvementTraca;
 use App\Service\EnCoursService;
-
 use App\Service\UserService;
-use DateInterval;
-use DatePeriod;
-use DateTime as DateTimeAlias;
+use Doctrine\DBAL\DBALException;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
 use Exception;
-
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+
 class EnCoursController extends AbstractController
 {
-
-    /**
-     * @var MouvementTracaRepository
-     */
-    private $mouvementTracaRepository;
-
-    /**
-     * @var EmplacementRepository
-     */
-    private $emplacementRepository;
-
-    /**
-     * @var DaysWorkedRepository
-     */
-    private $daysRepository;
-
-    /**
-     * @var EnCoursService
-     */
-    private $enCoursService;
-
-    private $userService;
-
-	/**
-	 * EnCoursController constructor.
-	 * @param UserService $userService
-	 * @param MouvementTracaRepository $mouvementTracaRepository
-	 * @param EmplacementRepository $emplacementRepository
-	 * @param DaysWorkedRepository $daysRepository
-	 * @param EnCoursService $enCoursService
-	 */
-    public function __construct(UserService $userService, MouvementTracaRepository $mouvementTracaRepository, EmplacementRepository $emplacementRepository, DaysWorkedRepository $daysRepository, EnCoursService $enCoursService)
-    {
-        $this->enCoursService = $enCoursService;
-        $this->daysRepository = $daysRepository;
-        $this->mouvementTracaRepository = $mouvementTracaRepository;
-        $this->emplacementRepository = $emplacementRepository;
-        $this->userService = $userService;
-    }
-
-
     /**
      * @Route("/encours", name="en_cours", methods={"GET"})
-     * @throws Exception
+     * @param UserService $userService
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
-    public function index(): Response
+    public function index(UserService $userService,
+                          EntityManagerInterface $entityManager): Response
     {
-		if (!$this->userService->hasRightFunction(Menu::TRACA, Action::DISPLAY_ENCO)) {
+		if (!$userService->hasRightFunction(Menu::TRACA, Action::DISPLAY_ENCO)) {
 			return $this->redirectToRoute('access_denied');
 		}
+
+        $emplacementRepository = $entityManager->getRepository(Emplacement::class);
+
         return $this->render('en_cours/index.html.twig', [
-            'emplacements' => $this->emplacementRepository->findWhereArticleIs()
+            'emplacements' => $emplacementRepository->findWhereArticleIs()
         ]);
     }
 
-	/**
-	 * @Route("/encours-api", name="en_cours_api", options={"expose"=true}, methods="GET|POST")
-	 * @param Request $request
-	 * @return JsonResponse
-	 * @throws NonUniqueResultException
-	 * @throws NoResultException
-	 */
-    public function apiForEmplacement(Request $request): Response {
+    /**
+     * @Route("/encours-api", name="en_cours_api", options={"expose"=true}, methods="GET|POST")
+     * @param Request $request
+     * @param EnCoursService $enCoursService
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function apiForEmplacement(Request $request,
+                                      EnCoursService $enCoursService,
+                                      EntityManagerInterface $entityManager): Response {
         if ($request->isXmlHttpRequest() && $data = json_decode($request->getContent(), true)) {
-            $emplacement = $this->emplacementRepository->find($data['id']);
-            return new JsonResponse($this->enCoursService->getEnCoursForEmplacement($emplacement));
+            $emplacement = $entityManager->find(Emplacement::class, $data['id']);
+            return new JsonResponse($enCoursService->getEnCoursForEmplacement($emplacement));
         }
         throw new NotFoundHttpException("404");
     }
@@ -103,19 +67,26 @@ class EnCoursController extends AbstractController
     /**
      * @Route("/retard-api", name="api_retard", options={"expose"=true}, methods="GET|POST")
      * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param EnCoursService $enCoursService
      * @return JsonResponse
+     * @throws DBALException
      * @throws Exception
      */
-    public function apiForRetard(Request $request): Response {
+    public function apiForRetard(Request $request,
+                                 EntityManagerInterface $entityManager,
+                                 EnCoursService $enCoursService): Response {
         if ($request->isXmlHttpRequest()) {
+            $emplacementRepository = $entityManager->getRepository(Emplacement::class);
+            $mouvementTracaRepository = $entityManager->getRepository(MouvementTraca::class);
             $retards = [];
-            foreach ($this->emplacementRepository->findWhereArticleIs() as $emplacementArray) {
-                $emplacement = $this->emplacementRepository->find($emplacementArray['id']);
-                $mouvements = $this->mouvementTracaRepository->findObjectOnLocation($emplacement);
+            foreach ($emplacementRepository->findWhereArticleIs() as $emplacementArray) {
+                $emplacement = $emplacementRepository->find($emplacementArray['id']);
+                $mouvements = $mouvementTracaRepository->findObjectOnLocation($emplacement);
                 foreach ($mouvements as $mouvement) {
                     $dateMvt = $mouvement->getDatetime();
-                    $minutesBetween = $this->enCoursService->getMinutesBetween($dateMvt);
-                    $dataForTable = $this->enCoursService->buildDataForDatatable($minutesBetween, $emplacement);
+                    $minutesBetween = $enCoursService->getMinutesBetween($dateMvt);
+                    $dataForTable = $enCoursService->buildDataForDatatable($minutesBetween, $emplacement);
                     if ($dataForTable && $dataForTable['late']) {
                         $retards[] = [
                             'colis' => $mouvement->getColis(),
@@ -134,37 +105,19 @@ class EnCoursController extends AbstractController
     }
 
     /**
-     * @param $dateMvt DateTimeAlias
-     * @return int
+     * @Route("/verification-temps-travaille", name="check_time_worked_is_defined", options={"expose"=true}, methods="GET|POST")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
      * @throws NonUniqueResultException
-     * @throws Exception
+     * @throws NoResultException
      */
-    private function getMinutesBetween($dateMvt): int
-    {
-        $now = new DateTimeAlias("now", new \DateTimeZone("Europe/Paris"));
-        $nowIncluding = (new DateTimeAlias("now", new \DateTimeZone("Europe/Paris")))
-            ->add(new DateInterval('PT' . (23 - intval($now->format('H'))) . 'H'));
-        // Get days between now and date mvt including now
-        $interval = DateInterval::createFromDateString('1 day');
-        $period = new DatePeriod($dateMvt, $interval, $nowIncluding);
-        $minutesBetween = 0;
-        /**
-         * @var $day DateTimeAlias
-         */
-        foreach ($period as $day) {
-        	$minutesBetween += $this->enCoursService->getMinutesWorkedDuringThisDay($day, $now, $dateMvt);
-        }
-
-        return $minutesBetween;
-    }
-
-	/**
-	 * @Route("/verification-temps-travaille", name="check_time_worked_is_defined", options={"expose"=true}, methods="GET|POST")
-	 */
-    public function checkTimeWorkedIsDefined(Request $request)
+    public function checkTimeWorkedIsDefined(Request $request,
+                                             EntityManagerInterface $entityManager)
 	{
 		if ($request->isXmlHttpRequest()) {
-			$nbEmptyTimes = $this->daysRepository->countEmptyTimes();
+		    $daysRepository = $entityManager->getRepository(DaysWorked::class);
+			$nbEmptyTimes = $daysRepository->countEmptyTimes();
 
 			return new JsonResponse($nbEmptyTimes == 0);
 		}
