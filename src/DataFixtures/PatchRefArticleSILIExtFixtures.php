7eb8c03eb69c3284494eec8b1380cdc018b08156
m.hevin <matteo.hevin@wiilog.fr>
2020-02-20 16:03:44 +0100

    renamed fixtures

---
+++ src/DataFixtures/PatchRefArticleSILIExtFixtures.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Type;
+use App\Entity\ValeurChampLibre;
+use App\Repository\CategorieCLRepository;
+use App\Repository\StatutRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use App\Entity\ReferenceArticle;
+use App\Repository\TypeRepository;
+use App\Repository\ChampLibreRepository;
+
+class PatchRefArticleSILIExtFixtures extends Fixture implements FixtureGroupInterface
+{
+    private $encoder;
+
+
+    /**
+     * @var TypeRepository
+     */
+    private $typeRepository;
+    /**
+     * @var ChampLibreRepository
+     */
+    private $champLibreRepository;
+
+    /**
+     * @var StatutRepository
+     */
+    private $statutRepository;
+
+    /**
+     * @var CategorieCLRepository
+     */
+    private $categorieCLRepository;
+
+    public function __construct(CategorieCLRepository $categorieCLRepository, UserPasswordEncoderInterface $encoder, TypeRepository $typeRepository, ChampLibreRepository $champLibreRepository, StatutRepository $statutRepository)
+    {
+        $this->typeRepository = $typeRepository;
+        $this->champLibreRepository = $champLibreRepository;
+        $this->encoder = $encoder;
+        $this->statutRepository = $statutRepository;
+        $this->categorieCLRepository = $categorieCLRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $path = "src/DataFixtures/Csv/sili-ext.csv";
+        $file = fopen($path, "r");
+
+        $rows = [];
+        while (($data = fgetcsv($file, 1000, ";")) !== false) {
+            $rows[] = array_map('utf8_encode', $data);
+        }
+
+        array_shift($rows); // supprime la 1è ligne d'en-têtes
+
+        $i = 1;
+        foreach ($rows as $row) {
+            if (empty($row[0])) continue;
+            dump($i);
+            $i++;
+            $typeSiliExt = $this->typeRepository->findOneBy(['label' => Type::LABEL_SILI_EXT]);
+
+            // contruction référence
+            $referenceNum = str_pad($i, 5, '0', STR_PAD_LEFT);
+
+            // champs fixes
+            $referenceArticle = new ReferenceArticle();
+            $referenceArticle
+                ->setType($typeSiliExt)
+                ->setReference('SILI_EXT_' . $referenceNum)
+                ->setLibelle($row[1])
+                ->setQuantiteStock(1)
+                ->setTypeQuantite('reference')
+                ->setStatut($this->statutRepository->findOneByCategorieNameAndStatutCode(ReferenceArticle::CATEGORIE, ReferenceArticle::STATUT_ACTIF));
+            $manager->persist($referenceArticle);
+            $manager->flush();
+
+
+            // champs libres
+            $listFields = [
+                ['label' => 'adresse', 'col' => 0],
+                ['label' => 'famille produit', 'col' => 2],
+                ['label' => 'date', 'col' => 3],
+                ['label' => "projet", 'col' => 4],
+                ['label' => "demandeur", 'col' => 5],
+                ['label' => "date fin de projet", 'col' => 6],
+                ['label' => "lot", 'col' => 7],
+//                ['label' => "sortie", 'col' => 8],
+//                ['label' => "commentaire", 'col' => 9],
+//                ['label' => "jours de péremption", 'col' => 10],
+            ];
+
+            foreach($listFields as $field) {
+                $vcl = new ValeurChampLibre();
+                $label = $field['label'] . ' (' . $typeSiliExt->getLabel() . ')';
+                $cl = $this->champLibreRepository->findOneBy(['label' => $label]);
+                if (empty($cl)) {
+                    dump('il manque le champ libre de label ' . $label);
+                } else {
+                    $vcl
+                        ->setChampLibre($cl)
+                        ->addArticleReference($referenceArticle)
+                        ->setValeur($row[$field['col']]);
+                    $manager->persist($vcl);
+                }
+            }
+
+            $manager->flush();
+        }
+        fclose($file);
+    }
+
+    public static function getGroups():array {
+        return ['siliext'];
+    }
+
+}
---
+++ src/DataFixtures/PatchRefArticleSILIFixtures.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ArticleFournisseur;
+use App\Entity\Fournisseur;
+use App\Entity\Type;
+use App\Entity\ValeurChampLibre;
+use App\Repository\ArticleFournisseurRepository;
+use App\Repository\CategorieCLRepository;
+use App\Repository\EmplacementRepository;
+use App\Repository\FournisseurRepository;
+use App\Repository\ReferenceArticleRepository;
+use App\Repository\StatutRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+use App\Entity\ReferenceArticle;
+use App\Repository\TypeRepository;
+use App\Repository\ChampLibreRepository;
+
+class PatchRefArticleSILIFixtures extends Fixture implements FixtureGroupInterface
+{
+    private $encoder;
+
+
+    /**
+     * @var TypeRepository
+     */
+    private $typeRepository;
+    /**
+     * @var ChampLibreRepository
+     */
+    private $champLibreRepository;
+
+    /**
+     * @var StatutRepository
+     */
+    private $statutRepository;
+
+    /**
+     * @var ReferenceArticleRepository
+     */
+    private $refArticleRepository;
+
+    /**
+     * @var CategorieCLRepository
+     */
+    private $categorieCLRepository;
+
+    /**
+     * @var EmplacementRepository
+     */
+    private $emplacementRepository;
+
+    /**
+     * @var FournisseurRepository
+     */
+    private $fournisseurRepository;
+
+    /**
+     * @var ArticleFournisseurRepository
+     */
+    private $articleFournisseurRepository;
+
+    public function __construct(ArticleFournisseurRepository $articleFournisseurRepository, FournisseurRepository $fournisseurRepository, EmplacementRepository $emplacementRepository, CategorieCLRepository $categorieCLRepository, ReferenceArticleRepository $refArticleRepository, UserPasswordEncoderInterface $encoder, TypeRepository $typeRepository, ChampLibreRepository $champLibreRepository, StatutRepository $statutRepository)
+    {
+        $this->typeRepository = $typeRepository;
+        $this->champLibreRepository = $champLibreRepository;
+        $this->encoder = $encoder;
+        $this->statutRepository = $statutRepository;
+        $this->refArticleRepository = $refArticleRepository;
+        $this->categorieCLRepository = $categorieCLRepository;
+        $this->emplacementRepository = $emplacementRepository;
+        $this->fournisseurRepository = $fournisseurRepository;
+        $this->articleFournisseurRepository = $articleFournisseurRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $path = "src/DataFixtures/Csv/sili.csv";
+        $file = fopen($path, "r");
+
+        $rows = [];
+        while (($data = fgetcsv($file, 1000, ";")) !== false) {
+            $rows[] = array_map('utf8_encode', $data);
+        }
+
+        $fournisseur = $this->initFournisseur($manager);
+
+        array_shift($rows); // supprime la 1è ligne d'en-têtes
+        $i = 1;
+        foreach ($rows as $row) {
+            if (empty($row[0])) continue;
+            dump($i);
+            $i++;
+            $typeSili = $this->typeRepository->findOneBy(['label' => Type::LABEL_SILI]);
+
+            // si l'article de référence existe déjà on le dédoublonne
+            $referenceArticle = $this->refArticleRepository->findOneBy(['reference' => $row[0]]);
+            if (!empty($referenceArticle)) {
+                $row[0] = $row[0] . '-' . $i;
+            }
+
+            // champs fixes
+            $referenceArticle = new ReferenceArticle();
+            $referenceArticle
+                ->setType($typeSili)
+                ->setStatut($this->statutRepository->findOneByCategorieNameAndStatutCode(ReferenceArticle::CATEGORIE, ReferenceArticle::STATUT_ACTIF))
+                ->setReference($row[0])
+                ->setQuantiteStock($row[3])
+                ->setLibelle($row[1])
+                ->setTypeQuantite(ReferenceArticle::TYPE_QUANTITE_REFERENCE);
+            $manager->persist($referenceArticle);
+            $manager->flush();
+
+
+            // article fournisseur
+            $articleFournisseur = new ArticleFournisseur();
+            $articleFournisseur
+                ->setLabel($row[1])
+                ->setReference(time() . '-' . $i)// code aléatoire unique
+                ->setFournisseur($fournisseur)
+                ->setReferenceArticle($referenceArticle);
+
+            $manager->persist($articleFournisseur);
+
+
+            // champs libres
+            $listFields = [
+                ['label' => 'adresse', 'col' => 2],
+                ['label' => 'famille produit', 'col' => 4],
+                ['label' => "alerte mini", 'col' => 13],
+                ['label' => "alerte prévision", 'col' => 14],
+            ];
+
+            foreach ($listFields as $field) {
+                $vcl = new ValeurChampLibre();
+                $label = $field['label'] . ' (' . $typeSili->getLabel() . ')';
+                $cl = $this->champsLibreRepository->findOneBy(['label' => $label]);
+                if (empty($cl)) {
+                    dump('il manque le champ libre de label ' . $label);
+                } else {
+                    $vcl
+                        ->setChampLibre($cl)
+                        ->addArticleReference($referenceArticle)
+                        ->setValeur($row[$field['col']]);
+                    $manager->persist($vcl);
+                }
+            }
+            $manager->flush();
+        }
+        fclose($file);
+    }
+
+    public static function getGroups():array {
+        return ['articlesSILI', 'articles'];
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return Fournisseur
+     */
+    public function initFournisseur(ObjectManager $manager): Fournisseur
+    {
+        $fournisseurLabel = 'A DETERMINER';
+        $fournisseurRef = 'A_DETERMINER';
+        $fournisseur = $this->fournisseurRepository->findOneBy(['codeReference' => $fournisseurRef]);
+
+        // si le fournisseur n'existe pas, on le crée
+        if (empty($fournisseur)) {
+            $fournisseur = new Fournisseur();
+            $fournisseur
+                ->setNom($fournisseurLabel)
+                ->setCodeReference($fournisseurRef);
+            $manager->persist($fournisseur);
+            $manager->flush();
+        }
+        return $fournisseur;
+    }
+
+}
