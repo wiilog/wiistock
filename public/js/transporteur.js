65b5ef0551b01561572216ed7ce31468a5d7863f
c.gazaniol <JauneCitr0n!>
2020-03-05 15:57:12 +0100

    WIIS-2059: nom et code transporteur uniques

--- public/js/transporteur.js
+++ public/js/transporteur.js
@@ -27,7 +27,7 @@ let tableTransporteur = $('#tableTransporteur_id').DataTable({
 let modalNewTransporteur = $("#modalNewTransporteur");
 let submitNewTransporteur = $("#submitNewTransporteur");
 let urlNewTransporteur = Routing.generate('transporteur_new', true);
-InitialiserModal(modalNewTransporteur, submitNewTransporteur, urlNewTransporteur, tableTransporteur);
+InitialiserModal(modalNewTransporteur, submitNewTransporteur, urlNewTransporteur, tableTransporteur, displayErrorTransporteur, false);
 
 let modalModifyTransporteur = $('#modalEditTransporteur');
 let submitModifyTransporteur = $('#submitEditTransporteur');
@@ -38,3 +38,8 @@ let modalDeleteTransporteur = $('#modalDeleteTransporteur');
 let submitDeleteTransporteur = $('#submitDeleteTransporteur');
 let urlDeleteTransporteur = Routing.generate('transporteur_delete', true);
 InitialiserModal(modalDeleteTransporteur, submitDeleteTransporteur, urlDeleteTransporteur, tableTransporteur);
+
+function displayErrorTransporteur(data) {
+    let modal = $('#modalNewTransporteur');
+    displayError(modal, data.msg, data.success);
+}
