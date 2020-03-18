d5d2ad38223d0bcafc2f9fe68b4c93a35fae9e86
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-03-18 11:45:19 +0100

    WIIS-2090 : on met 0 à la place des valeurs null

--- src/Migrations/Version20200313090612.php
+++ src/Migrations/Version20200313090612.php
@@ -32,6 +32,7 @@ final class Version20200313090612 extends AbstractMigration
                                 WHERE s.nom = 'disponible' AND af.reference_article_id = ra.id
                             )
                             WHERE ra.type_quantite = 'article'");
+        $this->addSql("UPDATE reference_article AS ra SET quantite_stock = 0 WHERE quantite_stock IS NULL");
         $this->addSql("
                             UPDATE reference_article AS ra
                             SET quantite_reservee =
@@ -42,6 +43,7 @@ final class Version20200313090612 extends AbstractMigration
                                 INNER JOIN statut s on p.statut_id = s.id
                                 WHERE (s.nom = 'à traiter' OR s.nom = 'en cours de préparation') AND l.reference_id = ra.id
                             )");
+        $this->addSql("UPDATE reference_article AS ra SET quantite_reservee = 0 WHERE quantite_reservee IS NULL");
         $this->addSql("
                             UPDATE reference_article AS ra
                             SET ra.quantite_disponible = ra.quantite_stock - ra.quantite_reservee
