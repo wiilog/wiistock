7e0464fe6cb872be4a1e2dfd7cbc1b072563974e
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-04-10 15:09:12 +0200

    WIIS-2213 : alignement boutons

--- public/js/script-menu.js
+++ public/js/script-menu.js
@@ -3,6 +3,7 @@ $(function () {
     $('.menu-button').on('click', function () {
         $('.dropdown-menu-sub').removeClass(classShow);
     });
+
     $('.dropdown-item-sub').on('click', function () {
         const $dropdownItem = $(this);
         const $dropdownSub = $dropdownItem.siblings('.dropdown-menu-sub');
