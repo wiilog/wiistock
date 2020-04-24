ae58055ba61a663cdcf31e88795dec2e4708f5b0
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-04-24 09:58:10 +0200

    WIIS-2280 : Fix élément non pris en compte pour diviser les tableaux

--- public/js/mission-inventaire-show.js
+++ public/js/mission-inventaire-show.js
@@ -1,6 +1,11 @@
 $(function() {
     initDateTimePicker();
-    initSearchDate(tableMission);
+
+    const {tableArticle, tableRefArticle} = initPage();
+
+    initSearchDate(tableArticle);
+    initSearchDate(tableRefArticle);
+
     $('.select2').select2();
 
     // filtres enregistrés en base pour chaque utilisateur
@@ -11,67 +16,88 @@ $(function() {
     }, 'json');
 });
 
-let mission = $('#missionId').val();
-let pathApiArticle = Routing.generate('inv_entry_article_api', { id: mission}, true);
-let tableMission = $('#tableMissionInvArticle').DataTable({
-    processing: true,
-    serverSide: true,
-    language: {
-        url: "/js/i18n/dataTableLanguage.json",
-    },
-    order: [[3, 'desc']],
-    scrollX: true,
-    ajax:{
-        "url": pathApiArticle,
-        "type": "POST",
-    },
-    'drawCallback': function() {
-        overrideSearch($('#tableMissionInvArticle_filter input'), tableMission);
-    },
-    'rowCallback': function(row, data) {
-        if (data.EmptyLocation) alertErrorMsg('Il manque un ou plusieurs emplacements : ils n\'apparaîtront pas sur le nomade.');
-    },
-    columns:[
-        { "data": 'Ref', 'title' : 'Reférence' },
-        { "data": 'Label', 'title' : 'Libellé' },
-        { "data": 'Location', 'title' : 'Emplacement', 'name': 'location' },
-        { "data": 'Date', 'title' : 'Date de saisie', 'name': 'date' },
-        { "data": 'Anomaly', 'title' : 'Anomalie', 'name' : 'anomaly'  }
-    ],
-});
 
-let pathApiReferenceArticle = Routing.generate('inv_entry_reference_article_api', { id: mission}, true);
-let tableMission2 = $('#tableMissionInvReferenceArticle').DataTable({
-    processing: true,
-    serverSide: true,
-    language: {
-        url: "/js/i18n/dataTableLanguage.json",
-    },
-    order: [[3, 'desc']],
-    scrollX: true,
-    ajax:{
-        "url": pathApiReferenceArticle,
-        "type": "POST",
-    },
-    'drawCallback': function() {
-        overrideSearch($('#tableMissionInvReferenceArticle_filter input'), tableMission2);
-    },
-    'rowCallback': function(row, data) {
-        if (data.EmptyLocation) alertErrorMsg('Il manque un ou plusieurs emplacements : ils n\'apparaîtront pas sur le nomade.');
-    },
-    columns:[
-        { "data": 'Ref', 'title' : 'Reférence' },
-        { "data": 'Label', 'title' : 'Libellé' },
-        { "data": 'Location', 'title' : 'Emplacement', 'name': 'location' },
-        { "data": 'Date', 'title' : 'Date de saisie', 'name': 'date' },
-        { "data": 'Anomaly', 'title' : 'Anomalie', 'name' : 'anomaly'  }
-    ],
-});
+function initPageElements() {
+    let mission = $('#missionId').val();
+    let pathApiArticle = Routing.generate('inv_entry_article_api', { id: mission}, true);
+    let tableArticle = $('#tableMissionInvArticle').DataTable({
+        processing: true,
+        serverSide: true,
+        language: {
+            url: "/js/i18n/dataTableLanguage.json",
+        },
+        order: [[3, 'desc']],
+        scrollX: true,
+        ajax:{
+            "url": pathApiArticle,
+            "type": "POST",
+        },
+        'drawCallback': function() {
+            overrideSearch($('#tableMissionInvArticle_filter input'), tableArticle);
+        },
+        'rowCallback': function(row, data) {
+            if (data.EmptyLocation) alertErrorMsg('Il manque un ou plusieurs emplacements : ils n\'apparaîtront pas sur le nomade.');
+        },
+        columns:[
+            { "data": 'Ref', 'title' : 'Reférence' },
+            { "data": 'Label', 'title' : 'Libellé' },
+            { "data": 'Location', 'title' : 'Emplacement', 'name': 'location' },
+            { "data": 'Date', 'title' : 'Date de saisie', 'name': 'date' },
+            { "data": 'Anomaly', 'title' : 'Anomalie', 'name' : 'anomaly'  }
+        ],
+    });
 
-let modalAddToMission = $("#modalAddToMission");
-let submitAddToMission = $("#submitAddToMission");
-let urlAddToMission = Routing.generate('add_to_mission', true);
-InitialiserModal(modalAddToMission, submitAddToMission, urlAddToMission, tableMission, displayErrorAddToMission);
+    let pathApiReferenceArticle = Routing.generate('inv_entry_reference_article_api', { id: mission}, true);
+    let tableRefArticle = $('#tableMissionInvReferenceArticle').DataTable({
+        processing: true,
+        serverSide: true,
+        language: {
+            url: "/js/i18n/dataTableLanguage.json",
+        },
+        order: [[3, 'desc']],
+        scrollX: true,
+        ajax:{
+            "url": pathApiReferenceArticle,
+            "type": "POST",
+        },
+        'drawCallback': function() {
+            overrideSearch($('#tableMissionInvReferenceArticle_filter input'), tableRefArticle);
+        },
+        'rowCallback': function(row, data) {
+            if (data.EmptyLocation) alertErrorMsg('Il manque un ou plusieurs emplacements : ils n\'apparaîtront pas sur le nomade.');
+        },
+        columns:[
+            { "data": 'Ref', 'title' : 'Reférence' },
+            { "data": 'Label', 'title' : 'Libellé' },
+            { "data": 'Location', 'title' : 'Emplacement', 'name': 'location' },
+            { "data": 'Date', 'title' : 'Date de saisie', 'name': 'date' },
+            { "data": 'Anomaly', 'title' : 'Anomalie', 'name' : 'anomaly'  }
+        ],
+    });
+
+    let modalAddToMission = $("#modalAddToMission");
+    let submitAddToMission = $("#submitAddToMission");
+    let urlAddToMission = Routing.generate('add_to_mission', true);
+    InitialiserModal(
+        modalAddToMission,
+        submitAddToMission,
+        urlAddToMission,
+        {
+            ajax: {
+                reload(param1, param2) {
+                    tableArticle.ajax.reload(param1, param2);
+                    tableRefArticle.ajax.reload(param1, param2);
+                }
+            }
+        },
+        displayErrorAddToMission
+    );
+
+    return {
+        tableArticle,
+        tableRefArticle
+    };
+}
 
 function displayErrorAddToMission(data)
 {
