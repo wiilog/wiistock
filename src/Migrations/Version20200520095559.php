d6b24c9c3f64649a73723ab24c6374384ebd4ed4
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-05-25 14:35:05 +0200

    WIIS-2375 : code cleaning

--- src/Migrations/Version20200520095559.php
+++ src/Migrations/Version20200520095559.php
@@ -7,10 +7,8 @@ namespace DoctrineMigrations;
 use App\Entity\Utilisateur;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -20,8 +18,7 @@ final class Version20200520095559 extends AbstractMigration implements Container
 
     use ContainerAwareTrait;
 
-    public function getDescription() : string
-    {
+    public function getDescription(): string {
         return 'On enlève "Référence" du champ recherche_for_article des utilisateurs';
     }
 
@@ -37,9 +34,7 @@ final class Version20200520095559 extends AbstractMigration implements Container
         $usersRepository = $em->getRepository(Utilisateur::class);
         $users = $usersRepository->findByFieldNotNull('rechercheForArticle');
         foreach ($users as $user) {
-            /**
-             * @var Utilisateur $user
-             */
+            /** @var Utilisateur $user */
             $userArticleSearch = $user->getRechercheForArticle();
             if (in_array('Référence', $userArticleSearch)) {
                 $newArticleSearch = array_filter($userArticleSearch, function (string $field) {
