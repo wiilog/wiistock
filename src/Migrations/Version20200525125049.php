d574e7279d65dbe7db4f1f5ac150b05566fa7bfb
m.hevin <matteo.hevin@wiilog.fr>
2020-05-25 15:55:41 +0200

    WIIS-2403 refacto dl cl, with migration for previous mistakes

---
+++ src/Migrations/Version20200525125049.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\ChampLibre;
+use App\Entity\Demande;
+use App\Entity\Utilisateur;
+use App\Entity\ValeurChampLibre;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200525125049 extends AbstractMigration implements ContainerAwareInterface
+{
+
+    use ContainerAwareTrait;
+
+    public function getDescription(): string
+    {
+        return 'On supprime les mauvaises valeur cl';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+
+    public function postUp(Schema $schema): void
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $demandeRepository = $em->getRepository(Demande::class);
+        $valeurChampLibreRepository = $em->getRepository(ValeurChampLibre::class);
+        $champLibreRepository = $em->getRepository(ChampLibre::class);
+        $demandes = $demandeRepository->findAll();
+        foreach ($demandes as $demande) {
+            $vclToDelete = $demandeRepository->findByDemandeWhereTypeIsDifferent($demande);
+            $valeurChampLibreRepository->deleteIfNotIn($vclToDelete);
+        }
+        $em->flush();
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
