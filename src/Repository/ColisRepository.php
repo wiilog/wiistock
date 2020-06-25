a87f00f970e71948a3470ffbea5e92e1caa7ec19
Matteo <matteohevin@gmail.com>
2020-06-25 17:46:55 +0200

    opti safran ed test

--- src/Repository/ColisRepository.php
+++ src/Repository/ColisRepository.php
@@ -74,17 +74,12 @@ class ColisRepository extends EntityRepository
 
     public function updateByIds(array $ids, int $mvtId)
     {
-        $queryBuilder = $this->createQueryBuilder('colis');
-        $queryBuilderExpr = $queryBuilder->expr();
-        $queryBuilder
-            ->update(Colis::class, 'colis')
-            ->set('colis.lastDrop', $mvtId)
-            ->where(
-                $queryBuilderExpr->in('colis.id', ':ids')
-            )
-            ->setParameter('ids', $ids)
-            ->getQuery()
-            ->execute();
+        $arrayColisId = implode(',', $ids);
+        $sqlQuery = "
+            UPDATE colis SET last_drop_id = ${mvtId} WHERE id IN (${arrayColisId})
+        ";
+        $connection = $this->getEntityManager()->getConnection();
+        $connection->executeQuery($sqlQuery, []);
     }
 
     /**
