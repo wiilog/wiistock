8d3bffe7b7f1acbc2340955190f7e3e7a3044de2
t.gentile <thomas.gentile@wiilog.fr>
2020-07-22 12:20:13 +0200

    WIIS-2587:
    - Remove deploy folder
    - Remove MJML dependency
    - Remove all MJML occurrences
    - Restylize all mail templates + DOM changes

--- config/bundles.php
+++ config/bundles.php
@@ -19,5 +19,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SGK\BarcodeBundle\SGKBarcodeBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
-    Assoconnect\MJMLBundle\AssoconnectMJMLBundle::class => ['all' => true],
 ];
