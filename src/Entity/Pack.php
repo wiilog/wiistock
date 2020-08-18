8a2c25f507d9077f15126e2d301557803cdd2763
m.hevin <matteo.hevin@wiilog.fr>
2020-08-18 16:19:13 +0200

    WIIS-2662

--- src/Entity/Pack.php
+++ src/Entity/Pack.php
@@ -55,7 +55,7 @@ class Pack
 
     /**
      * @var Collection
-     * @ORM\OneToMany(targetEntity="App\Entity\MouvementTraca", mappedBy="pack")
+     * @ORM\OneToMany(targetEntity="App\Entity\MouvementTraca", mappedBy="pack", cascade={"remove"})
      * @ORM\OrderBy({"datetime" = "DESC"})
      */
     private $trackingMovements;
@@ -156,7 +156,7 @@ class Pack
     }
 
     /**
-     * @return Collection
+     * @return Collection|MouvementTraca[]
      */
     public function getTrackingMovements(): Collection {
         return $this->trackingMovements;
