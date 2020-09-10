78ba0c719e37f0a6d93128c37c4a9b06b038593b
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-09-10 14:25:46 +0200

    fix merge

--- src/Controller/HandlingController.php
+++ src/Controller/HandlingController.php
@@ -258,7 +258,7 @@ class HandlingController extends AbstractController
             $json = $this->renderView('handling/modalEditHandlingContent.html.twig', [
                 'handling' => $handling,
                 'utilisateurs' => $utilisateurRepository->findAll(),
-                'treatedStatus' => $statutRepository->findTreatedStatusByType(CategorieStatut::HANDLING, $handling->getType(), true),
+                'handlingStatus' => $statutRepository->findTreatedStatusByType(CategorieStatut::HANDLING, $handling->getType(), true),
                 'attachments' => $attachmentsRepository->findBy(['handling' => $handling]),
             ]);
 
