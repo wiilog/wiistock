83b80b733d1d0f84d8f363851af2bc6fa2533627
Adrien Bounader <adrien.bounade@wiilog.fr>
2020-09-10 09:56:50 +0200

    WIIS-2694 translation improve & code cleaning

--- migrations/Version20200907142945.php
+++ migrations/Version20200907142945.php
@@ -19,12 +19,12 @@ final class Version20200907142945 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('ALTER TABLE handling CHANGE `date` `creation_date` datetime');
-        $this->addSql('ALTER TABLE handling CHANGE `date_attendue` `desired_date` datetime');
-        $this->addSql('ALTER TABLE handling CHANGE `date_end` `validation_date` datetime');
-        $this->addSql('ALTER TABLE handling CHANGE `libelle` `subject` text');
-        $this->addSql('ALTER TABLE handling CHANGE `demandeur_id` `requester_id` integer');
-        $this->addSql('ALTER TABLE handling CHANGE commentaire `comment` text');
+        $this->addSql('ALTER TABLE handling CHANGE `date` `creation_date` DATETIME');
+        $this->addSql('ALTER TABLE handling CHANGE `date_attendue` `desired_date` DATETIME');
+        $this->addSql('ALTER TABLE handling CHANGE `date_end` `validation_date` DATETIME');
+        $this->addSql('ALTER TABLE handling CHANGE `libelle` `subject` TEXT');
+        $this->addSql('ALTER TABLE handling CHANGE `demandeur_id` `requester_id` INT');
+        $this->addSql('ALTER TABLE handling CHANGE commentaire `comment` TEXT');
     }
 
     public function down(Schema $schema) : void
