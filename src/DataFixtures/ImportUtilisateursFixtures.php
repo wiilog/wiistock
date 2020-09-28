54b0622d9057319cfbd3bac9797a6046cc6c8a42
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2020-09-28 16:26:30 +0200

    Import utilisateurs

--- src/DataFixtures/ImportUtilisateursFixtures.php
+++ src/DataFixtures/ImportUtilisateursFixtures.php
@@ -9,11 +9,11 @@ use App\Service\PasswordService;
 use App\Service\UserService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class ImportUtilisateursFixtures extends Fixture implements FixtureGroupInterface
-{
+class ImportUtilisateursFixtures extends Fixture implements FixtureGroupInterface {
 
     /**
      * @var PasswordService
@@ -28,82 +28,76 @@ class ImportUtilisateursFixtures extends Fixture implements FixtureGroupInterfac
 
     public function __construct(PasswordService $passwordService,
                                 UserService $userService,
-                                UserPasswordEncoderInterface $encoder)
-    {
+                                UserPasswordEncoderInterface $encoder) {
         $this->passwordService = $passwordService;
         $this->encoder = $encoder;
         $this->userService = $userService;
     }
 
-    public function load(ObjectManager $manager)
-    {
-		$path = "src/DataFixtures/utilisateurs.csv";
-		$file = fopen($path, "r");
+    public function load(ObjectManager $manager) {
+        $emails = [];
 
-        $firstRow = true;
+        $path = "src/DataFixtures/utilisateurs.csv";
+        $file = fopen($path, "r");
+        $header = fgetcsv($file, 0, ",");
 
-        $emplacementRepository = $manager->getRepository(Emplacement::class);
-        $roleRepository = $manager->getRepository(Role::class);
         $utilisateurRepository = $manager->getRepository(Utilisateur::class);
+        $roleRepository = $manager->getRepository(Role::class);
 
-        while (($data = fgetcsv($file, 1000, ";")) !== false) {
-        	if ($firstRow) {
-        		$firstRow = false;
-			} else {
-				$row = array_map('utf8_encode', $data);
-                $username = $row[2];
-                $mail = $row[3];
-                $emplacementStr = $row[4];
-                $roleStr = $row[5];
-                $pass = $this->passwordService->generateToken(8);
-				$utilisateur = $utilisateurRepository->findOneBy(['email' => $mail]);
-				if (empty($utilisateur)) {
-                    $role = $roleRepository->findByLabel($roleStr);
-                    $emplacement = $emplacementRepository->findOneByLabel($emplacementStr);
-				    if (empty($role)) {
-				        $role = new Role();
-				        $role
-                            ->setLabel($roleStr)
-                            ->setActive(true);
-                        $manager->persist($role);
-                    }
-                    if (empty($emplacement)) {
-                        $emplacement = new Emplacement();
-                        $emplacement
-                            ->setLabel($emplacementStr)
-                            ->setIsActive(true);
-                        $manager->persist($emplacement);
-                    }
+        $role = $roleRepository->findOneBy(["label" => "Utilisateur Emerson"]);
+        if(!$role) {
+            $role = new Role();
+            $role->setLabel("Utilisateur Emerson");
+            $role->setIsMailSendAccountCreation(false);
+            $role->setDashboardsVisible([]);
+            $role->setActive(true);
 
+            $manager->persist($role);
+            $manager->flush();
+        }
+
+
+        while($line = fgetcsv($file, 0, ",")) {
+            $matches = [];
+            preg_match("/[a-z-_\.]+@[a-z]+\.[a-z]+/i", $line[1], $matches);
+
+            if(isset($matches[0])) {
+                $email = strtolower($matches[0]);
+                $active = $line[2];
+
+                $existing = $utilisateurRepository->findOneBy(['email' => $email]);
+
+                if(!$existing && !isset($emails[$email])) {
                     $uniqueMobileKey = $this->userService->createUniqueMobileLoginKey($manager);
 
-                    $utilisateur = new Utilisateur();
-					$utilisateur
-                        ->setUsername($username)
-                        ->setEmail($mail)
+                    $user = new Utilisateur();
+                    $user
+                        ->setUsername($email)
+                        ->setEmail($email)
                         ->setRole($role)
-                        ->setStatus(true)
-						->setRoles(['USER'])// évite bug -> champ roles ne doit pas être vide
-						->setRechercheForArticle(Utilisateur::SEARCH_DEFAULT)
-						->setRecherche(Utilisateur::SEARCH_DEFAULT)
-						->setDropzone($emplacement)
-						->setColumnVisible(Utilisateur::COL_VISIBLE_REF_DEFAULT)
-						->setColumnsVisibleForArticle(Utilisateur::COL_VISIBLE_ARTICLES_DEFAULT)
+                        ->setStatus($active)
+                        ->setRoles(['USER'])// évite bug -> champ roles ne doit pas être vide
+                        ->setRechercheForArticle(Utilisateur::SEARCH_DEFAULT)
+                        ->setRecherche(Utilisateur::SEARCH_DEFAULT)
+                        ->setColumnVisible(Utilisateur::COL_VISIBLE_REF_DEFAULT)
+                        ->setColumnsVisibleForArticle(Utilisateur::COL_VISIBLE_ARTICLES_DEFAULT)
                         ->setColumnsVisibleForArrivage(Utilisateur::COL_VISIBLE_ARR_DEFAULT)
                         ->setColumnsVisibleForDispatch(Utilisateur::COL_VISIBLE_DISPATCH_DEFAULT)
                         ->setColumnsVisibleForLitige(Utilisateur::COL_VISIBLE_LIT_DEFAULT)
-                        ->setPassword($this->encoder->encodePassword($utilisateur, $pass))
+                        ->setPassword(null)
                         ->setMobileLoginKey($uniqueMobileKey);
 
-					$manager->persist($utilisateur);
-        			$manager->flush();
-				}
-			}
+                    $manager->persist($user);
+
+                    $emails[$email] = true;
+                }
+            }
         }
+
+        $manager->flush();
     }
 
-	public static function getGroups(): array
-	{
-		return ['import-utilisateurs'];
-	}
+    public static function getGroups(): array {
+        return ['import-utilisateurs'];
+    }
 }
