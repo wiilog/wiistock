463c1a574d0d6588461dc6d4e1b83bfb908dd488
m.hevin <matteo.hevin@wiilog.fr>
2020-10-05 09:38:35 +0200

    WIIS-2572 revue + phpdoc

--- src/Helper/QueryCounter.php
+++ src/Helper/QueryCounter.php
@@ -2,11 +2,20 @@
 
 namespace App\Helper;
 
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\QueryBuilder;
 use RuntimeException;
 
 class QueryCounter {
 
+    /**
+     * @param QueryBuilder $query
+     * @param string|null $alias
+     * @return int
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
     public static function count(QueryBuilder $query, ?string $alias = null): int {
         $original = $query->getDQLPart("select");
 
