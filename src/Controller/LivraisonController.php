bb52b81072b959f238a1719a12981a1094e639e3
c.roux <cedric.roux@wiilog.fr>
2020-10-30 12:55:08 +0100

    WIIS-3149 hotfix bug delete Delivery Order

--- src/Controller/LivraisonController.php
+++ src/Controller/LivraisonController.php
@@ -315,7 +315,7 @@ class LivraisonController extends AbstractController
 
             $data = [
                 'success' => true,
-                'redirect' => $this->redirect('preparation_show', [
+                'redirect' => $this->generateUrl('preparation_show', [
                     'id' => $preparation->getId()
                 ]),
             ];
