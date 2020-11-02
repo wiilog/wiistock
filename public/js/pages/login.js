7d53bc1efecd3700a64239aa35c83eec24d9edbb
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2020-11-02 11:09:56 +0100

    WIIS-3151: Allow firefox and chromium based browsers

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,19 +1,8 @@
 $(document).ready(function() {
-    let isChromium = window.chrome;
-    let winNav = window.navigator;
-    let vendorName = winNav.vendor;
-    let isOpera = typeof window.opr !== "undefined";
-    let isIEedge = winNav.userAgent.indexOf("Edge") > -1;
-    let isIOSChrome = winNav.userAgent.match("CriOS");
+    let isChromium = !!window.chrome;
+    let isFirefox = navigator.userAgent.includes("Firefox");
 
-    if (isIOSChrome) {
-        // is Google Chrome on IOS
-    } else if(
-        isChromium === null ||
-        typeof isChromium === "undefined" ||
-        vendorName !== "Google Inc." ||
-        isOpera !== false ||
-        isIEedge !== false) {
+    if(!isChromium && !isFirefox) {
         $('.messageChrome').addClass('d-block');
         $('.form-signin').addClass('d-none');
     }
