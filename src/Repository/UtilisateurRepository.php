5ca7101782512b7375ab2274080812fa01d2208a
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2020-11-12 11:54:11 +0100

    Fix export

--- src/Repository/UtilisateurRepository.php
+++ src/Repository/UtilisateurRepository.php
@@ -210,4 +210,26 @@ class UtilisateurRepository extends EntityRepository implements UserLoaderInterf
     public function loadUserByUsername($username) {
         return $this->findOneBy(['email' => $username]);
     }
+
+    public function getUsernameManagersGroupByReference() {
+        $result = $this->createQueryBuilder('utilisateur')
+            ->select('referencesArticle.id AS referencesArticleId')
+            ->addSelect('utilisateur.username')
+            ->join('utilisateur.referencesArticle', 'referencesArticle')
+            ->getQuery()
+            ->getResult();
+
+        return array_reduce($result, function ($acc, $attachment) {
+            $referenceArticleId = (int)$attachment['referencesArticleId'];
+            if (empty($acc[$referenceArticleId])) {
+                $acc[$referenceArticleId] = '';
+            } else {
+                $acc[$referenceArticleId] .= ', ';
+            }
+
+            $acc[$referenceArticleId] .= $attachment['username'];
+            return $acc;
+        }, []);
+    }
+
 }
