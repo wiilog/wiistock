e7e6a91ecadc48241796588937c2e51101741807
Adrien Bounader <adrien.bounader@wiilog.fr>
2020-11-13 12:17:28 +0100

    WIIS-3143

--- migrations/Version20201110095405.php
+++ migrations/Version20201110095405.php
@@ -19,120 +19,76 @@ final class Version20201110095405 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $dispatch = $this->connection->executeQuery("
-            SELECT number AS number, COUNT(*) AS count
-            FROM dispatch
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $deliveryRequest = $this->connection->executeQuery("
-            SELECT numero AS number, COUNT(*) AS count
-            FROM demande
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $collectRequest = $this->connection->executeQuery("
-            SELECT numero AS number, COUNT(*) AS count
-            FROM collecte
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $collectOrder = $this->connection->executeQuery("
-            SELECT numero AS number, COUNT(*) AS count
-            FROM ordre_collecte
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $reception = $this->connection->executeQuery("
-            SELECT numero_reception AS number, COUNT(*) AS count
-            FROM reception
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $transferRequest = $this->connection->executeQuery("
-            SELECT number AS number, COUNT(*) AS count
-            FROM transfer_request
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $transferOrder = $this->connection->executeQuery("
-            SELECT number AS number, COUNT(*) AS count
-            FROM transfer_order
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
-        $handling = $this->connection->executeQuery("
-            SELECT number AS number, COUNT(*) AS count
-            FROM handling
-            GROUP BY number
-            HAVING COUNT(*) > 1
-        ");
-
         $sqlRequests = [
             'dispatch' => [
-                $dispatch,
-                'number',
-                'creation_date'
+                'number' => 'number',
+                'order' => 'creation_date'
             ],
             'demande' => [
-                $deliveryRequest,
-                'numero',
-                'date'
+                'number' => 'numero',
+                'order' => 'date'
             ],
             'collecte' => [
-                $collectRequest,
-                'numero',
-                'date'
+                'number' => 'numero',
+                'order' => 'date'
             ],
             'ordre_collecte' => [
-                $collectOrder,
-                'numero',
-                'ordre'
+                'number' => 'numero',
+                'order' => 'date'
             ],
             'reception' => [
-                $reception,
-                'numero_reception',
-                'date'
+                'number' => 'numero_reception',
+                'order' => 'date'
             ],
             'transfer_request' => [
-                $transferRequest,
-                'number',
-                'creation_date'
+                'number' => 'number',
+                'order' => 'creation_date'
             ],
             'transfer_order' => [
-                $transferOrder,
-                'number',
-                'creation_date'
+                'number' => 'number',
+                'order' => 'creation_date'
             ],
             'handling' => [
-                $handling,
-                'number',
-                'creation_date'
+                'number' => 'number',
+                'order' => 'creation_date'
             ]
         ];
 
         foreach ($sqlRequests as $table => $request) {
-            $duplicateNumbers = $request[0]->fetchAll();
-            if (!empty($duplicateNumbers)) {
-                foreach ($duplicateNumbers as $number) {
-                    $index = 1;
-                    while ($index < intval($number['count'])) {
-                        $currentNumber = $number['number'];
-                        $newNumber = $currentNumber . '-0' . $index;
-                        $this->addSql("
-                            UPDATE ${table}
-                            SET ${request[1]} = '${newNumber}'
-                            WHERE ${request[1]} = '${currentNumber}'
-                            LIMIT 1");
-                        $index++;
+            $numberLabel = $request['number'];
+            $orderLabel = $request['order'];
+
+            $results = $this->connection
+                ->executeQuery("
+                    SELECT id, ${numberLabel} AS number
+                    FROM ${table}
+                    WHERE ${numberLabel} IN (SELECT duplicate.${numberLabel} FROM ${table} AS duplicate GROUP BY duplicate.${numberLabel} HAVING COUNT(id) > 1)
+                    ORDER BY ${orderLabel}
+                ")
+                ->fetchAll();
+
+            if (!empty($results)) {
+                $lastNumber = null;
+                $lastIndex = null;
+                foreach ($results as $row) {
+                    $currentNumber = $row['number'];
+                    $currentId = $row['id'];
+
+                    // we reset counter if we have change number
+                    if (!isset($lastNumber) || $lastNumber !== $currentNumber) {
+                        $lastNumber = $currentNumber;
+                        $lastIndex = 0;
                     }
+
+                    $lastIndex++;
+
+                    $suffix = sprintf('%02u', $lastIndex);
+                    $newNumber = $currentNumber . '-' . $suffix;
+                    $this->addSql("
+                        UPDATE ${table}
+                        SET ${numberLabel} = '${newNumber}'
+                        WHERE id = ${currentId}
+                    ");
                 }
             }
         }
