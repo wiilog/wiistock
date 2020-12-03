e1342f792f4def5e4b66ce81209cc592f2fe247f
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2020-12-03 14:42:59 +0100

    Feedback for update translations

--- src/Command/UpdateTranslationsCommand.php
+++ src/Command/UpdateTranslationsCommand.php
@@ -1,42 +1,33 @@
 <?php
 
-
 namespace App\Command;
-
 
 use App\Service\TranslationService;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateTranslationsCommand extends Command
-{
+class UpdateTranslationsCommand extends Command {
+
     private $entityManager;
     private $translationService;
 
-    public function __construct(EntityManagerInterface $entityManager,
-                                TranslationService $translationService) {
+    public function __construct(EntityManagerInterface $entityManager, TranslationService $translationService) {
         parent::__construct();
         $this->entityManager = $entityManager;
         $this->translationService = $translationService;
     }
 
-    protected function configure()
-    {
+    protected function configure() {
 		$this->setName('app:update:translations');
 		$this->setDescription('This commands generate the yaml translations.');
         $this->setHelp('This command is supposed to be executed every night.');
     }
 
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     * @return int|void
-     * @throws Exception
-     */
     protected function execute(InputInterface $input, OutputInterface $output) {
         $this->translationService->generateTranslationsFile();
+        $output->writeln("Updated translation files");
     }
+
 }
