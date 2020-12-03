295bdd5c0276d4ccf33db804c5fc414a2934c8c8
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2020-12-03 17:15:57 +0100

    Fix cache clear command

--- src/Command/CacheClearCommand.php
+++ src/Command/CacheClearCommand.php
@@ -25,26 +25,24 @@ class CacheClearCommand extends SymfonyCacheClearCommand {
 
     protected function execute(InputInterface $input, OutputInterface $output): int {
         $name = $this->kernel->getProjectDir() . "/.env";
-        if(!file_exists($name)) {
-            return 0; //abort if .env doesn't exist
-        }
+        if(file_exists($name)) {
+            $env = file($name);
+            $now = time();
 
-        $env = file($name);
-        $now = time();
-
-        foreach($env as $line => $content) {
-            if(str_starts_with($content, "APP_LAST_CACHE_CLEAR")) {
-                $found = true;
-                $env[$line] = "APP_LAST_CACHE_CLEAR=$now\n";
-                break;
+            foreach($env as $line => $content) {
+                if(str_starts_with($content, "APP_LAST_CACHE_CLEAR")) {
+                    $found = true;
+                    $env[$line] = "APP_LAST_CACHE_CLEAR=$now\n";
+                    break;
+                }
             }
-        }
 
-        if(!isset($found)) {
-            $env[count($env)] = "APP_LAST_CACHE_CLEAR=$now\n";
-        }
+            if(!isset($found)) {
+                $env[count($env)] = "APP_LAST_CACHE_CLEAR=$now\n";
+            }
 
-        file_put_contents($name, $env);
+            file_put_contents($name, $env);
+        }
 
         return parent::execute($input, $output);
     }
