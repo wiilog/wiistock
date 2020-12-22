18fb39d87b8e50233c7ca2d14a898312892c3300
Adrien Bounader <adrien.bounader@wiilog.fr>
2020-12-22 10:19:12 +0100

    WIIS-3378: add version checking on each mobile authenticated requests + renaming Authenticated annotation to RestAuthenticated

---
+++ src/Annotation/RestVersionChecked.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target("METHOD")
+ */
+class RestVersionChecked {
+
+}
