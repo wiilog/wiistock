103a6b04734ff196ce1de06fc4a5e6e3fccc0769
t.gentile <thomas.gentile@wiilog.fr>
2021-01-13 14:46:30 +0100

    WIIS-3552: Monetary reliability
    - Move persist function into DashboardFeedCommand.php

--- src/Service/DashboardService.php
+++ src/Service/DashboardService.php
@@ -8,6 +8,7 @@ use App\Entity\ArrivalHistory;
 use App\Entity\Dashboard;
 use App\Entity\LocationCluster;
 use App\Entity\LocationClusterMeter;
+use App\Entity\MouvementStock;
 use App\Entity\Pack;
 use App\Entity\Dashboard\Meter as DashboardMeter;
 use App\Entity\DaysWorked;
@@ -695,6 +696,50 @@ class DashboardService {
 
         $meter
             ->setCount($count ?? 0);
+    }
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param Dashboard\Component $component
+     */
+    public function persistMonetaryReliability(EntityManagerInterface $entityManager,
+                                               Dashboard\Component $component): void {
+        $meter = $this->persistDashboardMeter($entityManager, $component, DashboardMeter\Chart::class);
+
+        $mouvementStockRepository = $entityManager->getRepository(MouvementStock::class);
+
+        $firstDayOfCurrentMonth = date("Y-m-d", strtotime("first day of this month"));
+        $lastDayOfCurrentMonth = date("Y-m-d", strtotime("last day of this month", strtotime($firstDayOfCurrentMonth)));
+        $precedentMonthFirst = $firstDayOfCurrentMonth;
+        $precedentMonthLast = $lastDayOfCurrentMonth;
+        $idx = 0;
+        $value = [];
+        $value['data'] = [];
+        while ($idx !== 6) {
+            $month = date("m", strtotime($precedentMonthFirst));
+            $month = date("F", mktime(0, 0, 0, $month, 10));
+            $totalEntryRefArticleOfPrecedentMonth = $mouvementStockRepository->countTotalEntryPriceRefArticle($precedentMonthFirst, $precedentMonthLast);
+            $totalExitRefArticleOfPrecedentMonth = $mouvementStockRepository->countTotalExitPriceRefArticle($precedentMonthFirst, $precedentMonthLast);
+            $totalRefArticleOfPrecedentMonth = $totalEntryRefArticleOfPrecedentMonth - $totalExitRefArticleOfPrecedentMonth;
+            $totalEntryArticleOfPrecedentMonth = $mouvementStockRepository->countTotalEntryPriceArticle($precedentMonthFirst, $precedentMonthLast);
+            $totalExitArticleOfPrecedentMonth = $mouvementStockRepository->countTotalExitPriceArticle($precedentMonthFirst, $precedentMonthLast);
+            $totalArticleOfPrecedentMonth = $totalEntryArticleOfPrecedentMonth - $totalExitArticleOfPrecedentMonth;
+
+            $nbrFiabiliteMonetaireOfPrecedentMonth = $totalRefArticleOfPrecedentMonth + $totalArticleOfPrecedentMonth;
+            $month = str_replace(
+                array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+                array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
+                $month
+            );
+            $value['data'][$month] = $nbrFiabiliteMonetaireOfPrecedentMonth;
+            $precedentMonthFirst = date("Y-m-d", strtotime("-1 month", strtotime($precedentMonthFirst)));
+            $precedentMonthLast = date("Y-m-d", strtotime("last day of -1 month", strtotime($precedentMonthLast)));
+            $idx += 1;
+        }
+        $values = array_reverse($value['data']);
+
+        $meter
+            ->setData($values);
     }
 
     /**
--- src/Service/DashboardSettingsService.php
+++ src/Service/DashboardSettingsService.php
@@ -456,41 +456,16 @@ class DashboardSettingsService {
                                                   array $config,
                                                   bool $example = false,
                                                   ?Dashboard\Meter\Chart $chart = null): array {
-        if (!$example) {
-            $mouvementStockRepository = $entityManager->getRepository(MouvementStock::class);
-
-            $firstDayOfCurrentMonth = date("Y-m-d", strtotime("first day of this month"));
-            $lastDayOfCurrentMonth = date("Y-m-d", strtotime("last day of this month", strtotime($firstDayOfCurrentMonth)));
-            $precedentMonthFirst = $firstDayOfCurrentMonth;
-            $precedentMonthLast = $lastDayOfCurrentMonth;
-            $idx = 0;
-            $value = [];
-            $value['data'] = [];
-            while ($idx !== 6) {
-                $month = date("m", strtotime($precedentMonthFirst));
-                $month = date("F", mktime(0, 0, 0, $month, 10));
-                $totalEntryRefArticleOfPrecedentMonth = $mouvementStockRepository->countTotalEntryPriceRefArticle($precedentMonthFirst, $precedentMonthLast);
-                $totalExitRefArticleOfPrecedentMonth = $mouvementStockRepository->countTotalExitPriceRefArticle($precedentMonthFirst, $precedentMonthLast);
-                $totalRefArticleOfPrecedentMonth = $totalEntryRefArticleOfPrecedentMonth - $totalExitRefArticleOfPrecedentMonth;
-                $totalEntryArticleOfPrecedentMonth = $mouvementStockRepository->countTotalEntryPriceArticle($precedentMonthFirst, $precedentMonthLast);
-                $totalExitArticleOfPrecedentMonth = $mouvementStockRepository->countTotalExitPriceArticle($precedentMonthFirst, $precedentMonthLast);
-                $totalArticleOfPrecedentMonth = $totalEntryArticleOfPrecedentMonth - $totalExitArticleOfPrecedentMonth;
-
-                $nbrFiabiliteMonetaireOfPrecedentMonth = $totalRefArticleOfPrecedentMonth + $totalArticleOfPrecedentMonth;
-                $month = str_replace(
-                    array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-                    array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'),
-                    $month
-                );
-                $value['data'][$month] = $nbrFiabiliteMonetaireOfPrecedentMonth;
-                $precedentMonthFirst = date("Y-m-d", strtotime("-1 month", strtotime($precedentMonthFirst)));
-                $precedentMonthLast = date("Y-m-d", strtotime("last day of -1 month", strtotime($precedentMonthLast)));
-                $idx += 1;
-            }
-            $values = ["chartData" => array_reverse($value['data'])];
-        } else {
+        if ($example) {
             $values = $componentType->getExampleValues();
+        } else {
+            $values = [
+                'chartData' => $chart
+                    ? $chart->getData()
+                    : []
+            ];
         }
+
         return $values;
     }
 
