8b228c20a57b468efcff1ccf6c62cb3538fb9727
c.roux <cedric.roux@wiilog.fr>
2021-01-19 14:45:16 +0100

    WIIS-3581 use streamResponse in InventoryEntry Export

--- src/Controller/InventoryEntryController.php
+++ src/Controller/InventoryEntryController.php
@@ -4,17 +4,21 @@
 namespace App\Controller;
 
 use App\Entity\Action;
+use App\Entity\InventoryEntry;
 use App\Entity\Menu;
 
 use App\Repository\InventoryEntryRepository;
 
+use App\Service\CSVExportService;
 use App\Service\InventoryEntryService;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -87,64 +91,87 @@ class InventoryEntryController extends AbstractController
 
     /**
      * @Route("/saisies-infos", name="get_entries_for_csv", options={"expose"=true}, methods={"GET","POST"})
+     * @param Request $request
+     * @param CSVExportService $CSVExportService
+     * @param EntityManagerInterface $entityManager
+     * @return Response
      */
-    public function getEntriesIntels(Request $request): Response
+    public function getEntriesIntels(Request $request,
+                                     CSVExportService $CSVExportService,
+                                     EntityManagerInterface $entityManager): Response
     {
-        if ($request->isXmlHttpRequest() && $data = json_decode($request->getContent(), true)) {
-			$dateMin = $data['dateMin'] . ' 00:00:00';
-			$dateMax = $data['dateMax'] . ' 23:59:59';
+        $dateMin = $request->query->get('dateMin');
+        $dateMax = $request->query->get('dateMax');
 
-			$dateTimeMin = DateTime::createFromFormat('d/m/Y H:i:s', $dateMin);
-			$dateTimeMax = DateTime::createFromFormat('d/m/Y H:i:s', $dateMax);
-
-            $entries = $this->inventoryEntryRepository->findByDates($dateTimeMin, $dateTimeMax);
-
-            $headers = [];
-            $headers = array_merge($headers, [
-                'Libellé',
-                'Référence',
-                'Code barre',
-                'Opérateur',
-                'Emplacement',
-                'Date de saisie',
-                'Quantité'
-            ]);
-
-            $data = [];
-            $data[] = $headers;
-
-            foreach ($entries as $entry) {
-                $article = $entry->getArticle();
-                $referenceArticle = $entry->getRefArticle();
-
-                if (!empty($referenceArticle)) {
-                    $articleLabel = $referenceArticle->getLibelle();
-                    $reference = $referenceArticle->getReference();
-                    $barCode = $referenceArticle->getBarCode();
-                }
-                else if (!empty($article)) {
-                    $articleLabel = $article->getLabel();
-                    $articleFournisseur = $article->getArticleFournisseur();
-                    $referenceArticle = $articleFournisseur ? $articleFournisseur->getReferenceArticle() : null;
-                    $reference = $referenceArticle ? $referenceArticle->getReference() : '';
-                    $barCode = $article->getBarCode();
-                }
-
-                $entryData = [];
-                $entryData[] = $articleLabel ?? '';
-                $entryData[] = $reference ?? '';
-                $entryData[] = $barCode ?? '';
-                $entryData[] = $entry->getOperator() ? $entry->getOperator()->getUsername() : '';
-                $entryData[] = $entry->getLocation() ? $entry->getLocation()->getLabel() : '';
-                $entryData[] = $entry->getDate() ? $entry->getDate()->format('d/m/Y') : '';
-                $entryData[] = $entry->getQuantity();
-
-                $data[] = $entryData;
-            }
-
-            return new JsonResponse($data);
-        } else {
-            throw new BadRequestHttpException();
+        try {
+            $dateTimeMin = DateTime::createFromFormat('Y-m-d H:i:s', $dateMin . ' 00:00:00');
+            $dateTimeMax = DateTime::createFromFormat('Y-m-d H:i:s', $dateMax . ' 23:59:59');
+        } catch (Throwable $throwable) {
         }
+        $headers = [
+            'Libellé',
+            'Référence',
+            'Code barre',
+            'Opérateur',
+            'Emplacement',
+            'Date de saisie',
+            'Quantité'
+        ];
+
+        $entriesRepository = $entityManager->getRepository(InventoryEntry::class);
+        $entries = $entriesRepository->findByDates($dateTimeMin, $dateTimeMax);
+ // TODO if !empty($dateTimeMin) && !empty($dateTimeMax)
+
+        return $CSVExportService->streamResponse(
+
+            function ($output) use ($entries, $CSVExportService) {
+                foreach ($entries as $entry) {
+                    $article = $entry->getArticle();
+                    $referenceArticle = $entry->getRefArticle();
+
+                    if (!empty($referenceArticle)) {
+                        $this->putReferenceArticleLine($output, $CSVExportService, $entry);
+                    } else if (!empty($article)) {
+                        $this->putArticleLine($output, $CSVExportService, $entry);
+                    }
+                }
+            },
+            'Export_Saisies_Inventaire.csv',
+            $headers
+        );
+        // TODO else
+        //            throw new NotFoundHttpException('404');
+    }
+
+    private function putReferenceArticleLine($handle,
+                                             CSVExportService $CSVExportService,
+                                             InventoryEntry $entry)
+    {
+        $referenceArticle = $referenceArticle = $entry->getRefArticle();
+        $dataEntry = [
+            $referenceArticle->getLibelle() ?? '',
+            $referenceArticle->getReference() ?? '',
+            $referenceArticle->getBarCode() ?? '',
+        ];
+
+        $data = array_merge($dataEntry, $entry->serialize());
+        $CSVExportService->putLine($handle, $data);
+    }
+
+    private function putArticleLine($handle,
+                                    CSVExportService $CSVExportService,
+                                    InventoryEntry $entry) {
+
+        $article = $entry->getArticle();
+        $articleFournisseur = $article->getArticleFournisseur();
+        $referenceArticle = $articleFournisseur ? $articleFournisseur->getReferenceArticle() : null;
+        $dataEntry = [
+            $article->getLabel(),
+            $referenceArticle ? $referenceArticle->getReference() : '',
+            $article->getBarCode(),
+        ];
+
+        $data = array_merge($dataEntry, $entry->serialize());
+        $CSVExportService->putLine($handle, $data);
     }
 }
