8de1cfa4296c42538a44177f0f5b2cc4bf8ce13d
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-01-28 15:16:50 +0100

    Cleaning

--- public/js/init-modal.js
+++ public/js/init-modal.js
@@ -23,6 +23,7 @@ function InitModal($modal, $submit, path, options = {}) {
     if(options.clearOnClose) {
         $modal.on('hidden.bs.modal', function () {
             clearModal($modal);
+            clearFormErrors();
         });
     }
 
