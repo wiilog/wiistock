223468224e6c018f7f377070f5b9965b4f22a257
t.gentile <thomas.gentile@wiilog.fr>
2021-02-11 17:02:26 +0100

    WIIS-3778: Order by title column visible

--- src/Service/VisibleColumnService.php
+++ src/Service/VisibleColumnService.php
@@ -4,8 +4,13 @@
 namespace App\Service;
 
 
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 class VisibleColumnService {
     public const FREE_FIELD_NAME_PREFIX = 'free_field';
+
+    /** @Required */
+    public TranslatorInterface $translator;
 
     public function getArrayConfig(array $fields,
                                    array $freeFields = [],
@@ -16,15 +21,18 @@ class VisibleColumnService {
                 function (array $column) use ($columnsVisible) {
                     $alwaysVisible = $column['alwaysVisible'] ?? false;
                     $visible = $alwaysVisible || in_array($column['name'], $columnsVisible);
+                    $translated = $column['translated'] ?? false;
+                    $title = $column['title'] ?? '';
                     return [
-                        'title' => $column['title'] ?? '',
+                        'title' => $title,
                         'hiddenTitle' => $column['hiddenTitle'] ?? '',
+                        'displayedTitle' => $translated ? $this->translator->trans($title) : $title,
                         'alwaysVisible' => $column['alwaysVisible'] ?? null,
                         'hiddenColumn' => $column['hiddenColumn'] ?? false,
                         'orderable' => $column['orderable'] ?? true,
                         'data' => $column['name'],
                         'name' => $column['name'],
-                        'translated' => $column['translated'] ?? false,
+                        'translated' => $translated,
                         'class' => $column['class'] ?? null,
                         'isColumnVisible' => $visible,
                         "type" => $column['type'] ?? null,
@@ -38,8 +46,10 @@ class VisibleColumnService {
                     $freeFieldName = $this->getFreeFieldName($freeField['id']);
                     $alwaysVisible = $column['alwaysVisible'] ?? null;
                     $visible = $alwaysVisible || in_array($freeFieldName, $columnsVisible);
+                    $title = ucfirst(mb_strtolower($freeField['label']));
                     return [
-                        "title" => ucfirst(mb_strtolower($freeField['label'])),
+                        "title" => $title,
+                        'displayedTitle' => $title,
                         "data" => $freeFieldName,
                         "name" => $freeFieldName,
                         "isColumnVisible" => $visible,
