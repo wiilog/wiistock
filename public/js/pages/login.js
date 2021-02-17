ecee77d456f8ae72188bd28d781d517c432370a2
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-02-17 16:45:59 +0100

    WIIS-3887

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,11 +1,30 @@
-$(document).ready(function() {
-    initImagePopovers();
+try {
+    $(document).ready(function () {
+        initImagePopovers();
 
-    let isChromium = !!window.chrome;
-    let isFirefox = navigator.userAgent.includes("Firefox");
+        let isChromium = !!window.chrome;
+        let isFirefox = navigator.userAgent.includes("Firefox");
 
-    if(!isChromium && !isFirefox) {
-        $('.messageChrome').addClass('d-block');
-        $('.form-signin').addClass('d-none');
+        if (!isChromium && !isFirefox) {
+            showCompliantBrowserMessage();
+        }
+    });
+}
+catch (ignored) {
+    showCompliantBrowserMessage();
+}
+
+function showCompliantBrowserMessage() {
+    const elementsToDisplay = document.getElementsByClassName('messageChrome');
+    const elementsToHide = document.getElementsByClassName('form-signin');
+    if (elementsToDisplay) {
+        for (let i = 0; i < elementsToDisplay.length; i++) {
+            elementsToDisplay[i].classList.add('d-block');
+        }
     }
-});
+    if (elementsToHide) {
+        for (let i = 0; i < elementsToHide.length; i++) {
+            elementsToHide[i].classList.add('d-none');
+        }
+    }
+}
