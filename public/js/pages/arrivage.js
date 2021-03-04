e32289632a34235142e8c3e139a0592f1f6acb81
m.hevin <matteo.hevin@wiilog.fr>
2021-03-04 16:31:14 +0100

    WIIS-4017

--- public/js/pages/arrivage.js
+++ public/js/pages/arrivage.js
@@ -9,7 +9,7 @@ $(function () {
     const $filtersContainer = $('.filters-container');
     initDateTimePicker('#dateMin, #dateMax, .date-cl');
     Select2.init($('#statut'), 'Statuts');
-    Select2.location($('#emplacement'), {}, 'Emplacements de déposes');
+    Select2.location($('#emplacement'), {}, 'Emplacement de dépose');
     Select2.init($filtersContainer.find('[name="carriers"]'), 'Transporteurs');
     initOnTheFlyCopies($('.copyOnTheFly'));
 
