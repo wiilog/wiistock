1d1f90097ecc0926e65131655c80579773180d86
Marwane BENOU-KAÏSS <marwane.benoukaiss@wiilog.fr>
2021-03-31 15:56:03 +0200

    Cache review

--- src/Service/CacheService.php
+++ src/Service/CacheService.php
@@ -2,12 +2,11 @@
 
 namespace App\Service;
 
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Component\Cache\CacheItem;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class CacheService {
-    private static array $CREATED = [];
+
+    private const CACHE_FOLDER = "/cache";
 
     public const PERMISSIONS = "permissions";
     public const IMPORTS = "imports";
@@ -15,45 +14,56 @@ class CacheService {
     /** @Required  */
     public KernelInterface $kernel;
 
-    public function delete(string $namespace, string $key): void {
-        $cache = $this->getCache($namespace);
-        $cache->deleteItem($key);
+    private function getCacheDirectory(): string {
+        return $this->kernel->getProjectDir() . self::CACHE_FOLDER;
     }
 
-    public function set(string $namespace, string $key, $value): void {
-        $cache = $this->getCache($namespace);
-        $item = $cache->getItem($key);
-        $this->setValue($cache, $item, $value);
-    }
-
-    public function get(string $namespace, string $key, ?callable $callable) {
-        $cache = $this->getCache($namespace);
-        $item = $cache->getItem($key);
-
-        $value = null;
-
-        if (!$item->isHit()) {
-            if ($callable) {
-                $value = $callable();
-                $this->setValue($cache, $item, $value);
-            }
-        }
-        else {
-            $value = $item->get();
+    public function get(string $namespace, $key, ?callable $callback = null) {
+        if($callback === null) {
+            $callback = $key;
+            $key = "";
+        } else {
+            $key = ".$key";
         }
 
-        return $value;
-    }
-
-    private function getCache(string $namespace): FilesystemAdapter {
-        if (!isset(self::$CREATED[$namespace])) {
-            self::$CREATED[$namespace] = new FilesystemAdapter($namespace, 0, $this->kernel->getCacheDir());
+        $cache = $this->getCacheDirectory();
+        if (!file_exists("$cache/$namespace$key")) {
+            $this->set($namespace, $callback());
         }
-        return self::$CREATED[$namespace];
+
+        return unserialize(file_get_contents("$cache/$namespace$key"));
     }
 
-    private function setValue(FilesystemAdapter $cache, CacheItem $item, $value) {
-        $item->set($value);
-        $cache->save($item);
+    public function set(string $namespace, $key, $value = null) {
+        if($value === null) {
+            $value = $key;
+            $key = "";
+        } else {
+            $key = ".$key";
+        }
+
+        $cache = $this->getCacheDirectory();
+        if (!is_dir($cache)) {
+            mkdir($cache);
+        }
+
+        $handle = fopen("$cache/$namespace$key", 'w+');
+        chmod("$cache/$namespace$key", 0777);
+        fwrite($handle, serialize($value));
+        fclose($handle);
     }
+
+    public function delete(string $namespace, ?string $key = null): void {
+        if($key) {
+            $key = ".$key";
+        }
+
+        $cache = $this->getCacheDirectory();
+        if (!is_dir($cache)) {
+            mkdir($cache);
+        }
+
+        unlink("$cache/$namespace.$key");
+    }
+
 }
