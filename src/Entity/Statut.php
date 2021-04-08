8f1a3ab7197fd457eb36951830cdc2112027ee8b
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-04-08 19:07:12 +0200

    WIIS-4354

--- src/Entity/Statut.php
+++ src/Entity/Statut.php
@@ -112,7 +112,7 @@ class Statut
     private $sendNotifToBuyer;
 
     /**
-     * @ORM\Column(type="boolean", nullable=true)
+     * @ORM\Column(type="boolean", nullable=true, options={"default": true})
      */
     private $commentNeeded;
 
