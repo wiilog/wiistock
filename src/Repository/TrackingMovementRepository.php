653a7bb8216247f6c7808cb55bba0915bdb5cfaf
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-05-20 15:13:18 +0200

    WIIS-4654: send to mobile only last tracking

--- src/Repository/TrackingMovementRepository.php
+++ src/Repository/TrackingMovementRepository.php
@@ -349,6 +349,7 @@ class TrackingMovementRepository extends EntityRepository
             ->leftJoin('join_pack.nature', 'join_pack_nature')
             ->leftJoin('tracking_movement.mouvementStock', 'join_stockMovement')
             ->leftJoin('tracking_movement.packParent', 'join_packParent')
+            ->innerJoin('tracking_movement.linkedPackLastTracking', 'linkedPackLastTracking') // check if it's the last tracking pick
             ->where('join_operator = :operator')
             ->andWhere('join_trackingType.nom LIKE :priseType')
             ->andWhere('tracking_movement.finished = :finished')
