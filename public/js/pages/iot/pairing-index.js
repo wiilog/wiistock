6d803cf984ae398e808ea5a6d46703ad2a4c48ac
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-06-24 16:30:52 +0200

    WIIS-4854

--- public/js/pages/iot/pairing-index.js
+++ public/js/pages/iot/pairing-index.js
@@ -62,22 +62,23 @@ function pairingList(search = '', filter = '', types = '', elements = '') {
                     const highTemperatureAlert = temperature > highTemperatureThreshold;
 
                     const $pairingContainer = `
-                    <div class="col-lg-3 col-md-4 col-12 pairing-container">
-                        <a class="card wii-card request-card pointer shadow-sm bg-white pairing-card" href="${Routing.generate('pairing_show', {pairing: pairing.id})}">
-                            <div class="d-flex sensor-details">
-                                <div class="type d-flex justify-content-center align-items-center ${lowTemperatureAlert ? 'low-temperature-bg' : highTemperatureAlert ? 'high-temperature-bg' : ''}">
-                                    <span class="wii-icon wii-icon-iot-${pairing.typeIcon} ${lowTemperatureAlert ? 'low-temperature-icon' : highTemperatureAlert ? 'high-temperature-icon' : ''}"></span>
+                        <div class="col-lg-3 col-md-4 col-12 pairing-container">
+                            <a class="card wii-card request-card pointer shadow-sm bg-white pairing-card" href="${Routing.generate('pairing_show', {pairing: pairing.id})}">
+                                <div class="d-flex sensor-details">
+                                    <div class="type d-flex justify-content-center align-items-center ${lowTemperatureAlert ? 'low-temperature-bg' : highTemperatureAlert ? 'high-temperature-bg' : ''}">
+                                        <span class="wii-icon wii-icon-iot-${pairing.typeIcon} ${lowTemperatureAlert ? 'low-temperature-icon' : highTemperatureAlert ? 'high-temperature-icon' : ''}"></span>
+                                    </div>
+                                    <div class="name col-10 d-flex justify-content-center align-items-center ${lowTemperatureAlert ? 'low-temperature-font' : highTemperatureAlert ? 'high-temperature-font' : ''}">
+                                        ${pairing.name}
+                                    </div>
                                 </div>
-                                <div class="name col-10 d-flex justify-content-center align-items-center ${lowTemperatureAlert ? 'low-temperature-font' : highTemperatureAlert ? 'high-temperature-font' : ''}">
-                                    ${pairing.name}
+                                <div class="element d-flex justify-content-center align-items-center">
+                                    <span class="wii-icon wii-icon-iot-${pairing.elementIcon} mr-2"></span>
+                                    ${pairing.element}
                                 </div>
-                            </div>
-                            <div class="element d-flex justify-content-center align-items-center">
-                                <span class="wii-icon wii-icon-iot-${pairing.elementIcon} mr-2"></span>
-                                ${pairing.element}
-                            </div>
-                        </a>
-                    </div>`;
+                            </a>
+                        </div>
+                    `;
 
                     $pairings.append($pairingContainer);
                     $pairings.find('.pairing-container').last().hide().fadeIn(600);
