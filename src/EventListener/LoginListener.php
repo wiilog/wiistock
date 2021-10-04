4b143b1a951f6b9b4db945199450437ac2aa1341
Adrien Bounader <adrien.bounader@wiilog.fr>
2021-10-04 10:24:39 +0200

    WIIS-5653: cleaning

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener;
 
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use App\Entity\Utilisateur;
@@ -13,15 +14,12 @@ class LoginListener
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
-// Get the User entity.
         /** @var Utilisateur $user */
         $user = $event->getAuthenticationToken()->getUser();
 
-// Update your field here.
-        $user->setLastLogin(new \DateTime());
-
-// Persist the data to database.
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        if ($user instanceof Utilisateur) {
+            $user->setLastLogin(new DateTime());
+            $this->entityManager->flush();
+        }
     }
 }
