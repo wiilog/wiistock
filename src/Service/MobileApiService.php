c2d9b44dab471a3aeee63875a03fb2acb9c7599d
Thomas Gentile <thomas.gentile@wiilog.fr>
2021-12-08 15:37:19 +0100

    WIIS-6294

--- src/Service/MobileApiService.php
+++ src/Service/MobileApiService.php
@@ -96,6 +96,7 @@ class MobileApiService {
             "skipValidationsManualTransfer" => $globalsParameters->getOneParamByLabel(ParametrageGlobal::MANUAL_TRANSFER_TO_TREAT_SKIP_VALIDATIONS),
             "skipValidationsLivraisons" => $globalsParameters->getOneParamByLabel(ParametrageGlobal::LIVRAISON_SKIP_VALIDATIONS),
             "skipQuantitiesLivraisons" => $globalsParameters->getOneParamByLabel(ParametrageGlobal::LIVRAISON_SKIP_QUANTITIES),
+            "displayTargetLocationPicking" => $globalsParameters->getOneParamByLabel(ParametrageGlobal::DISPLAY_PICKING_LOCATION),
         ];
     }
 }
