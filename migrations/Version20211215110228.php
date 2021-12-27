397a06e70e7da567d836856224cf96003ab3a30b
m.hevin <matteo.hevin@wiilog.fr>
2021-12-27 10:29:50 +0100

    WIIS-6548

--- migrations/Version20211215110228.php
+++ migrations/Version20211215110228.php
@@ -26,10 +26,13 @@ final class Version20211215110228 extends AbstractMigration
         // je recupere l'id de l'utilisateur Société GT
             $user = $this->connection->executeQuery("SELECT id FROM utilisateur WHERE username = 'Société GT'")->fetchOne();
             // j'attribue l'id de société GT à la colonne createdBy de toute les references
-            $this->addSql(
-            "UPDATE reference_article
-            SET reference_article.created_by_id = $user
-            WHERE 1");
+            if ($user) {
+                $this->addSql("
+                    UPDATE reference_article
+                    SET reference_article.created_by_id = $user
+                    WHERE 1
+                ");
+            }
         }
     }
 
