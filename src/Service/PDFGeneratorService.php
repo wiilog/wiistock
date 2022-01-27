a13ec823a4e49fef2b95e0e3b952455c7af00e08
Adrien Bounader <adrien.bounader@wiilog.fr>
2022-01-27 16:57:16 +0100

    WIIS-6762

--- src/Service/PDFGeneratorService.php
+++ src/Service/PDFGeneratorService.php
@@ -118,7 +118,7 @@ class PDFGeneratorService {
                 'dispatch' => $dispatch,
                 'logo' => $logo
             ]),
-            ($this->kernel->getProjectDir() . '/public/' . $fileName),
+            ($this->kernel->getProjectDir() . '/public/uploads/attachements/' . $fileName),
             [
                 'page-size' => "A4",
                 'encoding' => 'UTF-8'
