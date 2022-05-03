de235a5da21210e7e57d5188f7663b6cc3d84f66
Romain BAROT <romain.barot@wiilog.fr>
2022-05-03 11:30:48 +0200

    WIIS-6531_start

--- src/Controller/Transport/PlanningController.php
+++ src/Controller/Transport/PlanningController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Transport;
 
+use App\Entity\Transport\TransportRequest;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +16,17 @@ class PlanningController extends AbstractController {
      * Called in /index.html.twig
      */
     #[Route("/liste", name: "transport_planning_index", methods: "GET")]
-    public function index(): Response {
-        // TODO
+    public function index(EntityManagerInterface $manager,): Response {
+        $transportRequestRepository = $manager->getRepository(TransportRequest::class);
+
+        $transportRequests = $transportRequestRepository;
         return $this->render('transport/planning/index.html.twig');
+    }
+
+    #[Route("/api", name: "transport_planning_api", methods: "POST")]
+    public function api(): Response {
+
+        return [];
     }
 
 }
