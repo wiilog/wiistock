f68c4a0ec5e66e298a73c85325ca83e5965b2e17
Romain BAROT <romain.barot@wiilog.fr>
2022-05-04 12:21:10 +0200

    WIIS-6531

--- assets/js/pages/transport/order/planning.js
+++ assets/js/pages/transport/order/planning.js
@@ -1,4 +1,12 @@
 import '@styles/pages/transport/planning.scss';
+import {GET} from "@app/ajax";
 
 $(function () {
+    getOrders();
 })
+
+function getOrders(){
+    AJAX.route(GET,'transport_planning_api').json().then(({template})=>{
+        $('.planning-container').empty().append(template);
+    });
+}
--- src/Controller/Transport/PlanningController.php
+++ src/Controller/Transport/PlanningController.php
@@ -2,28 +2,77 @@
 
 namespace App\Controller\Transport;
 
+use App\Entity\Transport\TransportDeliveryRequest;
 use App\Entity\Transport\TransportOrder;
 use App\Entity\Transport\TransportRequest;
 use App\Entity\Transport\TransportRound;
+use App\Entity\Type;
+use App\Helper\FormatHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use WiiCommon\Helper\Stream;
 
 
 #[Route("transport/planning")]
-class PlanningController extends AbstractController {
+class PlanningController extends AbstractController
+{
 
     /**
      * Called in /index.html.twig
      */
     #[Route("/liste", name: "transport_planning_index", methods: "GET")]
-    public function index(EntityManagerInterface $manager,): Response {
+    public function index(): Response
+    {
+        return $this->render('transport/planning/index.html.twig');
+    }
+
+    #[Route("/api", name: "transport_planning_api", options: ['expose' => true], methods: "GET")]
+    public function api(Request $request, EntityManagerInterface $manager)
+    {
         $transportOrderRepository = $manager->getRepository(TransportOrder::class);
 
-        $transportOrders = $transportOrderRepository->findOrdersForPlanning();
-        return $this->render('transport/planning/index.html.twig', [
-            'transportOrders' => $transportOrders
+        $dateForContainer = (new \DateTime());
+        $date = (new \DateTime());
+        $transportOrders = $transportOrderRepository->findOrdersForPlanning($date, $dateForContainer->modify('+1 day'));
+
+       $orderedTransportOrders = [];
+       $deliveryAndCollectCount = [];
+       $countDeliveryAndCollect = 1;
+       $typesCount = [];
+       $countTypes = 1;
+
+       /** @var TransportOrder $transportOrder */
+        foreach ($transportOrders as $transportOrder){
+            $requestExpectedAt = $transportOrder->getRequest()->getExpectedAt()->format('Y-m-d');
+           if($requestExpectedAt === $date->format('Y-m-d')){
+               $orderedTransportOrders[$date->format('Y-m-d')][] = $transportOrder;
+               $deliveryAndCollectCount[$date->format('Y-m-d')][$transportOrder->getRequest() instanceof TransportDeliveryRequest ? 'delivery' : 'collect'] = $countDeliveryAndCollect;
+               $typesCount[$date->format('Y-m-d')][$transportOrder->getRequest() instanceof TransportDeliveryRequest ? 'delivery' : 'collect'][] =
+                    [$transportOrder->getRequest()->getType()->getLogo()->getFullPath(), $countTypes];
+           } else if ($requestExpectedAt === $dateForContainer->format('Y-m-d')){
+               $orderedTransportOrders[$dateForContainer->format('Y-m-d')][] = $transportOrder;
+               $deliveryAndCollectCount[$dateForContainer->format('Y-m-d')][$transportOrder->getRequest() instanceof TransportDeliveryRequest ? 'delivery' : 'collect'] = $countDeliveryAndCollect;
+               $typesCount[$dateForContainer->format('Y-m-d')][$transportOrder->getRequest() instanceof TransportDeliveryRequest ? 'delivery' : 'collect'][] =
+                   [$transportOrder->getRequest()->getType()->getLogo()->getFullPath(), $countTypes];
+           }
+            $countDeliveryAndCollect++;
+            $countTypes++;
+       }
+
+        dump($deliveryAndCollectCount);
+        dump($typesCount);
+
+        return $this->json([
+            'success' => true,
+            'template' => $this->renderView("transport/planning/planning_by_date_container.html.twig", [
+                'transportOrders' => $orderedTransportOrders,
+                'dateForContainer' => $date,
+                'deliveryAndCollectCount' => $deliveryAndCollectCount,
+                'typesCount' => $typesCount,
+            ]),
         ]);
     }
 }
