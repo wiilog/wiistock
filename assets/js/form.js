a2a11e61cf07424b835ab8f7ee227b7a7b089ee6
tgentile <thomas.gentile@wiilog.fr>
2022-05-13 14:13:29 +0200

    WIIS-7370

--- assets/js/form.js
+++ assets/js/form.js
@@ -300,7 +300,7 @@ function formatInputValue($input) {
         value = $input.val() || null;
     }
 
-    if (Array.isArray(value)) {
+    if ($input.parents('.free-field').exists() && Array.isArray(value)) {
         value = value.join(';');
     }
 
