707f6d784655410d23cad3e89aa9070032f0d94d
Roux <cedric.roux@wiilog.fr>
2022-05-20 12:19:57 +0200

    WIIS-7405

--- src/Service/RefArticleDataService.php
+++ src/Service/RefArticleDataService.php
@@ -376,7 +376,10 @@ class RefArticleDataService {
             $refArticle->setNeedsMobileSync(filter_var($data['mobileSync'] ?? false, FILTER_VALIDATE_BOOLEAN));
         }
 
-        $refArticle->setBuyer(isset($data['buyer']) ? $userRepository->find($data['buyer']) : null);
+        if (isset($data['buyer'])) {
+            $refArticle->setBuyer(!empty($data['buyer']) ? $userRepository->find($data['buyer']) : null);
+        }
+
         $refArticle->setLimitWarning((empty($data['limitWarning']) && $data['limitWarning'] !== 0 && $data['limitWarning'] !== '0') ? null : intval($data['limitWarning']));
         $refArticle->setLimitSecurity((empty($data['limitSecurity']) && $data['limitSecurity'] !== 0 && $data['limitSecurity'] !== '0') ? null : intval($data['limitSecurity']));
 
@@ -391,7 +394,9 @@ class RefArticleDataService {
             }
         }
 
-        $refArticle->setStockManagement($data['stockManagement'] ?? null);
+        if (isset($data['stockManagement'])) {
+            $refArticle->setStockManagement($data['stockManagement'] ?? null);
+        }
 
         if (isset($data['managers'])) {
             $refArticle->getManagers()->clear();
@@ -406,10 +411,13 @@ class RefArticleDataService {
             }
         }
 
-        if (isset($data["visibility-group"]) && $data["visibility-group"] !== 'null') {
-            $refArticle->setProperties(['visibilityGroup' => $data['visibility-group'] ? $visibilityGroupRepository->find(intval($data['visibility-group'])) : null]);
-        } else {
-            $refArticle->setProperties(['visibilityGroup' => null]);
+        if (isset($data["visibility-group"])) {
+            if ($data["visibility-group"] !== 'null') {
+                $refArticle->setProperties(['visibilityGroup' => $data['visibility-group'] ? $visibilityGroupRepository->find(intval($data['visibility-group'])) : null]);
+            }
+            else {
+                $refArticle->setProperties(['visibilityGroup' => null]);
+            }
         }
 
 
