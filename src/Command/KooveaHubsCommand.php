2dce1bb9843cc0b3935e3bc328a857b17c79c8fa
Matteo Hevin <matteo.hevin@wiilog.fr>
2022-06-01 15:01:14 +0200

    WIIS-7306

---
+++ src/Command/KooveaHubsCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\IOT\IOTService;
+use App\Service\TranslationService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class KooveaHubsCommand extends Command {
+
+    private $entityManager;
+    private $iotService;
+
+    public function __construct(EntityManagerInterface $entityManager, IOTService $iotService) {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+        $this->iotService = $iotService;
+    }
+
+    protected function configure() {
+		$this->setName('app:run:koovea:hub');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->iotService->runKooveaJOB($this->entityManager, IOTService::KOOVEA_HUB);
+        return 0;
+    }
+
+}
