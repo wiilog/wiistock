e331b39ea7c10d261289a54a990f238a8c8bd1e5
Adrien Bounader <adrien.bounader@wiilog.fr>
2022-06-08 17:23:39 +0200

    WIIS-7489

--- assets/js/planning.js
+++ assets/js/planning.js
@@ -1,25 +1,42 @@
 import AJAX, {GET} from "@app/ajax";
 import '@styles/planning.scss';
 
-const PLANNING_SELECTOR = 'wii-planning'
+const PLANNING_DATA = 'wii-planning';
+export const PLANNING_EVENT_LOADED = 'wii-planning-loaded';
 
 export default class Planning {
     $container;
 
-    constructor() {
-        this.$container = $(`[data-${PLANNING_SELECTOR}]`);
-        this.$container.data(`${PLANNING_SELECTOR}-instance`, this);
+    constructor($container, route) {
+        this.$container = $container;
+        this.route = route;
+
+        this.$container
+            .addClass(PLANNING_DATA)
+            .data(PLANNING_DATA, this);
 
         this.fetch();
     }
 
     fetch() {
-        const route = this.$container.data(PLANNING_SELECTOR);
-        AJAX.route(GET, route)
+        AJAX.route(GET, this.route)
             .json()
             .then(({template}) => {
-                this.$container.html(template);
-                this.$container.trigger('planning-loaded');
+                const $template = $(template);
+                const $planningContainer = this.$container.find('.planning-container');
+                if ($planningContainer.exists()) {
+                    $planningContainer.replaceWith($template);
+                }
+                else {
+                    this.$container.append($template);
+                }
+                this.$container.trigger(PLANNING_EVENT_LOADED);
             });
+    }
+
+    onPlanningLoad(callback) {
+        this.$container.on(PLANNING_EVENT_LOADED, function() {
+            callback(event, this)
+        });
     }
 }
