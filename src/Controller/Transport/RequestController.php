f9a196847cf900b87bf096d927243bab29e2551b
Roux <cedric.roux@wiilog.fr>
2022-06-23 14:07:39 +0200

    WIIS-7683

--- src/Controller/Transport/RequestController.php
+++ src/Controller/Transport/RequestController.php
@@ -36,6 +36,7 @@ use App\Service\MailerService;
 use App\Service\NotificationService;
 use App\Service\PDFGeneratorService;
 use App\Service\StatusHistoryService;
+use App\Service\StringService;
 use App\Service\Transport\TransportHistoryService;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -73,6 +74,7 @@ class RequestController extends AbstractController {
 
         $natures = $natureRepository->findByAllowedForms([Nature::TRANSPORT_COLLECT_CODE, Nature::TRANSPORT_DELIVERY_CODE]);
         $requestLines = Stream::from($natures)
+            ->sort(fn(Nature $a, Nature $b) => StringService::mbstrcmp($a->getLabel(), $b->getLabel()))
             ->map(fn(Nature $nature) => [
                 'nature' => $nature,
             ])
