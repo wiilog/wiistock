4130ed162b27e10c8288ce59315dc470c53ce013
mbenoukaiss <mbenoukaiss>
2022-06-29 16:41:28 +0200

    WIIS-7558

--- src/Service/InventoryService.php
+++ src/Service/InventoryService.php
@@ -12,6 +12,7 @@ use App\Exceptions\ArticleNotAvailableException;
 use App\Exceptions\RequestNeedToBeProcessedException;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Service\Attribute\Required;
 
 class InventoryService {
