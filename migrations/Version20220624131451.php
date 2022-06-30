6bfe2834b235e0feccdb4353ce2697c17d059715
Thomas Gentile <thomas.gentile@wiilog.fr>
2022-06-30 14:11:33 +0200

    Fix migration

--- migrations/Version20220624131451.php
+++ migrations/Version20220624131451.php
@@ -19,13 +19,15 @@ final class Version20220624131451 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        if(!$schema->getTable("transport_round_line")->hasColumn("rejected_at")) {
-            $this->addSql("ALTER TABLE transport_round_line ADD rejected_at DATETIME DEFAULT NULL, ADD failed_at DATETIME DEFAULT NULL;");
-        }
+        if ($schema->hasTable("transport_order") && $schema->getTable("transport_order")->hasColumn("rejected_at")) {
+            if (!$schema->getTable("transport_round_line")->hasColumn("rejected_at")) {
+                $this->addSql("ALTER TABLE transport_round_line ADD rejected_at DATETIME DEFAULT NULL, ADD failed_at DATETIME DEFAULT NULL;");
+            }
 
-        $this->addSql("UPDATE transport_round_line
+            $this->addSql("UPDATE transport_round_line
                             INNER JOIN transport_order ON transport_round_line.order_id = transport_order.id AND (transport_round_line.fulfilled_at OR transport_order.treated_at)
                             SET transport_round_line.rejected_at = transport_order.rejected_at, transport_round_line.failed_at = transport_order.failed_at");
+        }
     }
 
     public function down(Schema $schema): void
