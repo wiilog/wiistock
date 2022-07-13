f5192b882f877c9c36173894e76c67c8ee43c91e
Grégoire Dolidon <gregoire.dolidon@wiilog.fr>
2022-07-13 15:25:40 +0200

    WIIS-7765

--- src/Controller/Transport/RoundController.php
+++ src/Controller/Transport/RoundController.php
@@ -688,7 +688,7 @@ class RoundController extends AbstractController {
                 && $now === $transportRound->getExpectedAt()->format("d-m-Y")) {
                 $userChannel = $userService->getUserFCMChannel($deliverer);
                 $notificationService->send($userChannel, "Une nouvelle tournée attribuée aujourd'hui", null, [
-                    "roundId" => $transportRound->getId(),
+                    'type' => 'round',
                 ]);
             }
         }
