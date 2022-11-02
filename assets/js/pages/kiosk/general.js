199dc650245256bfcb4c0e591a9bb97010298399
Grégoire Dolidon <gregoire.dolidon@wiilog.fr>
2022-11-02 11:01:57 +0100

    WIIS-7948

--- assets/js/pages/kiosk/general.js
+++ assets/js/pages/kiosk/general.js
@@ -2,4 +2,14 @@ import '@styles/pages/kiosk/general.scss';
 
 $(document).ready(function() {
     console.log('Kiosk General Page Ready');
+
+    let modal = $("#modalDeTest");
+    let submit = $("#submit");
+    let url = '#';
+    InitModal(modal, submit, url, {});
+
+    $('#openModal').on('click', function() {
+        modal.modal('show');
+    });
+
 });
