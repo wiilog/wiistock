e48b18f27f95edc89dd868805a9dc1aeaa0418dc
Romain BAROT <romain.barot@wiilog.fr>
2022-11-03 17:09:45 +0100

    WIIS-7950

--- assets/js/pages/kiosk.js
+++ assets/js/pages/kiosk.js
@@ -1,5 +1,27 @@
 import '@styles/pages/kiosk.scss';
 
-$(function() {
+let scannedReference = '';
+const $referenceRefInput = $('.reference-ref-input');
+const $referenceLabelInput = $('.reference-label-input');
 
+$(function() {
+    $(document).on('keypress', function(event) {
+        if(event.originalEvent.key === 'Enter') {
+            window.location.href = Routing.generate('kiosk_form', {scannedReference: scannedReference});
+        } else {
+            scannedReference += event.originalEvent.key;
+        }
+    });
+
+    $referenceRefInput.on('keypress keyup', function(event) {
+        if(event.originalEvent.key === 'Backspace' && event.type === 'keyup') {
+            $referenceLabelInput.val($referenceLabelInput.val().slice(0,-1));
+        } else if(event.originalEvent.key !== 'Enter' && event.originalEvent.key !== 'Backspace' && event.type === 'keypress'){
+            $referenceLabelInput.val($referenceLabelInput.val()+event.originalEvent.key);
+        }
+    });
+
+    $('.button-next').on('click', function (){
+        console.log($('.entry-stock-container').children());
+    });
 });
--- src/Controller/Kiosk/KioskController.php
+++ src/Controller/Kiosk/KioskController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Kiosk;
 
 use App\Controller\AbstractController;
+use App\Entity\ReferenceArticle;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,8 +20,14 @@ class KioskController extends AbstractController
         return $this->render('kiosk/home.html.twig');
     }
 
-    #[Route("/formulaire", name: "kiosk_form")]
-    public function form(): Response {
-        return $this->render('kiosk/form.html.twig');
+    #[Route("/formulaire", name: "kiosk_form", options: ["expose" => true])]
+    public function form(Request $request, EntityManagerInterface $entityManager): Response {
+        $referenceArticleRepository = $entityManager->getRepository(ReferenceArticle::class);
+        $scannedReference = $request->query->get('scannedReference');
+
+        $reference = $referenceArticleRepository->findOneBy(['reference' => $scannedReference]) ?? new ReferenceArticle();
+        return $this->render('kiosk/form.html.twig', [
+            'reference' => $reference
+        ]);
     }
 }
