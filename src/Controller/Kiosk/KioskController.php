1513ec3444c2e441147d1015b33484ddaebee789
Grégoire Dolidon <gregoire.dolidon@wiilog.fr>
2022-11-03 12:44:04 +0100

    WIIS-7955

--- src/Controller/Kiosk/KioskController.php
+++ src/Controller/Kiosk/KioskController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Kiosk;
 
 use App\Controller\AbstractController;
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,15 @@ class KioskController extends AbstractController
     #[Route("/", name: "kiosk_index")]
     public function index(): Response {
         return $this->render('kiosk/pages/home.html.twig');
+    }
+
+    #[Route("/form", name: "kiosk_form")]
+    public function kioskForm(): Response {
+        $article =  new Article();
+        $article->setLabel('test');
+
+        return $this->render('kiosk/form.html.twig', [
+            'article' => $article
+        ]);
     }
 }
