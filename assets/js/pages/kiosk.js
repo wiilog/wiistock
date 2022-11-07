b34808c5fd3b237a2f0ea565a9abc45862a920c4
Romain BAROT <romain.barot@wiilog.fr>
2022-11-07 10:04:53 +0100

    WIIS-7950

--- assets/js/pages/kiosk.js
+++ assets/js/pages/kiosk.js
@@ -3,8 +3,11 @@ import '@styles/pages/kiosk.scss';
 let scannedReference = '';
 const $referenceRefInput = $('.reference-ref-input');
 const $referenceLabelInput = $('.reference-label-input');
+let $inputStockEntryData = $('input[name=stockEntryData]');
 
 $(function() {
+    Select2Old.user($('[name=applicant]'), '', 3);
+    Select2Old.user($('[name=follower]'), '', 3);
     $(document).on('keypress', function(event) {
         if(event.originalEvent.key === 'Enter') {
             window.location.href = Routing.generate('kiosk_form', {scannedReference: scannedReference});
@@ -22,6 +25,20 @@ $(function() {
     });
 
     $('.button-next').on('click', function (){
-        console.log($('.entry-stock-container').children());
+        const $current = $(this).closest('.entry-stock-container').find('.active');
+        const $timeline = $('.timeline-container');
+        const $currentTimelineEvent = $timeline.find('.current');
+        $currentTimelineEvent.removeClass('current');
+        $($currentTimelineEvent.next()[0]).addClass('current').removeClass('future');
+        $current.removeClass('active').addClass('d-none');
+        $($current.next()[0]).addClass('active').removeClass('d-none');
     });
+
+    $('.give-up-button').on('click', function() {
+        $("#modalGiveUpStockEntry").modal('show');
+    });
+
+    $('#submitGiveUpStockEntry').on('click', function() {
+        window.location.href = Routing.generate('kiosk_index', true);
+    })
 });
--- src/Controller/Kiosk/KioskController.php
+++ src/Controller/Kiosk/KioskController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Kiosk;
 
 use App\Controller\AbstractController;
+use App\Entity\FreeField;
 use App\Entity\ReferenceArticle;
+use App\Entity\Setting;
+use App\Entity\Type;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class KioskController extends AbstractController
 {
-    #[Route("/", name: "kiosk_index")]
+    #[Route("/", name: "kiosk_index", options: ["expose" => true])]
     public function index(): Response {
         return $this->render('kiosk/home.html.twig');
     }
@@ -23,11 +26,17 @@ class KioskController extends AbstractController
     #[Route("/formulaire", name: "kiosk_form", options: ["expose" => true])]
     public function form(Request $request, EntityManagerInterface $entityManager): Response {
         $referenceArticleRepository = $entityManager->getRepository(ReferenceArticle::class);
+        $settingRepository = $entityManager->getRepository(Setting::class);
+        $freeFieldRepository = $entityManager->getRepository(FreeField::class);
         $scannedReference = $request->query->get('scannedReference');
 
+        $freeField = $freeFieldRepository->find($settingRepository->getOneParamByLabel(Setting::FREE_FIELD_REFERENCE_CREATE));
         $reference = $referenceArticleRepository->findOneBy(['reference' => $scannedReference]) ?? new ReferenceArticle();
+
         return $this->render('kiosk/form.html.twig', [
-            'reference' => $reference
+            'reference' => $reference,
+            'scannedReference' => $scannedReference,
+            'freeField' => $freeField
         ]);
     }
 }
