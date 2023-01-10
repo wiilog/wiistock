160a1c609f784bc2dc803a287abf70a0d22a87e7
Thomas Gentile <thomas.gentile@wiilog.fr>
2023-01-10 15:14:04 +0100

    WIIS-8741: Review

--- data-warehouse/unite_logistique.sql
+++ data-warehouse/unite_logistique.sql
@@ -1,14 +1,13 @@
-SELECT
-    pack.code AS code_unite_logistique,
-    nature.label AS nature,
-    pack.quantity AS quantite,
-    project.code AS projet,
-    dispatch_pack.dispatch_id AS acheminement_id,
-    pack.arrivage_id AS arrivage_id,
-    (SELECT COUNT(article.id)
-     FROM article
-     WHERE article.current_logistic_unit_id = pack.id) AS nb_articles_contenus,
-    article.bar_code AS code_barre_article
+SELECT pack.code                                          AS code_unite_logistique,
+       nature.label                                       AS nature,
+       pack.quantity                                      AS quantite,
+       project.code                                       AS projet,
+       dispatch_pack.dispatch_id                          AS acheminement_id,
+       pack.arrivage_id                                   AS arrivage_id,
+       (SELECT COUNT(article.id)
+        FROM article
+        WHERE article.current_logistic_unit_id = pack.id) AS nb_articles_contenus,
+       article.bar_code                                   AS code_barre_article
 
 FROM pack
          LEFT JOIN article ON pack.id = article.current_logistic_unit_id
