cb510063621d3cf2eb78a8f07f838f0adbdb1190
Grégoire Dolidon <gregoire.dolidon@wiilog.fr>
2023-01-24 12:00:59 +0100

    WIIS-9013

--- src/Service/DispatchService.php
+++ src/Service/DispatchService.php
@@ -459,8 +459,8 @@ class DispatchService {
                 $receiverEmailUses = [];
                 $receiverEmailUses[] = $dispatch->getLocationFrom()->getEmail();
                 $receiverEmailUses[] = $dispatch->getLocationTo()->getEmail();
-                $receiverEmailUses[] = $signatory;
-                $receiverEmailUses = Stream::from($receiverEmailUses)->filter()->toArray();
+                $receiverEmailUses[] = $signatory->getEmail();
+                $receiverEmailUses = Stream::from($receiverEmailUses)->filter()->unique()->toArray();
                 // TODO WIIS-8832 ajouter les emails du nouveau champ sur les ache nomade
             }
 
