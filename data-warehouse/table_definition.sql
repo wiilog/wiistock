940e923cf4e13c5ec92e54e6af6802ddff452e0d
Clo <cloe.magnien@wiilog.fr>
2023-03-16 15:00:36 +0100

    WIIS-8873: table dw_arrivage_camion

--- data-warehouse/table_definition.sql
+++ data-warehouse/table_definition.sql
@@ -491,3 +491,18 @@ CREATE TABLE dw_article_champs_libres
     libelle            varchar(255),
     valeur             text
 );
+
+CREATE TABLE dw_arrivage_camion
+(
+    id                  integer,
+    no_arrivage_camion  varchar(255),
+    date_creation       timestamp(0),
+    transporteur        varchar(255),
+    chauffeur           varchar(255),
+    immatriculation     varchar(255),
+    emplacement         varchar(255),
+    operateur           varchar(255),
+    nb_tracking_total   integer,
+    reserve_general     varchar(255),
+    reserve_quantite    varchar(255)
+);
