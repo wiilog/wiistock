d7c19e8a7ee04f199346eb0f2475f300a11fd71d
Romain BAROT <romain.barot@wiilog.fr>
2023-03-17 10:21:38 +0100

    WIIS-9345

--- src/Controller/TransporteurController.php
+++ src/Controller/TransporteurController.php
@@ -99,6 +99,13 @@ class TransporteurController extends AbstractController
             $transporteur = new Transporteur();
         }
 
+        $countRecurrentCarrier = count($transporteurRepository->findBy(['recurrent' => true]));
+        if($countRecurrentCarrier === 10 && !$transporteur->isRecurrent()){
+            return new JsonResponse([
+                'success' => false,
+                'msg' => 'Vous avez déjà renseigné 10 transporteurs récurrents',
+            ]);
+        }
 		// unicité du code et du nom transporteur
 		$codeAlreadyUsed = intval($transporteurRepository->countByCode($code, $carrierId ? $transporteur : null));
 		$labelAlreadyUsed = intval($transporteurRepository->countByLabel($label, $carrierId ? $transporteur : null));
