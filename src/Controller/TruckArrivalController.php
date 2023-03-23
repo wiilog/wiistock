60a84ea25c8446cde8b0c93b8f4410ae10b11bc2
Remy Nowe <remy.nowe@wiilog.fr>
2023-03-23 16:41:03 +0100

    WIIS-8857: Modale ajout n°tracking

--- src/Controller/TruckArrivalController.php
+++ src/Controller/TruckArrivalController.php
@@ -64,10 +64,15 @@ class TruckArrivalController extends AbstractController
                 ->count()
             . '/'
             . $truckArrival->getTrackingLines()->count();
+        $carrier = $truckArrival->getCarrier();
+        $minTrackingNumber = $carrier->getMinTrackingNumberLength();
+        $maxTrackingNumber = $carrier->getMaxTrackingNumberLength();
 
         return $this->render('truck_arrival/show.html.twig', [
             'truckArrival' => $truckArrival,
             'lineAssociated' => $lineAssociated,
+            'minTrackingNumber' => $minTrackingNumber,
+            'maxTrackingNumber' => $maxTrackingNumber,
         ]);
     }
 
@@ -93,7 +98,7 @@ class TruckArrivalController extends AbstractController
     public function saveColumns(VisibleColumnService $visibleColumnService, Request $request, EntityManagerInterface $entityManager): Response {
         $data = json_decode($request->getContent(), true);
         $fields = array_keys($data);
-        $user  = $this->getUser();
+        $user = $this->getUser();
 
         $visibleColumnService->setVisibleColumns('truckArrival', $fields, $user);
 
@@ -243,4 +248,44 @@ class TruckArrivalController extends AbstractController
             'truckArrivalId' => $truckArrival->getId(),
         ]);
     }
+
+    // Reçois info POST du show.js
+    // Si le nouveau nombre existe déjà -> erreur
+    // Sinon -> enregistre en BDD le nombre et message succès
+    #[Route('/add-tracking-number', name: 'add_tracking_number', options: ['expose' => true], methods: 'POST', condition: 'request.isXmlHttpRequest()')]
+    public function addTrackingNumber(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $data = $request->request->all();
+        $truckArrivalLineRepository = $entityManager->getRepository(TruckArrivalLine::class);
+        $truckArrivalRepository = $entityManager->getRepository(TruckArrival::class);
+        $truckArrival = $truckArrivalRepository->find($request->request->get('truckArrival'));
+
+
+        foreach (explode(',', $data['trackingNumbers'] ?? '') as $trackingNumber) {
+            $truckArrivalLine = $truckArrivalLineRepository->findOneBy(['number' => $trackingNumber]);
+            if ($trackingNumber && !$truckArrivalLine) {
+
+                $line = (new TruckArrivalLine())
+                ->setNumber($trackingNumber);
+                $truckArrival->addTrackingLine($line);
+                $entityManager->persist($line);
+            } else {
+                return new JsonResponse([
+                    'success' => false,
+                    'trackingNumber' => $trackingNumber,
+                    'msg' => "Le numéro de tracking transporteur " . $truckArrivalLine->getNumber() . " existe déjà. Impossible de l'ajouter à cet arrivage camion. Veuillez en sélectionner un autre."
+                ]);
+            }
+        }
+
+        $entityManager->flush();
+
+
+        return new JsonResponse([
+            'success' => true,
+            'trackingNumber' => $trackingNumber,
+            'msg' => 'Le numéro de tracking transporteur a bien été ajouté.'
+        ]);
+    }
+
 }
