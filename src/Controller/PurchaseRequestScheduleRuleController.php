e64173bc6fc37d02cba3604b77135025c7b3c38e
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-05-05 15:05:58 +0200

    Revert "WIIS-9690"
    This reverts commit 08779eba6d268cbd49a8462016028b1eda988273.

--- src/Controller/PurchaseRequestScheduleRuleController.php
+++ src/Controller/PurchaseRequestScheduleRuleController.php
@@ -18,6 +18,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use WiiCommon\Helper\Stream;
--- src/Controller/ReserveController.php
+++ src/Controller/ReserveController.php
@@ -12,6 +12,7 @@ use App\Exceptions\FormException;
 use App\Repository\ReserveRepository;
 use App\Service\AttachmentService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
