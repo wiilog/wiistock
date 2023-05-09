9dd877fa3bb9533e77498cdc701e9250e6013e8e
Romain BAROT <romain.barot@wiilog.fr>
2023-05-09 17:29:57 +0200

    WIIS-9580: without application

--- public/js/script-wiilog.js
+++ public/js/script-wiilog.js
@@ -1257,3 +1257,70 @@ function registerEasterEgg() {
         count = 0;
     })
 }
+
+
+function loadAndDisplayLabels($select, name) {
+    const $form = $select.closest('.ligneFournisseurArticle');
+    const $codeSelect = $form.find(`[name=${name}]`);
+    if($select.val()) {
+        const [selected] = $select.select2('data');
+        if (selected) {
+            const {id, code} = selected;
+            const [codeSelectSelected] = $codeSelect.select2('data');
+            const selectCodeFournisseur = () => {
+                let option = new Option(code, id, true, true);
+                $codeSelect.append(option).trigger('change');
+            }
+            if (codeSelectSelected) {
+                const {id: codeSelectId} = codeSelectSelected;
+                if (id !== codeSelectId) {
+                    selectCodeFournisseur();
+                }
+            }
+            else {
+                selectCodeFournisseur();
+            }
+        }
+        else {
+            $codeSelect.val(null).trigger('change');
+        }
+    }
+}
+
+function loadAndDisplayInfos($select, name) {
+    const $form = $select.closest('.ligneFournisseurArticle');
+    const $nomSelect = $form.find(`[name=${name}]`);
+    if($select.val()) {
+        const [selected] = $select.select2('data');
+        if (selected) {
+            const {id, text} = selected;
+            const [nomSelectSelected] = $nomSelect.select2('data');
+            const selectNomFournisseur = () => {
+                let option = new Option(text, id, true, true);
+                $nomSelect.append(option).trigger('change');
+            }
+            if (nomSelectSelected) {
+                const {id: nomSelectId} = nomSelectSelected;
+                if (id !== nomSelectId) {
+                    selectNomFournisseur();
+                }
+            }
+            else {
+                selectNomFournisseur();
+            }
+        }
+    }
+    else {
+        $nomSelect.val(null).trigger('change');
+    }
+    let $modal = $select.closest('.modal');
+
+    $select.parent()
+        .siblings('.newContent')
+        .removeClass('d-none')
+        .addClass('d-block');
+
+    $modal.find('span[role="textbox"]').each(function () {
+        $(this).parent().css('border-color', '');
+    });
+}
