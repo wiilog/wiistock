4ad44f8c94f1f06e184db0636b73124c1bbf36cb
RobinPluviaux <robin.pluviaux@wiilog.fr>
2023-05-15 17:23:32 +0200

    WIIS-9590

--- assets/js/pages/shipping-request/index.js
+++ assets/js/pages/shipping-request/index.js
@@ -2,8 +2,6 @@ import {GET} from "@app/ajax";
 
 let tableShippings;
 
-global.validateShippingRequest = validateShippingRequest;
-global.openScheduledShippingRequestModal = openScheduledShippingRequestModal;
 
 $(function() {
     Select2Old.init($('.filters select[name="carriers"]'), 'Transporteurs');
@@ -75,36 +73,4 @@ function initTableShippings() {
 
         return initDataTable('tableShippings', tableShippingsConfig);
     }
-}
-
-function validateShippingRequest(shipping_request_id){
-    AJAX.route(`GET`, `shipping_request_validation`, {id:shipping_request_id})
-        .json()
-        .then((res) => {
-            if (res.success) {
-                location.reload()
-            }
-        });
-}
-
-function initScheduledShippingRequestForm(){
-    let $modalScheduledShippingRequest = $('#modalScheduledShippingRequest');
-    Form.create($modalScheduledShippingRequest).onSubmit((data, form) => {
-        $modalScheduledShippingRequest.modal('hide');
-        openPackingPack(data);
-    });
-}
-function openPackingPack(dataShippingRequestForm){
-    //todo WIIS-9591
-}
-
-function openScheduledShippingRequestModal($button){
-    const id = $button.data('id')
-    AJAX.route(`GET`, `check_expected_lines_data`, {id})
-        .json()
-        .then((res) => {
-            if (res.success) {
-                $('#modalScheduledShippingRequest').modal('show');
-            }
-        });
 }
--- assets/js/pages/shipping-request/show.js
+++ assets/js/pages/shipping-request/show.js
@@ -1,5 +1,10 @@
+global.validateShippingRequest = validateShippingRequest;
+global.openScheduledShippingRequestModal = openScheduledShippingRequestModal;
+
+
 $(function() {
     const shippingId = $('[name=shippingId]').val();
+    initScheduledShippingRequestForm();
 });
 
 function refreshTransportHeader(shippingId){
@@ -9,5 +14,36 @@ function refreshTransportHeader(shippingId){
         .json()
         .then(({detailsTransportConfig}) => {
             $('.transport-header').empty().append(detailsTransportConfig);
+        });
+}
+function validateShippingRequest(shipping_request_id){
+    AJAX.route(`GET`, `shipping_request_validation`, {id:shipping_request_id})
+        .json()
+        .then((res) => {
+            if (res.success) {
+                location.reload()
+            }
+        });
+}
+
+function initScheduledShippingRequestForm(){
+    let $modalScheduledShippingRequest = $('#modalScheduledShippingRequest');
+    Form.create($modalScheduledShippingRequest).onSubmit((data, form) => {
+        $modalScheduledShippingRequest.modal('hide');
+        openPackingPack(data);
+    });
+}
+function openPackingPack(dataShippingRequestForm){
+    //todo WIIS-9591
+}
+
+function openScheduledShippingRequestModal($button){
+    const id = $button.data('id')
+    AJAX.route(`GET`, `check_expected_lines_data`, {id})
+        .json()
+        .then((res) => {
+            if (res.success) {
+                $('#modalScheduledShippingRequest').modal('show');
+            }
         });
 }
