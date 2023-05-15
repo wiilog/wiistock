b73511f27f626cfb2aaf00feae120b0e13666e59
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-05-15 16:01:33 +0200

    WIIS-9578

--- assets/js/pages/shipping-request/form.js
+++ assets/js/pages/shipping-request/form.js
@@ -2,6 +2,7 @@ import {POST} from "@app/ajax";
 
 export function initModalFormShippingRequest($modal, submitRoute, onSuccess)  {
     const $requestersSelect = $modal.find('[name=requesters]')
+    const $customersSelect = $modal.find('[name=customerName]')
     const form = Form
         .create($modal)
         .submitTo(POST, submitRoute, {success: (data) => {
@@ -12,26 +13,23 @@ export function initModalFormShippingRequest($modal, submitRoute, onSuccess)  {
         .onOpen(()=>{
             // pre-filling phone select according to the applicant
             $requestersSelect.trigger('change');
-        });
-
-    $requestersSelect.on('change', () => {
-        const $requesterPhoneInput = $('[name=requesterPhoneNumbers]')
-        const requestersData = $requestersSelect.select2('data');
-        $requesterPhoneInput.find('[data-from-user=1]').remove();
-        Object.entries(requestersData).forEach(([key, value]) => {
-            const phone = value.phone || $(value.element).data('phone');
-            if (phone) {
-                $requesterPhoneInput.append($('<option>', {value: phone, 'data-from-user': 1, selected: true}).text(phone));
-            }
         })
-    });
-
-    // pre-filling customer information according to the customer
-    const $customersSelect = $modal.find('[name=customerName]')
-    $customersSelect.on('change', () => {
-        const customerData = $customersSelect.select2('data');
-        $modal.find('[name=customerPhone]').val(customerData[0]?.phoneNumber);
-        $modal.find('[name=customerRecipient]').val(customerData[0]?.recipient);
-        $modal.find('[name=customerAddress]').val(customerData[0]?.address);
-    });
+        .on('change', $requestersSelect, () => {
+            const $requesterPhoneInput = $('[name=requesterPhoneNumbers]')
+            const requestersData = $requestersSelect.select2('data');
+            $requesterPhoneInput.find('[data-from-user=1]').remove();
+            Object.entries(requestersData).forEach(([key, value]) => {
+                const phone = value.phone || $(value.element).data('phone');
+                if (phone) {
+                    $requesterPhoneInput.append($('<option>', {value: phone, 'data-from-user': 1, selected: true}).text(phone));
+                }
+            })
+        })
+        .on('change', $customersSelect, () => {
+            // pre-filling customer information according to the customer
+            const customerData = $customersSelect.select2('data');
+            $modal.find('[name=customerPhone]').val(customerData[0]?.phoneNumber);
+            $modal.find('[name=customerRecipient]').val(customerData[0]?.recipient);
+            $modal.find('[name=customerAddress]').val(customerData[0]?.address);
+        });
 }
