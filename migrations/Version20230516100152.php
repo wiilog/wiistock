2317f841d04fe94331d29b6de3d184908f6b8cb5
Clo <cloe.magnien@wiilog.fr>
2023-05-16 17:33:48 +0200

    WIIS-9594: fix migration

--- migrations/Version20230516100152.php
+++ migrations/Version20230516100152.php
@@ -19,7 +19,9 @@ final class Version20230516100152 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE shipping_request_utilisateur RENAME TO shipping_request_requester;');
+        if ($schema->hasTable('shipping_request_utilisateur')) {
+            $this->addSql('ALTER TABLE shipping_request_utilisateur RENAME TO shipping_request_requester;');
+        }
     }
 
     public function down(Schema $schema): void
