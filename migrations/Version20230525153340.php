c82bcce89ef793f5db733737d995e771b88d9217
Louise Delauney <louise.delauney@wiilog.fr>
2023-05-25 17:41:06 +0200

    WIIS-9596 : migration

---
+++ migrations/Version20230525153340.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230525153340 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        if ($schema->hasTable("shipping_request_expected_line")
+            && $schema->getTable("shipping_request_expected_line")->hasColumn("price")) {
+            $this->addSql('ALTER TABLE shipping_request_expected_line RENAME COLUMN price TO unit_price');
+        }
+
+        if ($schema->hasTable("shipping_request_expected_line")
+            && $schema->getTable("shipping_request_expected_line")->hasColumn("weight")) {
+            $this->addSql('ALTER TABLE shipping_request_expected_line RENAME COLUMN weight TO unit_weight');
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
