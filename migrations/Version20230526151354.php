fefe0321c27769992a6161366a427e373b2c0a59
adrbounaderwiilog <56256255+adrbounaderwiilog@users.noreply.github.com>
2023-05-29 17:30:23 +0200

    WIIS-9776

--- migrations/Version20230526151354.php
+++ migrations/Version20230526151354.php
@@ -20,10 +20,10 @@ final class Version20230526151354 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // rename into FieldsParam::FIELD_CODE_EMERGENCY_TYPE the the filed param where entity_code = FieldsParam::ENTITY_CODE_EMERGENCY and field code = "type"
-        $this->addSql('UPDATE fields_param SET field_code = :newCode WHERE entity_code = :entiyCode AND field_code = :oldCode', [
+        // rename into FieldsParam::FIELD_CODE_EMERGENCY_TYPE the filed param where entity_code = FieldsParam::ENTITY_CODE_EMERGENCY and field code = "type"
+        $this->addSql('UPDATE fields_param SET field_code = :newCode WHERE entity_code = :entityCode AND field_code = :oldCode', [
             'newCode' => FieldsParam::FIELD_CODE_EMERGENCY_TYPE,
-            'entiyCode' => FieldsParam::ENTITY_CODE_EMERGENCY,
+            'entityCode' => FieldsParam::ENTITY_CODE_EMERGENCY,
             'oldCode' => 'type'
         ]);
     }
