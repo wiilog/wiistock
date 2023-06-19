9af64bb8f0b38b3edb42a0e7fc5f4ee5e4db92a2
Matteo Hevin <matteo.hevin@wiilog.fr>
2023-06-19 09:50:58 +0200

    WIIS-9911

--- src/Controller/DispatchController.php
+++ src/Controller/DispatchController.php
@@ -484,7 +484,7 @@ class DispatchController extends AbstractController {
         $dispatchNoteAttachment = new Attachment();
         $dispatchNoteAttachment
             ->setDispatch($dispatch)
-            ->setFileName($dispatchNoteData['file'])
+            ->setFileName(uniqid() . '.pdf')
             ->setOriginalName($dispatchNoteData['name'] . '.pdf');
 
         $entityManager->persist($dispatchNoteAttachment);
@@ -1266,7 +1266,7 @@ class DispatchController extends AbstractController {
         $deliveryNoteAttachment = new Attachment();
         $deliveryNoteAttachment
             ->setDispatch($dispatch)
-            ->setFileName($deliveryNoteData['file'])
+            ->setFileName(uniqid() . '.pdf')
             ->setOriginalName($deliveryNoteData['name'] . '.pdf');
 
         $entityManager->persist($deliveryNoteAttachment);
