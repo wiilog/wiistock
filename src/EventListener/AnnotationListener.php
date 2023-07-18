6ed250772edea8e483c2c012fbfc1cf6ff7c5c16
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-07-18 12:13:04 +0200

    Fix nomade

--- src/EventListener/AnnotationListener.php
+++ src/EventListener/AnnotationListener.php
@@ -22,6 +22,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Service\Attribute\Required;
 use Twig\Environment;
@@ -110,7 +111,7 @@ class AnnotationListener {
         return null;
     }
 
-    private function handleRestAuthenticated(ControllerArgumentsEvent $event, AbstractController $controller) {
+    private function handleRestAuthenticated(ControllerArgumentsEvent $event, SymfonyAbstractController $controller) {
         $request = $event->getRequest();
 
         if (!method_exists($controller, "setUser")) {
