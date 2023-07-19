bf0219a7306365203fbb86499ba9557e38ee2071
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-07-19 15:28:25 +0200

    WIIS-10029

--- data-warehouse/iot/capteur_messages.sql
+++ data-warehouse/iot/capteur_messages.sql
@@ -1,10 +1,15 @@
 SELECT
     sensor.code             AS code_capteur,
     sensor_message.date     AS date_message,
-    sensor_message.event    AS type_message,
+    IF(sensor_message.content_type = 1, 'Température',
+       IF(sensor_message.content_type = 2, 'Hygrométrie',
+          IF(sensor_message.content_type = 3, 'Action',
+             IF(sensor_message.content_type = 4, 'GPS', NULL)
+              )
+           )
+        ) AS type_donnee,
     sensor_message.content  AS donnee_principale
 
 FROM sensor_message
 
          INNER JOIN sensor ON sensor_message.sensor_id = sensor.id
-
