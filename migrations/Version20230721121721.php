17625b2ea12bf14f5b9d9546b4d2ec2d0380eadf
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-07-21 15:23:57 +0200

    WIIS-10002

--- migrations/Version20230721121721.php
+++ migrations/Version20230721121721.php
@@ -24,7 +24,7 @@ final class Version20230721121721 extends AbstractMigration
         $this->addSql('CREATE TABLE reserve_type_utilisateur (reserve_type_id INT NOT NULL, utilisateur_id INT NOT NULL, INDEX IDX_A8CB84365973EA4D (reserve_type_id), INDEX IDX_A8CB8436FB88E14F (utilisateur_id), PRIMARY KEY(reserve_type_id, utilisateur_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reserve_type_utilisateur ADD CONSTRAINT FK_A8CB84365973EA4D FOREIGN KEY (reserve_type_id) REFERENCES reserve_type (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE reserve_type_utilisateur ADD CONSTRAINT FK_A8CB8436FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE reserve ADD reserve_type_id INT DEFAULT NULL, CHANGE type kind VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE reserve ADD reserve_type_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE reserve ADD CONSTRAINT FK_1FE0EA225973EA4D FOREIGN KEY (reserve_type_id) REFERENCES reserve_type (id)');
         $this->addSql('CREATE INDEX IDX_1FE0EA225973EA4D ON reserve (reserve_type_id)');
     }
