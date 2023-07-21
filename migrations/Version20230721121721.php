9a38998fdbb8fb04e25b14603a7038dc35767849
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-07-21 14:20:17 +0200

    WIIS-10002

---
+++ migrations/Version20230721121721.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230721121721 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE reserve_type (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, default_reserve_type TINYINT(1) DEFAULT NULL, active TINYINT(1) DEFAULT 1, UNIQUE INDEX UNIQ_B7802434EA750E8 (label), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reserve_type_utilisateur (reserve_type_id INT NOT NULL, utilisateur_id INT NOT NULL, INDEX IDX_A8CB84365973EA4D (reserve_type_id), INDEX IDX_A8CB8436FB88E14F (utilisateur_id), PRIMARY KEY(reserve_type_id, utilisateur_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reserve_type_utilisateur ADD CONSTRAINT FK_A8CB84365973EA4D FOREIGN KEY (reserve_type_id) REFERENCES reserve_type (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE reserve_type_utilisateur ADD CONSTRAINT FK_A8CB8436FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE reserve ADD reserve_type_id INT DEFAULT NULL, CHANGE type kind VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE reserve ADD CONSTRAINT FK_1FE0EA225973EA4D FOREIGN KEY (reserve_type_id) REFERENCES reserve_type (id)');
+        $this->addSql('CREATE INDEX IDX_1FE0EA225973EA4D ON reserve (reserve_type_id)');
+        $this->addSql('ALTER TABLE sensor_message ADD content_type INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+
+    }
+}
---
+++ migrations/Version20230721121728.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\Reserve;
+use App\Entity\ReserveType;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230721121728 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("INSERT INTO reserve_type (label, default_reserve_type, active) VALUES (:qualitylabel, 1, 1)", [
+            "qualitylabel" => ReserveType::DEFAULT_QUALITY_TYPE,
+        ]);
+
+        $reserves = $this->connection->executeQuery("
+                SELECT reserve.id
+                FROM reserve
+                WHERE kind = '" . Reserve::KIND_QUALITY . "'"
+        )->fetchAllAssociative();
+
+        foreach ($reserves as $reserve) {
+            $this->addSql("
+                UPDATE reserve
+                SET reserve_type_id = (
+                    SELECT reserve_type.id
+                        FROM reserve_type
+                        LIMIT 1
+                    )
+                WHERE id = {$reserve["id"]}
+            ");
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
