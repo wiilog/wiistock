04c2e9c1f9f44a47e76b106cb9cf0cfc8461623d
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-08-03 11:59:04 +0200

    WIIS-10148

--- public/js/pages/iot/data-monitoring.js
+++ public/js/pages/iot/data-monitoring.js
@@ -1,6 +1,7 @@
 let noMapData = true;
 let noChartData = true;
 const $errorContainer = $('.no-monitoring-data');
+const charts = {};
 $(document).ready(() => {
     initData();
 
@@ -222,6 +223,10 @@ function smartDisplayCoordinates(markers, map) {
 
 function initLineChart(element, callback) {
     const DEFAULT_DATATYPE = 'DEFAULT_DATATYPE';
+    const oldChart = charts[element]
+    if (oldChart){
+        oldChart.destroy();
+    }
 
     const $element = $(element);
     $errorContainer.addClass('d-none');
@@ -258,15 +263,20 @@ function initLineChart(element, callback) {
                             label: type === DEFAULT_DATATYPE ? sensor : `${sensor} - ${type}`,
                             yAxisID: type,
                             fill: false,
-                            data: [value],
+                            data: new Array( data.labels.length - 1).fill(null),
                             borderColor: (sensors[sensor]) instanceof Object ? sensors[sensor][type] : sensors[sensor],
                             tension: 0.1,
                         };
-                    } else {
-                        sensorMessagesDatasets[type+sensor].data.push(value);
                     }
+                    sensorMessagesDatasets[type+sensor].data.push(value);
                     dataTypes.push(type);
                 });
+            });
+            Object.keys(sensorMessagesDatasets).forEach((key) => {
+                const dataset = sensorMessagesDatasets[key];
+                while (dataset.data.length < data.labels.length) {
+                    dataset.data.push(null);
+                }
             });
         });
 
@@ -306,8 +316,6 @@ function initLineChart(element, callback) {
                 ... {ticks}
             };
         });
-
-        console.log(yAxes);
 
         if (needsline) {
             sensorMessagesDatasets['lineDataMax'] = {
@@ -367,6 +375,8 @@ function initLineChart(element, callback) {
             noChartData = true;
             callback();
         }
+
+        charts[element] = chart;
     });
 }
 
