65f3642a2356c4a0a541cab22fef87386b9251d1
Thomas Gentile <thomas.gentile@wiilog.fr>
2023-08-21 12:04:43 +0200

    WIIS-10165

---
+++ migrations/Version20230821095530.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\CategoryType;
+use App\Entity\Type;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230821095530 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $categoryId = $this->connection
+            ->executeQuery("SELECT id FROM category_type WHERE label = :label", [
+                "label" => CategoryType::TRANSFER_REQUEST,
+            ])->fetchOne();
+
+        dump($categoryId);
+        dump('helllllllo');
+
+        $this->addSql("UPDATE type SET notifications_enabled = 1 WHERE category_id = :categoryId AND label = :label", [
+            "categoryId" => $categoryId,
+            "label" => Type::LABEL_STANDARD,
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
