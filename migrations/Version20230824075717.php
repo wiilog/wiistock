c12035177fe3ffdd55e912959005e08e2a7c3c6f
Thomas Gentile <thomas.gentile@wiilog.fr>
2023-08-24 14:55:42 +0200

    WIIS-10125

--- migrations/Version20230824075717.php
+++ migrations/Version20230824075717.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use App\Entity\Setting;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -14,7 +15,9 @@ final class Version20230824075717 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        $this->addSql("UPDATE setting SET label = 'CLEAR_AND_KEEP_MODAL_AFTER_NEW_MVT' WHERE label = 'CLOSE_AND_CLEAR_AFTER_NEW_MVT'");
+        $this->addSql("UPDATE setting SET label = :label WHERE label = 'CLOSE_AND_CLEAR_AFTER_NEW_MVT'", [
+            'label' => Setting::CLEAR_AND_KEEP_MODAL_AFTER_NEW_MVT,
+        ]);
     }
 
     public function down(Schema $schema): void
