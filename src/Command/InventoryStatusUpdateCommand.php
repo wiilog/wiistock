b575d168fd8effeb985cdcdfe353fca434079346
GregoireDolidonWiilog <103416698+GregoireDolidonWiilog@users.noreply.github.com>
2023-08-31 15:47:31 +0200

    WIIS-6096

--- src/Command/InventoryStatusUpdateCommand.php
+++ src/Command/InventoryStatusUpdateCommand.php
@@ -6,22 +6,21 @@ namespace App\Command;
 
 use App\Entity\ReferenceArticle;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Contracts\Service\Attribute\Required;
 
+#[AsCommand(
+    name: 'app:update:inventory-status',
+    description: 'This commands updates the inventory dates on the refs',
+)]
 class InventoryStatusUpdateCommand extends Command
 {
-    protected static $defaultName = 'app:update:inventory-status';
 
     #[Required]
     public EntityManagerInterface $entityManager;
-
-    protected function configure()
-    {
-		$this->setDescription('This commands updates the inventory dates on the refs');
-    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int {
         $referenceArticleRepository = $this->entityManager->getRepository(ReferenceArticle::class);
