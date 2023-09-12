f95787ce8f3e01439a1ecf8c8ae9032d71e4fd61
adrbounaderwiilog <56256255+adrbounaderwiilog@users.noreply.github.com>
2023-09-12 16:52:33 +0200

    WIIS-10198: use migration to rename attachments directory

--- migrations/Version20230906134809.php
+++ migrations/Version20230906134809.php
@@ -6,12 +6,18 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230906134809 extends AbstractMigration
+final class Version20230906134809 extends AbstractMigration implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     public function getDescription(): string
     {
         return '';
@@ -23,6 +29,13 @@ final class Version20230906134809 extends AbstractMigration
         $this->addSql("UPDATE attachment SET full_path = REPLACE(full_path, 'attachements', 'attachments') WHERE full_path IS NOT NULL");
         $this->addSql("UPDATE setting SET value = REPLACE(value, 'attachements', 'attachments') WHERE value IS NOT NULL");
         $this->addSql("UPDATE language SET flag = REPLACE(flag, 'attachements', 'attachments')");
+
+        $projectDir = getcwd();
+        $currentAttachmentsDirectory = "{$projectDir}/public/uploads/attachements";
+        $newAttachmentsDirectory = "{$projectDir}/public/uploads/attachments";
+        if (file_exists($currentAttachmentsDirectory)) {
+            rename($currentAttachmentsDirectory, $newAttachmentsDirectory);
+        }
     }
 
     public function down(Schema $schema): void
