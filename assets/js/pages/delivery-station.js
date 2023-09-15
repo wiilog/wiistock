c2cb677c20d386d4366d7613dcd5a250e070c403
ThomasGentile <thomas.gentile@wiilog.fr>
2023-09-15 18:43:01 +0200

    WIIS-10250 & WIIS-10239 & WIIS-10244 & WIIS-10240 & WIIS-10248 & WIIS-10241 & WIIS-10242 & WIIS-10243 & WIIS-10245 & WIIS-10282

--- assets/js/pages/delivery-station.js
+++ assets/js/pages/delivery-station.js
@@ -1,18 +1,21 @@
 import '@styles/pages/kiosk.scss';
 import '@styles/pages/delivery-station.scss';
 
+let references = [];
+const $modalGeneric = $(`.modal-generic`);
+
 $(function () {
     $(`button.login`).on(`click`, function () {
         const mobileLoginKey = $(`[name=mobileLoginKey]`).val();
         const type = $(`[name=type]`).val();
         const visibilityGroup = $(`[name=visibilityGroup]`).val();
 
-        if(mobileLoginKey) {
+        if (mobileLoginKey) {
             wrapLoadingOnActionButton($(this), () => (
                 AJAX.route(AJAX.POST, `delivery_station_login`, {mobileLoginKey})
                     .json()
                     .then(({success}) => {
-                        if(success) {
+                        if (success) {
                             window.location.href = Routing.generate(`delivery_station_form`, {mobileLoginKey, type, visibilityGroup});
                         } else {
                             toggleRequiredMobileLoginKeyModal();
@@ -26,7 +29,6 @@ $(function () {
 
     $('.button-next').on('click', function () {
         const $current = $(this).closest('.stock-exit-container').find('.active');
-        const $buttonNextContainer = $(this).parent();
         const $timeline = $(`.timeline-container`);
         const $currentTimelineEvent = $timeline.find(`.current`);
         const $inputs = $current.find(`input[required]`);
@@ -48,61 +50,209 @@ $(function () {
             }
         });
 
-        if($current.find(`.invalid`).length === 0) {
-            if($current.is(`.reference-choice-container`)) {
-                //const reference = $(`[name=reference]`).val();
-                const reference = 7646;
+        toggleSummaryButtons($current);
 
-                wrapLoadingOnActionButton($(this), () => (
-                    AJAX.route(AJAX.GET, `delivery_station_get_reference_informations`, {reference})
-                        .json()
-                        .then(({values}) => {
-                            console.log(values);
-                            pushNextPage($current);
-                            updateTimeline($currentTimelineEvent);
-                            updateReferenceInformations(values);
-                        })
-                ));
+        if ($current.find(`.invalid`).length === 0) {
+            if ($current.is(`.reference-choice-container`)) {
+                //const reference = $(`[name=reference]`).val();
+                const reference = $current.find(`[name=reference]`).val();
+
+                if(reference) {
+                    wrapLoadingOnActionButton($(this), () => (
+                        AJAX.route(AJAX.GET, `delivery_station_get_informations`, {reference})
+                            .json()
+                            .then(({values}) => {
+                                references.push(values);
+                                pushNextPage($current);
+                                updateTimeline($currentTimelineEvent);
+                                updateReferenceInformations();
+                            })
+                    ));
+                } else {
+                    showGenericModal(`Vous devez sélectionner une référence pour pouvoir continuer.`);
+                }
+
+            }
+
+            if ($current.is(`.quantity-choice-container`)) {
+                const barcode = $current.find(`[name=barcode]`).val();
+                const pickedQuantity = $current.find(`[name=pickedQuantity]`).val();
+
+                if(barcode && pickedQuantity !== "") {
+                    wrapLoadingOnActionButton($(this), () => (
+                        AJAX.route(AJAX.GET, `delivery_station_get_free_fields`)
+                            .json()
+                            .then(({template}) => {
+                                $current.find(`.suppliers ~ span`).removeClass(`d-none`); // todo vérifier pourquoi ça ne cache pas le message
+
+                                if(references.length === 1) {
+                                    pushNextPage($current);
+                                    updateTimeline($currentTimelineEvent);
+
+                                    template = template || `<div class="text-center">Il n'y a aucun champ libre pour ce type de demande de livraison.</div>`;
+                                    $(`.other-informations-container`).html(template);
+                                } else {
+                                    $current
+                                        .addClass(`d-none`)
+                                        .removeClass(`active`);
+
+                                    $(`.summary-container`)
+                                        .removeClass(`d-none`)
+                                        .addClass(`active`);
+                                }
+                            })
+                    ));
+                } else if(!barcode) {
+                    showGenericModal(`Vous devez renseigner un code barre pour continuer.`);
+                } else if(!pickedQuantity) {
+                    showGenericModal(`Vous devez renseigner une quantité prise pour continuer.`);
+                }
+            }
+
+            if ($current.is(`.other-informations-container`)) {
+                let values = {};
+                $current
+                    .find(`.free-field`)
+                    .each(function (index, freeField) {
+                        values[$(freeField).find(`.field-label`).text().trim()] = $(freeField).find(`input, select`).val();
+                    });
+
+                pushNextPage($current);
+                updateTimeline($currentTimelineEvent);
+                updateSummaryTable();
+                toggleSummaryButtons($current);
+
+                const $freeFieldsContainer = $(`.summary-container`).find(`.free-fields-container`);
+                $freeFieldsContainer.empty();
+                for (const [label, value] of Object.entries(values)) {
+                    renderFreeField($freeFieldsContainer, label, value);
+                }
             }
         }
     });
 
-    $('.return-or-give-up-button').on('click', function () {
-        const $current = $('.active')
-        const $timeline = $('.timeline-container');
-        const $currentTimelineEvent = $timeline.find('.current');
-        const $modalGiveUpStockExit = $(`.modal-give-up-stock-exit`);
+    $(`.validate-stock-exit-button`).on(`click`, function () {
+        const $current = $(this).closest('.stock-exit-container').find('.active');
 
-        if (!$current.prev().first().is(`body`)) {
-            $currentTimelineEvent.addClass('future').removeClass('current');
-            $($currentTimelineEvent.prev()[0]).addClass('current').removeClass('future');
-            $current.removeClass('active').addClass('d-none');
-            $($current.prev()[0]).addClass('active').removeClass('d-none');
-        } else {
-            $modalGiveUpStockExit.modal('show');
-            $modalGiveUpStockExit.find('.bookmark-icon').removeClass('d-none');
-        }
+        wrapLoadingOnActionButton($(this), () => (
+            AJAX.route(AJAX.POST, `delivery_station_submit_request`, references)
+                .json()
+                .then(({success}) => {
+                    if (success) {
+                        pushNextPage($current);
+                        updateTimeline(undefined, true);
+                    }
+                })
+        ));
+    });
+
+    $('.return-or-give-up-button, .edit-stock-exit-button').on('click', function () {
+        backPreviousPage();
+    });
+
+    $(`.give-up-button`).on(`click`, () => {
+        // todo rediriger vers le login
+        let list = [];
+        references.forEach(({reference, location}) => list.push(`<li>${reference} à son emplacement d'origine ${location}</li>`));
+
+        const message = `
+            <div>
+                Voulez-vous abandonner cette demande de livraison ?<br>
+                Si oui, pensez à ranger les références et les articles:
+                <ul>
+                    ${list}
+                </ul>
+            </div>
+        `;
+
+        showGenericModal(message);
     });
 
     $(`#submitGiveUpStockExit`).on('click', () => {
         window.location.href = Routing.generate('delivery_station_index', true);
     });
-});
 
+    $(`.quantity-choice-container [name=barcode]`).on(`focusout`, function () {
+        const barcode = $(this).val();
+        const reference = references.slice(-1)[0]?.id;
+
+        if(barcode !== ``) {
+            wrapLoadingOnActionButton($(`body`), () => (
+                AJAX.route(AJAX.GET, `delivery_station_get_informations`, {reference, barcode})
+                    .json()
+                    .then(({success, msg, values}) => {
+                        if(success) {
+                            const $quantityChoiceContainer = $(`.quantity-choice-container`);
+                            const {location, suppliers} = values;
+
+                            const orderedSuppliers = suppliers
+                                .split(`,`)
+                                .reduce((acc, supplier) => acc + `<li>${supplier}</li>`, ``);
+
+                            $quantityChoiceContainer.find(`.location`).text(location);
+                            $quantityChoiceContainer.find(`.suppliers ~ span`).addClass(`d-none`);
+                            $quantityChoiceContainer.find(`.suppliers`).html(orderedSuppliers);
+                        } else {
+                            showGenericModal(msg);
+                        }
+                    })
+            ));
+        }
+    });
+
+    const $modalDeleteLine = $(`.modal-delete-line`);
+    $(document).arrive('.delete-line', function () {
+        $(this).on(`click`, function () {
+            const lineIndex = $(this).data(`line-index`);
+            $modalDeleteLine.find(`.submit`).attr(`data-line-index`, lineIndex);
+
+            if (references.length === 1) {
+                $modalDeleteLine.find(`.last-reference`).removeClass(`d-none`);
+            }
+
+            $modalDeleteLine.modal(`show`);
+        });
+    });
+
+    $modalDeleteLine.find(`.submit`).on(`click`, function () {
+        const lineIndex = $(this).data(`line-index`);
+        references.splice(lineIndex, 1);
+        $(`.summary-container tbody tr`)[lineIndex]
+            .closest(`tr`)
+            .remove();
+
+        if (references.length === 0) {
+            window.location.href = Routing.generate('delivery_station_form', true);
+        } else {
+            Flash.add(Flash.SUCCESS, `La référence a bien été supprimée de la demande`);
+        }
+    });
+
+    $(`.add-reference-container`).on(`click`, () => {
+        Array(3).fill(0).forEach(() => backPreviousPage());
+    });
+
+    $modalGeneric.on(`hidden.bs.modal`, function () {
+        $(this).find(`.error-message`).empty();
+    });
+});
 
 
 function toggleRequiredMobileLoginKeyModal() {
     $(`.modal-required-mobile-login-key`).modal(`show`);
 }
 
-function updateTimeline($currentTimelineEvent) {
-    $currentTimelineEvent
-        .removeClass(`current`);
+function updateTimeline($currentTimelineEvent = undefined, hide = false) {
+    $(`.timeline-container`).toggleClass(`d-none`, hide);
+    if (!hide) {
+        $currentTimelineEvent
+            .removeClass(`current`);
 
-    $currentTimelineEvent
-        .next()
-        .removeClass(`future`)
-        .addClass(`current`);
+        $currentTimelineEvent
+            .next()
+            .removeClass(`future`)
+            .addClass(`current`);
+    }
 }
 
 function pushNextPage($current) {
@@ -116,15 +266,112 @@ function pushNextPage($current) {
         .addClass(`active`);
 }
 
-function updateReferenceInformations(values) {
+function backPreviousPage() {
+    const $current = $('.active')
+    const $timeline = $('.timeline-container');
+    const $currentTimelineEvent = $timeline.find('.current');
+    const $modalGiveUpStockExit = $(`.modal-give-up-stock-exit`);
+
+    toggleSummaryButtons();
+
+    if ($current.prev().exists() && !$current.prev().first().is(`body`)) {
+        $currentTimelineEvent.addClass('future').removeClass('current');
+        $($currentTimelineEvent.prev()[0]).addClass('current').removeClass('future');
+        $current.removeClass('active').addClass('d-none');
+        $($current.prev()[0]).addClass('active').removeClass('d-none');
+    } else {
+        $modalGiveUpStockExit.modal('show');
+        $modalGiveUpStockExit.find('.bookmark-icon').removeClass('d-none');
+    }
+}
+
+function updateReferenceInformations() {
     const $referenceInformations = $(`.reference-informations`);
-    for (const [index, value] of Object.entries(values)) {
-        if(index === `image` && value) {
-            $referenceInformations.find(`.${index}`).attr(`src`, value).removeAttr(`style`);
+    const reference = references.slice(-1)[0];
+    for (const [index, value] of Object.entries(reference)) {
+        if (index === `image`) {
+            if(value) {
+                $referenceInformations
+                    .find(`.${index}`)
+                    .attr(`src`, value)
+                    .removeClass(`default-image`);
+            } else {
+                $referenceInformations
+                    .find(`.${index}`)
+                    .attr(`src`, ``)
+                    .addClass(`default-image`);
+            }
         } else {
-            $referenceInformations.find(`.${index}`).text(value);
+            $referenceInformations.find(`.${index}`).text(value || '-');
         }
     }
 
-    $(`.quantity-choice-container .location`).text(values.location);
+    $(`.quantity-choice-container .location`).text(reference.location);
+}
+
+function updateSummaryTable() {
+    const $summaryContainer = $(`.summary-container`);
+    $summaryContainer.find(`tbody`).empty();
+    references.forEach((reference, lineIndex) => {
+        renderReferenceLine($summaryContainer, lineIndex, reference);
+    });
+}
+
+function renderFreeField($freeFieldsContainer, label, value) {
+    const $wrapper = `
+        <div class="col-4 d-flex align-items-center mt-5">
+            <span class="wii-icon wii-icon-document wii-icon-40px-primary mr-2"></span>
+            <div class="d-flex flex-column">
+                <span class="wii-field-name">${label}</span>
+                <span class="wii-field-text">${value || '-'}</span>
+            </div>
+        </div>
+    `;
+
+    $freeFieldsContainer.append($wrapper);
+}
+
+function renderReferenceLine($summaryContainer, lineIndex, reference) {
+    let line = `
+        <tr>
+            <td class="wii-body-text">@reference</td>
+            <td class="wii-body-text">@label</td>
+            <td class="wii-body-text">@barcode</td>
+            <td class="wii-body-text">@stockQuantity</td>
+            <td class="wii-body-text">
+                <i class="wii-icon wii-icon-trash wii-icon-25px-danger delete-line" data-line-index="${lineIndex}"></i>
+            </td>
+        </tr>
+    `;
+
+    for (const [index, value] of Object.entries(reference)) {
+        line = line.replace(`@${index}`, value);
+    }
+
+    $summaryContainer.find(`tbody`).append(line);
+}
+
+function toggleSummaryButtons() {
+    const $current = $('.stock-exit-container').find('.active');
+    const $stockExitContainer = $(`.stock-exit-container`);
+    const $giveUpButtonContainer = $(`.give-up-button-container`);
+
+    if ($current.is(`.summary-container`)) {
+        $giveUpButtonContainer.find(`.give-up-button`).removeClass(`d-none`);
+        $giveUpButtonContainer.find(`.return-or-give-up-button`).addClass(`d-none`);
+        $stockExitContainer.find(`.edit-stock-exit-button`).removeClass(`d-none`);
+        $stockExitContainer.find(`.button-next`).addClass(`d-none`);
+        $stockExitContainer.find(`.validate-stock-exit-button`).removeClass(`d-none`);
+    } else {
+        $giveUpButtonContainer.find(`.give-up-button`).addClass(`d-none`);
+        $giveUpButtonContainer.find(`.return-or-give-up-button`).removeClass(`d-none`);
+        $stockExitContainer.find(`.edit-stock-exit-button`).addClass(`d-none`);
+        $stockExitContainer.find(`.button-next`).removeClass(`d-none`);
+        $stockExitContainer.find(`.validate-stock-exit-button`).addClass(`d-none`);
+    }
+}
+
+function showGenericModal(message) {
+    $modalGeneric.find(`.error-message`).text(message);
+    $modalGeneric.modal(`show`);
 }
--- src/Controller/DeliveryStation/DeliveryStationController.php
+++ src/Controller/DeliveryStation/DeliveryStationController.php
@@ -4,7 +4,10 @@ namespace App\Controller\DeliveryStation;
 
 use App\Annotation\HasValidToken;
 use App\Controller\AbstractController;
+use App\Entity\ArticleFournisseur;
+use App\Entity\CategorieCL;
 use App\Entity\CategoryType;
+use App\Entity\DeliveryRequest\Demande;
 use App\Entity\FreeField;
 use App\Entity\KioskToken;
 use App\Entity\ReferenceArticle;
@@ -13,7 +16,9 @@ use App\Entity\Type;
 use App\Entity\Utilisateur;
 use App\Entity\VisibilityGroup;
 use App\Service\AttachmentService;
+use App\Service\DeliveryRequestService;
 use App\Service\Kiosk\KioskService;
+use App\Service\LivraisonService;
 use DateInterval;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
@@ -23,6 +28,7 @@ use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 use WiiCommon\Helper\Stream;
 
 
@@ -49,7 +55,8 @@ class DeliveryStationController extends AbstractController
     }
 
     #[Route("/login/{mobileLoginKey}", name: "delivery_station_login", options: ["expose" => true], methods: "POST")]
-    public function login(string $mobileLoginKey, EntityManagerInterface $entityManager): JsonResponse {
+    public function login(string $mobileLoginKey, EntityManagerInterface $entityManager): JsonResponse
+    {
         $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['mobileLoginKey' => $mobileLoginKey]);
         return $this->json([
             'success' => !!$user,
@@ -72,25 +79,121 @@ class DeliveryStationController extends AbstractController
         ]);
     }
 
-    #[Route("/get-reference-informations", name: "delivery_station_get_reference_informations", options: ["expose" => true], methods: "GET")]
-    public function getReferenceInformations(Request $request,
+    #[Route("/get-informations", name: "delivery_station_get_informations", options: ["expose" => true], methods: "GET")]
+    public function getReferenceInformations(Request                $request,
                                              EntityManagerInterface $entityManager): JsonResponse
     {
         $referenceArticleRepository = $entityManager->getRepository(ReferenceArticle::class);
-        $reference = $referenceArticleRepository->find($request->query->get('reference'));
+        $initialReference = $referenceArticleRepository->find($request->query->get('reference'));
+        $barcode = $request->query->has('barcode')
+            ? $request->query->get('barcode')
+            : null;
 
+        if ($barcode) {
+            if (str_starts_with($barcode, Article::BARCODE_PREFIX)) {
+                $article = $entityManager->getRepository(Article::class)->findOneBy(['barCode' => $barcode]);
+
+                if($article->isAvailable()) {
+                    if($article->getReferenceArticle()->getId() === $initialReference->getId()) {
+                        $values = [
+                            'location' => $this->formatService->location($article->getEmplacement()),
+                            'suppliers' => $article->getArticleFournisseur()?->getFournisseur()?->getCodeReference() ?: '-',
+                        ];
+                    } else {
+                        return $this->json([
+                            'success' => false,
+                            'msg' => "L'article renseigné n'est pas lié à la référence sélectionée.",
+                        ]);
+                    }
+                } else {
+                    return $this->json([
+                        'success' => false,
+                        'msg' => "L'article sélectionné n'est pas disponible.",
+                    ]);
+                }
+            } elseif (str_starts_with($barcode, ReferenceArticle::BARCODE_PREFIX)) {
+                $reference = $referenceArticleRepository->findOneBy(['barCode' => $barcode]);
+
+                if($reference->getId() === $initialReference->getId()) {
+                    $values = [
+                        'location' => $this->formatService->location($reference->getEmplacement()),
+                        'suppliers' => Stream::from($reference->getArticlesFournisseur())
+                            ->map(static fn(ArticleFournisseur $supplierArticle) => $supplierArticle->getFournisseur()->getCodeReference())
+                            ->join(','),
+                    ];
+                } else {
+                    return $this->json([
+                        'success' => false,
+                        'msg' => "La référence renseignée doit être identique à celle sélectionnée au début du processus.",
+                    ]);
+                }
+
+            } else {
+                return $this->json([
+                    'success' => false,
+                    'msg' => "Le code barre renseigné ne correspond à aucun article ou référence.",
+                ]);
+            }
+        } else {
+            $values = [
+                'id' => $initialReference->getId(),
+                'reference' => $initialReference->getReference(),
+                'label' => $initialReference->getLibelle(),
+                'stockQuantity' => $initialReference->getQuantiteDisponible(),
+                'barcode' => $initialReference->getBarCode(),
+                'image' => $initialReference->getImage()
+                    ? "{$initialReference->getImage()->getFullPath()}"
+                    : "",
+            ];
+        }
 
         return $this->json([
-            'values' => [
-                'reference' => $reference->getReference(),
-                'label' => $reference->getLibelle(),
-                'quantity' => $reference->getQuantiteDisponible(),
-                'supplierCode' => '',
-                'location' => $this->formatService->location($reference->getEmplacement()),
-                'image' => $reference->getImage()
-                    ? "{$reference->getImage()->getFullPath()}"
-                    : "",
-            ],
+            'success' => true,
+            'values' => $values,
+        ]);
+    }
+
+    #[Route("/get-free-fields", name: "delivery_station_get_free_fields", options: ["expose" => true], methods: "GET")]
+    public function getFreeFields(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $type = $entityManager->getRepository(Type::class)->findByCategoryLabelsAndLabels([CategoryType::DEMANDE_LIVRAISON], ['L - Consommable'])[0]; // TODO A remplacer par le paramétrage
+        $freeFields = $entityManager->getRepository(FreeField::class)->findByTypeAndCategorieCLLabel($type, CategorieCL::DEMANDE_LIVRAISON);
+
+        return $this->json([
+            'empty' => empty($freeFields),
+            'template' => !empty($freeFields) ?
+                $this->renderView('free_field/freeFieldsEdit.html.twig', [
+                    'freeFields' => $freeFields,
+                    'freeFieldValues' => null,
+                    'colType' => "col-6",
+                    'actionType' => "new",
+                    'disabledNeeded' => true,
+                ]) : "",
+        ]);
+    }
+
+    #[Route("/submit-request", name: "delivery_station_submit_request", options: ["expose" => true], methods: "POST")]
+    public function submitRequest(Request                $request,
+                                  EntityManagerInterface $entityManager,
+                                  DeliveryRequestService $deliveryRequestService,
+                                  LivraisonService       $deliveryOrderService): JsonResponse
+    {
+        $data = $request->request->all();
+        dump($data);
+        /*$request = $deliveryRequestService->newDemande([
+            'isManual' => true,
+            'type' => $data['type'],
+            'demandeur' => $nomadUser,
+            'destination' => $location['id'],
+            'expectedAt' => $delivery['expectedAt'] ?? $now->format('Y-m-d'),
+            'project' => $delivery['project'] ?? null,
+            'commentaire' => $delivery['comment'] ?? null,
+        ], $entityManager, $freeFieldService, true);
+
+        $entityManager->persist($request);*/
+
+        return $this->json([
+            'success' => true,
         ]);
     }
 }
