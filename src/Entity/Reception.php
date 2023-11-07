e21df28ed3f908b23b6a85bec16de5d581f6953e
Adrien Bounader <adrien.bounader@wiilog.fr>
2023-11-07 10:44:19 +0100

    WIIS-10511: remove unused import

--- src/Entity/Reception.php
+++ src/Entity/Reception.php
@@ -13,7 +13,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Google\Service\AdMob\Date;
 use WiiCommon\Helper\Stream;
 
 #[ORM\Entity(repositoryClass: ReceptionRepository::class)]
