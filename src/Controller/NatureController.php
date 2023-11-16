5da02cec79e2ce2e097c36cc0a5cb30057437a96
RobinWiilog <robin.pluviaux@wiilog.fr>
2023-11-16 16:23:54 +0100

    WIIS-10572 fix

--- src/Controller/NatureController.php
+++ src/Controller/NatureController.php
@@ -368,12 +368,9 @@ class NatureController extends AbstractController
     {
         if ($data = json_decode($request->getContent(), true)) {
             $natureRepository = $entityManager->getRepository(Nature::class);
-            $translationSourceRepository = $entityManager->getRepository(TranslationSource::class);
 
             $nature = $natureRepository->find($data['nature']);
-            $translationSource = $translationSourceRepository->findOneBy(['nature' => $nature]);
 
-            $entityManager->remove($translationSource);
             $entityManager->remove($nature);
             $entityManager->flush();
             return new JsonResponse();
