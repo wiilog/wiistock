5e84d0711e65644cea5cea49528d877150f2bc9d
Adrien Bounader <adrien.bounader@wiilog.fr>
2023-11-27 11:42:30 +0100

    WIIS-10359: fix migrations

--- migrations/Version20231122163207.php
+++ migrations/Version20231122163207.php
@@ -22,6 +22,10 @@ final class Version20231122163207 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        if (!$schema->hasTable("fixed_field_by_type")) {
+            return;
+        }
+
         // this up() migration is auto-generated, please modify it to your needs
         $locationPickFixedFieldId = $this->connection->fetchOne("SELECT id FROM fixed_field_by_type WHERE entity_code = :entity_code AND field_code = :field_code", [
             "entity_code" => FixedFieldStandard::ENTITY_CODE_DISPATCH,
@@ -36,12 +40,31 @@ final class Version20231122163207 extends AbstractMigration
         $categoryTypeId = $this->connection->fetchOne("SELECT id FROM category_type WHERE label = :category_type", [
             "category_type" => CategoryType::DEMANDE_DISPATCH,
         ]);
-        $types = $this->connection->fetchAllAssociative("SELECT id FROM type WHERE category_id = $categoryTypeId");
+        $typesCreate = $this->connection->fetchAllAssociative("
+            SELECT type.id
+            FROM type
+            LEFT JOIN fixed_field_by_type_required_create ON type.id = fixed_field_by_type_required_create.type_id
+            WHERE category_id = :categoryTypeId
+              AND fixed_field_by_type_required_create.fixed_field_by_type_id IS NULL
+        ", [
+            "categoryTypeId" => $categoryTypeId
+        ]);
+        $typesEdit = $this->connection->fetchAllAssociative("
+            SELECT type.id
+            FROM type
+            LEFT JOIN fixed_field_by_type_required_edit ON type.id = fixed_field_by_type_required_edit.type_id
+            WHERE category_id = :categoryTypeId
+              AND fixed_field_by_type_required_edit.fixed_field_by_type_id IS NULL
+        ", [
+            "categoryTypeId" => $categoryTypeId
+        ]);
 
-        foreach ($types as $type) {
+        foreach ($typesCreate as $type) {
             $this->addSql("INSERT INTO fixed_field_by_type_required_create (fixed_field_by_type_id, type_id) VALUES ($locationPickFixedFieldId, {$type['id']})");
             $this->addSql("INSERT INTO fixed_field_by_type_required_create (fixed_field_by_type_id, type_id) VALUES ($locationDropFixedFieldId, {$type['id']})");
+        }
 
+        foreach ($typesEdit as $type) {
             $this->addSql("INSERT INTO fixed_field_by_type_required_edit (fixed_field_by_type_id, type_id) VALUES ($locationPickFixedFieldId, {$type['id']})");
             $this->addSql("INSERT INTO fixed_field_by_type_required_edit (fixed_field_by_type_id, type_id) VALUES ($locationDropFixedFieldId, {$type['id']})");
         }
