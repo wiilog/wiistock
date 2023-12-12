93c35439d4b819ee57e7ad15cd663032c2dcc817
Romain Barot <romain.barot@wiilog.fr>
2023-12-12 15:17:11 +0100

    WIIS-10569 : review

--- src/Controller/DispatchController.php
+++ src/Controller/DispatchController.php
@@ -1506,7 +1506,7 @@ class DispatchController extends AbstractController {
 
         $dispatchStatuses = $statutRepository->findStatusByType(CategorieStatut::DISPATCH, $dispatch->getType());
         $overConsumptionBillStatus = Stream::from($dispatchStatuses)
-            ->filter(fn(Statut $status) => $status->getOverconsumptionBillGenerationStatus());
+            ->filter(static fn(Statut $status) => $status->getOverconsumptionBillGenerationStatus());
 
         if($overConsumptionBillStatus->count() === 1) {
             $untreatedStatus = $statutRepository->find($overConsumptionBillStatus->first());
