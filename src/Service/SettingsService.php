cd185b17ea8785820ed4be9c4199bfe74500738d
Romain Barot <romain.barot@wiilog.fr>
2023-12-12 15:15:50 +0100

    WIIS-10517

--- src/Service/SettingsService.php
+++ src/Service/SettingsService.php
@@ -958,6 +958,9 @@ class SettingsService {
                 $typeRepository = $this->manager->getRepository(Type::class);
                 $languageRepository = $this->manager->getRepository(Language::class);
 
+                $hasRightGroupedSignature = $this->userService->hasRightFunction(Menu::DEM, Action::GROUPED_SIGNATURE);
+
+
                 $categoryName = match ($statusesData[0]['mode']) {
                     StatusController::MODE_ARRIVAL_DISPUTE => CategorieStatut::DISPUTE_ARR,
                     StatusController::MODE_RECEPTION_DISPUTE => CategorieStatut::LITIGE_RECEPT,
@@ -1009,13 +1012,17 @@ class SettingsService {
                         ->setSendNotifToBuyer($statusData['sendMailBuyers'] ?? false)
                         ->setSendNotifToDeclarant($statusData['sendMailRequesters'] ?? false)
                         ->setSendNotifToRecipient($statusData['sendMailDest'] ?? false)
-                        ->setSendReport($statusData['sendReport'] ?? false)
-                        ->setGroupedSignatureType($statusData['groupedSignatureType'] ?? '')
-                        ->setGroupedSignatureColor($statusData['color'] ?? Statut::GROUPED_SIGNATURE_DEFAULT_COLOR)
                         ->setNeedsMobileSync($statusData['needsMobileSync'] ?? false)
                         ->setCommentNeeded($statusData['commentNeeded'] ?? false)
-                        ->setAutomaticReceptionCreation($statusData['automaticReceptionCreation'] ?? false)
-                        ->setDisplayOrder($statusData['order'] ?? 0);
+                        ->setAutomaticReceptionCreation($statusData['automaticReceptionCreation'] ?? false);
+
+                    if($hasRightGroupedSignature){
+                        $status
+                            ->setSendReport($statusData['sendReport'] ?? false)
+                            ->setGroupedSignatureType($statusData['groupedSignatureType'] ?? '')
+                            ->setGroupedSignatureColor($statusData['color'] ?? Statut::GROUPED_SIGNATURE_DEFAULT_COLOR)
+                            ->setDisplayOrder($statusData['order'] ?? 0);
+                    }
 
                     // label given on creation or edit is the French one
                     $labelTranslation = $status->getLabelTranslation();
