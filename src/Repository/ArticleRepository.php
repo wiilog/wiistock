9dfd8ddbfb1267f39988c55ade0e8b738284f378
ThomasGentile <thomas.gentile@wiilog.fr>
2024-01-26 10:02:26 +0100

    WIIS-10839

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -1247,8 +1247,8 @@ class ArticleRepository extends EntityRepository {
 
         if ($lastMonthOutput) {
             $queryBuilder
-                ->andWhere("article.inactiveSince > :one_month_ago")
-                ->setParameter("one_month_ago", new DateTime("-1 month"));
+                ->andWhere("article.inactiveSince > :collectable_delay")
+                ->setParameter("collectable_delay", new DateTime(Article::COLLECTABLE_DELAY));
         }
 
         if($referenceArticle) {
@@ -1283,8 +1283,8 @@ class ArticleRepository extends EntityRepository {
             ->addSelect('join_location.label AS location')
             ->addSelect('0 AS is_ref')
             ->leftJoin('article.emplacement', 'join_location')
-            ->andWhere("article.inactiveSince > :one_month_ago")
-            ->setParameter("one_month_ago", new DateTime("-1 month"));
+            ->andWhere("article.inactiveSince > :collectable_delay")
+            ->setParameter("collectable_delay", new DateTime(Article::COLLECTABLE_DELAY));
 
         if ($barcode) {
             $queryBuilder
