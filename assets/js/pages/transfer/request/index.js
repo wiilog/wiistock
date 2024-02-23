e5b190ef6aad82871b8c6150ddde70bcb6db4bd7
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-02-23 12:42:32 +0100

    WIIS-11055

---
+++ assets/js/pages/transfer/request/index.js
@@ -0,0 +1,79 @@
+import Routing from '@app/fos-routing';
+
+$(() => {
+    const $statusSelector = $('.filterService select[name="statut"]');
+
+    initDateTimePicker();
+    Select2Old.location($('.ajax-autocomplete-emplacements'), {}, "Emplacement", 3);
+
+    // applique les filtres si pré-remplis
+    let val = $('#filterStatus').val();
+
+    if (val && val.length > 0) {
+        let valuesStr = val.split(',');
+        let valuesInt = [];
+        valuesStr.forEach((value) => {
+            valuesInt.push(parseInt(value));
+        })
+        $statusSelector.val(valuesInt).select2();
+    } else {
+        // sinon, filtres enregistrés en base pour chaque utilisateur
+        let path = Routing.generate('filter_get_by_page');
+        let params = JSON.stringify(PAGE_TRANSFER_REQUEST);
+        $.post(path, params, function (data) {
+            displayFiltersSup(data);
+        }, 'json');
+    }
+
+    const $modalNewTransferRequest = $('#modalNewTransfer');
+    $modalNewTransferRequest.on('show.bs.modal', function () {
+        clearModal("#modalNewTransfer");
+        initNewTransferRequestEditor();
+    });
+
+    $('.select2').select2();
+
+    let pathTransferOrder = Routing.generate('transfer_request_api', true);
+    let transferOrderTableConfig = {
+        processing: true,
+        serverSide: true,
+        order: [['number', 'desc']],
+        ajax: {
+            "url": pathTransferOrder,
+            "type": "POST",
+            'data' : {
+                'filterStatus': $('#filterStatus').val()
+            },
+        },
+        rowConfig: {
+            needsRowClickAction: true,
+        },
+        drawConfig: {
+            needsSearchOverride: true,
+        },
+        columns: [
+            {"data": 'actions', 'name': 'Actions', 'title': '', className: 'noVis', orderable: false},
+            {"data": 'number', 'name': 'Numéro', 'title': 'Numéro'},
+            {"data": 'status', 'name': 'Statut', 'title': 'Statut'},
+            {"data": 'requester', 'name': 'Demandeur', 'title': 'Demandeur'},
+            {"data": 'origin', 'name': 'Origine', 'title': 'Origine'},
+            {"data": 'destination', 'name': 'Destination', 'title': 'Destination'},
+            {"data": 'creationDate', 'name': 'Création', 'title': 'Date de création'},
+            {"data": 'validationDate', 'name': 'Validation', 'title': 'Date de validation'},
+        ]
+    };
+    let table = initDataTable('tableTransferRequest', transferOrderTableConfig);
+
+
+    let modalNewTransferRequest = $("#modalNewTransfer");
+    let SubmitNewTransferRequest = $("#submitNewTransfer");
+    let urlNewTransferRequest = Routing.generate('transfer_request_new', true)
+    InitModal(modalNewTransferRequest, SubmitNewTransferRequest, urlNewTransferRequest, {tables: [table]});
+
+    initSearchDate(table, 'Création');
+
+});
+
+function initNewTransferRequestEditor() {
+    Select2Old.location($('.ajax-autocomplete-location'))
+}
--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,7 @@ Encore
     .addEntry('location-index', './assets/js/pages/location/index.js')
     .addEntry('transfer-order-index', './assets/js/pages/transfer/order/index.js')
     .addEntry('transfer-order-show', './assets/js/pages/transfer/order/show.js')
+    .addEntry('transfer-request-index', './assets/js/pages/transfer/request/index.js')
 
     .addStyleEntry('pack-common', './assets/scss/utils/pack.scss')
     .autoProvidejQuery()
