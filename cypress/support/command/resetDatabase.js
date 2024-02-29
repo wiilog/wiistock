a0c6f12a8c410450eeaf81c804b6070501635f34
RobinWiilog <robin.pluviaux@wiilog.fr>
2024-02-29 10:21:45 +0100

    WIIS-9654-2

--- cypress/support/command/resetDatabase.js
+++ cypress/support/command/resetDatabase.js
@@ -63,7 +63,7 @@ Cypress.Commands.add('dropAndRecreateDatabase', (databaseName = "wiistock") => {
 
 Cypress.Commands.add('curlDatabase', (pathToFile = '/etc/sqlscripts', fileName = 'BDD_cypress.sql') => {
     cy.exec('echo $FTP_USER : $FTP_PASSWORD : $FTP_HOST').then((result) => {
-        cy.log(result.stdout)
+        cy.log(result.stdout);
     })
     cy.exec(`curl -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/cypress/SQL_script/dev-script.sql -o ${pathToFile}/${fileName}`);
 })
