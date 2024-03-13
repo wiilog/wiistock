10785ba46d471fe0a220965ed0eba9cade2ab4f3
Romain Barot <romain.barot@wiilog.fr>
2024-03-13 13:59:58 +0100

    WIIS-11139: changes after review

--- assets/js/pages/purchase-request/common.js
+++ assets/js/pages/purchase-request/common.js
@@ -2,5 +2,5 @@ export function onStatusChange($select){
     const $selectedOption = $select.find('option:selected');
     const $modal = $select.closest('.modal-body');
 
-    $modal.find('[name=deliveryFee]').attr('required', Boolean($selectedOption.data('preventstatuschangewithoutdeliveryfees')));
+    $modal.find('[name="deliveryFee"]').attr('required', Boolean($selectedOption.data('preventstatuschangewithoutdeliveryfees')));
 }
