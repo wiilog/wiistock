dcfb4becadb43eebb4e751702c175486e4502a9d
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-03-21 12:16:59 +0100

    WIIS-11182

--- src/Controller/ReceptionController.php
+++ src/Controller/ReceptionController.php
@@ -816,6 +816,7 @@ class ReceptionController extends AbstractController {
                                 . $referenceArticle->getQuantiteDisponible(),
                         ]);
                     } else {
+                        $referenceArticle->setPrixUnitaire($receptionReferenceArticle->getUnitPrice());
                         $mouvementStock = $mouvementStockService->createMouvementStock(
                             $currentUser,
                             null,
