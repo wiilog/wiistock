0301aa9e2504c1b1720158d7de5d4f413366f072
Nuno Moreira <nuno.moreira@wiilog.fr>
2024-04-15 15:33:23 +0200

    WIIS-11282 Data warehouse

--- data-warehouse/iot/capteur_messages.sql
+++ data-warehouse/iot/capteur_messages.sql
@@ -1,20 +1,22 @@
 SELECT sensor.code            AS code_capteur,
        sensor_message.date    AS date_message,
-       IF(sensor_message.content_type = 1, 'Température',
-          IF(sensor_message.content_type = 2, 'Hygrométrie',
-             IF(sensor_message.content_type = 3, 'Action',
-                IF(sensor_message.content_type = 4, 'GPS',
-                   IF(sensor_message.content_type = 7, 'Position',
-                      IF(sensor_message.content_type = 8, 'Entrée zone',
-                         IF(sensor_message.content_type = 9, 'Sortie zone',
-                            IF(sensor_message.content_type = 10, 'Présence', NULL)
-                         )
-                      )
-                   )
-                )
-             )
-          )
-       )                      AS type_donnee,
+       IF(sensor_message.content_type = 1,'Erreur',
+           IF(sensor_message.content_type = 1, 'Température',
+              IF(sensor_message.content_type = 2, 'Hygrométrie',
+                 IF(sensor_message.content_type = 3, 'Action',
+                    IF(sensor_message.content_type = 4, 'GPS',
+                       IF(sensor_message.content_type = 7, 'Position',
+                          IF(sensor_message.content_type = 8, 'Entrée zone',
+                             IF(sensor_message.content_type = 9, 'Sortie zone',
+                                IF(sensor_message.content_type = 10, 'Présence', NULL)
+                             )
+                          )
+                       )
+                    )
+                 )
+              )
+           )
+       )                   AS type_donnee,
        sensor_message.content AS donnee_principale
 
 FROM sensor_message
