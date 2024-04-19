ce6ced0eb8b77af6a2e9a3e525ab50479dd00ee4
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-04-19 14:09:20 +0200

    WIIS-10929: add log on install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,24 @@ execute_query() {
 }
 
 prepare_project() {
-    mkdir -p /tmp/wiistock-tmp
+    echo ">>>>>> clear if we are in crash loop"
+    rm -rf /tmp/wiistock-deploy || true
+    rm -rf /project/public/build || true
+    rm -rf /project/node_modules || true
+    rm -rf /project/vendor || true
 
-    # Extract vendor and node_modules from cache if it exists
-    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/vendor.zip" -P /tmp/wiistock-tmp || true
-    if [ -f /tmp/wiistock-tmp/vendor.zip ]; then
-        unzip -q /tmp/wiistock-tmp/vendor.zip -d /project
+
+    echo ">>>>>> create tmp directory for zip download"
+    mkdir -p /tmp/wiistock-deploy
+
+    echo ">>>>>> wget vendor.zip"
+    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/vendor.zip" -P /tmp/wiistock-deploy || true
+    if [ -f /tmp/wiistock-deploy/vendor.zip ]; then
+        echo ">>>>>> unzip vendor.zip"
+        unzip -q /tmp/wiistock-deploy/vendor.zip -d /project
     fi
 
+    echo ">>>>>> composer install"
     composer install \
         --no-dev \
         --optimize-autoloader \
@@ -25,90 +35,102 @@ prepare_project() {
         APP_CONTEXT="prod"
     fi
 
+    echo ">>>>>> wget build.zip"
     BUILD_ZIP_NAME="build-$APP_CONTEXT.zip"
-    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/$BUILD_ZIP_NAME" -P /tmp/wiistock-tmp || true
-    if [ -f "/tmp/wiistock-tmp/$BUILD_ZIP_NAME" ]; then
-        unzip -q "/tmp/wiistock-tmp/$BUILD_ZIP_NAME" -d /project/public
+    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/$BUILD_ZIP_NAME" -P /tmp/wiistock-deploy || true
+    if [ -f "/tmp/wiistock-deploy/$BUILD_ZIP_NAME" ]; then
+        echo ">>>>>> unzip build.zip"
+        unzip -q "/tmp/wiistock-deploy/$BUILD_ZIP_NAME" -d /project/public
 
         if [ -d public/build ]; then
+            echo ">>>>>> replace <<DOMAIN_NAME>>"
             find "public/$BUILD_ZIP_NAME" -type f -exec sed -i "s/<<DOMAIN_NAME>>/$APP_DOMAIN_NAME/g" {} \;
         fi
     fi
 
-    # si /public/build existe pas on le crée
+    # Extract public/build from cache if it exists
+    # else extract node_modules from cache and run yarn build
     if [ ! -d public/build ]; then
-        wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/node_modules.zip" -P /tmp/wiistock-tmp || true
-        if [ -f /tmp/wiistock-tmp/node_modules.zip ]; then
-            unzip -q /tmp/wiistock-tmp/node_modules.zip -d /project
+        echo ">>>>>> wget node_modules.zip"
+        wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/node_modules.zip" -P /tmp/wiistock-deploy || true
+        if [ -f /tmp/wiistock-deploy/node_modules.zip ]; then
+            echo ">>>>>> unzip node_modules.zip"
+            unzip -q /tmp/wiistock-deploy/node_modules.zip -d /project
         else
+            echo ">>>>>> yarn install"
             yarn install
         fi
         build_yarn
     fi
 
-    rm -rf /tmp/wiistock-tmp
+    rm -rf /tmp/wiistock-deploy || true
+
     php bin/console app:initialize
 }
 
 install_symfony() {
+    echo ">>>>>> install_symfony"
     TABLE_COUNT=$(execute_query "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$DATABASE_NAME';")
     last_command_status=$?
     if [ $last_command_status -ne 0 ]; then
         echo "Failed to access database with error code $last_command_status, aborting installation"
         exit 1
     elif [ "$TABLE_COUNT" = "0" ]; then
-        echo "New instance, creating database"
+        echo ">>>>>> New instance, creating database"
 
+        echo ">>>>>> php bin/console doctrine:schema:update --force"
         php bin/console doctrine:schema:update --force
+
+        echo ">>>>>> php bin/console doctrine:migrations:sync-metadata-storage"
         php bin/console doctrine:migrations:sync-metadata-storage
+
+        echo ">>>>>> php bin/console doctrine:migrations:version --add --all --no-interaction"
         php bin/console doctrine:migrations:version --add --all --no-interaction
     else
-        echo "Existing instance, updating database"
+        echo ">>>>>> Existing instance, updating database"
 
+        echo ">>>>>> php bin/console doctrine:migrations:migrate --no-interaction --dry-run"
         php bin/console doctrine:migrations:migrate --no-interaction --dry-run
-        last_command_status=$?
-        if [ $last_command_status -ne 0 ]; then
-            echo "Failed to execute migrations dry-run with error code $last_command_status, aborting deployment"
-            exit 1
-        fi;
 
+        echo ">>>>>> php bin/console doctrine:migrations:migrate --no-interaction"
         php bin/console doctrine:migrations:migrate --no-interaction
-        last_command_status=$?
-        if [ $last_command_status -ne 0 ]; then
-            echo "Failed to execute migrations with error code $last_command_status, aborting deployment"
-            exit 1
-        fi;
 
+        echo ">>>>>> php bin/console doctrine:schema:update --force --dump-sql"
         php bin/console doctrine:schema:update --force --dump-sql
-        last_command_status=$?
-        if [ $last_command_status -ne 0 ]; then
-            echo "Failed to execute schema update with error code $last_command_status, aborting deployment"
-            exit 1
-        fi;
     fi
 
-    php bin/console doctrine:fixtures:load --append --group types
-    php bin/console doctrine:fixtures:load --append --group fixtures
-    last_command_status=$?
-    if [ $last_command_status -ne 0 ]; then
-        echo "Failed to execute fixtures with error code $last_command_status, aborting deployment"
-        exit 1
-    fi;
 
+    echo ">>>>>> php bin/console doctrine:fixtures:load --append --group types"
+    php bin/console doctrine:fixtures:load --append --group types
+
+    echo ">>>>>> php bin/console doctrine:fixtures:load --append --group fixtures"
+    php bin/console doctrine:fixtures:load --append --group fixtures
+
+    echo ">>>>>> php bin/console app:update:translations"
     php bin/console app:update:translations
 
+
+    echo ">>>>>> php bin/console cache:clear"
     php bin/console cache:clear
+
+    echo ">>>>>> php bin/console cache:warmup"
     php bin/console cache:warmup
 }
 
 build_yarn() {
+    echo ">>>>>> php bin/console fos:js-routing:dump --format=json --target=public/generated/routes.json"
     php bin/console fos:js-routing:dump --format=json --target=public/generated/routes.json
+
+    echo ">>>>>> php bin/console app:update:fixed-fields"
     php bin/console app:update:fixed-fields
 
+    echo ">>>>>> yarn build:only:production"
     yarn build:only:production
 }
 
 cd /project
+
+echo ">>>>>> Set default session_lifetime in config/generated.yaml"
 echo '{"parameters":{"session_lifetime": 1440}}' > config/generated.yaml
 
 prepare_project
