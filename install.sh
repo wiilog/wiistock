35d75914bbc5cacee07fe66723b76536aef12b87
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-04-19 10:36:57 +0200

    WIIS-10929

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ execute_query() {
 
 prepare_project() {
     # Extract vendor and node_modules from cache if it exists
-    wget https://github.com/wiilog/wiistock/releases/download/"$WIISTOCK_VERSION"/vendor.zip || true
-    if [ -f vendor.zip ]; then
-        unzip -q vendor.zip
-        rm vendor.zip
+    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/vendor.zip" -P /tmp || true
+    if [ -f /tmp/vendor.zip ]; then
+        unzip -q /tmp/vendor.zip -d /project
+        rm /tmp/vendor.zip
     fi
 
     composer install \
@@ -24,26 +24,27 @@ prepare_project() {
         APP_CONTEXT="prod"
     fi
 
-    wget https://github.com/wiilog/wiistock/releases/download/"$WIISTOCK_VERSION"/build-"$APP_CONTEXT".zip || true
-    if [ -f build-"$APP_CONTEXT".zip ]; then
-        unzip -q build-"$APP_CONTEXT".zip
-        rm build-"$APP_CONTEXT".zip
-        if [ -d build ]; then
-            find build -type f -exec sed -i "s/<<DOMAIN_NAME>>/$APP_DOMAIN_NAME/g" {} \;
-            mv build public/
+    BUILD_ZIP_NAME="build-$APP_CONTEXT.zip"
+    wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/$BUILD_ZIP_NAME" -P /tmp || true
+    if [ -f "/tmp/$BUILD_ZIP_NAME" ]; then
+        unzip -q "/tmp/$BUILD_ZIP_NAME" -d /project/public
+        rm "/tmp/$BUILD_ZIP_NAME"
+
+        if [ -d public/build ]; then
+            find "public/$BUILD_ZIP_NAME" -type f -exec sed -i "s/<<DOMAIN_NAME>>/$APP_DOMAIN_NAME/g" {} \;
         fi
     fi
 
     # si /public/build existe pas on le crée
     if [ ! -d public/build ]; then
-        wget https://github.com/wiilog/wiistock/releases/download/"$WIISTOCK_VERSION"/node_modules.zip || true
-        if [ -f node_modules.zip ]; then
-            unzip -q node_modules.zip
-            rm node_modules.zip
+        wget "https://github.com/wiilog/wiistock/releases/download/$WIISTOCK_VERSION/node_modules.zip" -P /tmp || true
+        if [ -f /tmp/node_modules.zip ]; then
+            unzip -q /tmp/node_modules.zip -d /project
+            rm /tmp/node_modules.zip
         else
             yarn install
-            install_yarn
         fi
+        build_yarn
     fi
 
     php bin/console app:initialize
@@ -100,7 +101,7 @@ install_symfony() {
     php bin/console cache:warmup
 }
 
-install_yarn() {
+build_yarn() {
     php bin/console fos:js-routing:dump --format=json --target=public/generated/routes.json
     php bin/console app:update:fixed-fields
 
