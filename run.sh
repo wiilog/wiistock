3e4e0f38774d799a3c015cf536316855206b01e1
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-05-13 16:59:21 +0200

    add cron

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Starting cron"
-php /project/bin/console cron:start
+echo "* * * * * php /var/www/bin/console cron:run > /tmp/cronlog.txt 2>&1" > /tmp/crontabwiistock.txt && crontab /tmp/crontabwiistock.txt && rm /tmp/crontabwiistock.txt
+crond -f -d 0
