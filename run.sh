4928cc9099eef829b331965489658208161132eb
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-05-14 15:38:07 +0200

    fix cron

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Starting cron"
-echo "* * * * * php /var/www/bin/console cron:run > /tmp/cronlog.txt 2>&1" > /tmp/crontabwiistock.txt && crontab /tmp/crontabwiistock.txt && rm /tmp/crontabwiistock.txt
+echo "* * * * * php /project/bin/console cron:run > /tmp/cronlog.txt 2>&1" > /tmp/crontabwiistock.txt && crontab /tmp/crontabwiistock.txt && rm /tmp/crontabwiistock.txt
 crond -f -d 0
