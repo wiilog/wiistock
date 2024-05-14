93480c3c1f1cc24a9dd278d0a185739314d7d29b
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-05-14 16:26:30 +0200

    fix cron

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo "Starting cron"
 echo "* * * * * php /project/bin/console cron:run > /tmp/cronlog.txt 2>&1" > /tmp/crontabwiistock.txt && crontab /tmp/crontabwiistock.txt && rm /tmp/crontabwiistock.txt
-crond -f -d 0
+crond -d 0
