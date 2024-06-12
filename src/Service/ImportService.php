4c2cd31a5cfddc09839a45dd040e81bf11c54624
Nuno Moreira <nuno.moreira@wiilog.fr>
2024-06-12 16:41:17 +0200

    WIIS-11524

--- src/Service/ImportService.php
+++ src/Service/ImportService.php
@@ -90,6 +90,8 @@ class ImportService
             "articleFournisseurReference",
             "fournisseurReference",
             "commentaire",
+            "emplacement",
+            "quantite",
             "stockEntryDate",
             "expiryDate",
             "dateLastInventory",
