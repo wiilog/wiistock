a01474a911db0fcbe3b1f84046c7578683b9b48f
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-06-14 18:33:41 +0200

    WIIS-11516

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Starting cron"
-mkdir -p /project/var/log
-printf '* * * * * echo -e "%s\\t%s\\n" 2>> /project/var/log/cron.log\n' '$(date)' '$(php /project/bin/console cron:run)' > /tmp/crontabwiistock.txt && crontab /tmp/crontabwiistock.txt && rm /tmp/crontabwiistock.txt
-crond -d 0
