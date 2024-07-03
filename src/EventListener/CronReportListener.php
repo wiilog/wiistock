66a8028e39d7f846867260ce9950740db3131318
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-07-03 11:38:07 +0200

    WIIS-10105

--- src/EventListener/CronReportListener.php
+++ src/EventListener/CronReportListener.php
@@ -6,14 +6,10 @@ namespace App\EventListener;
 
 use App\Service\ExceptionLoggerService;
 use Cron\CronBundle\Entity\CronReport;
-use Doctrine\Common\EventSubscriber;
-use Doctrine\ORM\Event\OnFlushEventArgs;
-use Doctrine\ORM\Event\PostFlushEventArgs;
-use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Contracts\Service\Attribute\Required;
-use WiiCommon\Helper\Stream;
 
-class CronReportListener implements EventSubscriber
+
+class CronReportListener
 {
     #[Required]
     public ExceptionLoggerService $exceptionLoggerService;
@@ -23,27 +19,15 @@ class CronReportListener implements EventSubscriber
      */
     private array $flushedErrorCronReports = [];
 
-    public function getSubscribedEvents(): array {
-        return [
-            'onFlush',
-            'postFlush',
-        ];
+    public function postPersist(CronReport $cronReport): void {
+        $this->flushedErrorCronReports[] = $cronReport;
     }
 
-    #[AsEventListener(event: 'onFlush')]
-    public function onFlush(OnFlushEventArgs $args): void {
-        $this->flushedErrorCronReports = Stream::from($args->getObjectManager()->getUnitOfWork()->getScheduledEntityInsertions())
-            ->filter(static fn($entity) => $entity instanceof CronReport && $entity->getExitCode() !== 0)
-            ->toArray();
-    }
-
-    #[AsEventListener(event: 'postFlush')]
-    public function postFlush(PostFlushEventArgs $args): void {
-        foreach ($this->flushedErrorCronReports ?? [] as $cronReports) {
-            $this->exceptionLoggerService->sendLog(
-                new \Exception("Cron job failed: {$cronReports->getJob()->getName()}. Output: {$cronReports->getOutput()} Error: {$cronReports->getError()}"),
-                null
-            );
+    public function postFlush(): void {
+        foreach ($this->flushedErrorCronReports as $cronReports) {
+            $cronException = new \Exception("Cron job failed: {$cronReports->getJob()->getName()}. Output: {$cronReports->getOutput()} Error: {$cronReports->getError()}");
+            $this->exceptionLoggerService->sendLog($cronException);
         }
+        $this->flushedErrorCronReports = [];
     }
 }
