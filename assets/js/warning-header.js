3ea3853a57d3e0004dcc9108893d7fcded0a6ca9
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-07-31 14:51:02 +0200

    WIIS-11670

--- assets/js/warning-header.js
+++ assets/js/warning-header.js
@@ -1,4 +1,4 @@
-import {createCookie, deleteCookie, getCookie} from "@app/cookie";
+import Cookie from "@app/cookie";
 
 const COOKIE_NAME = 'warning-header';
 
@@ -9,36 +9,38 @@ $(function () {
     const warningHeaderHash = $('input[name=warning-header-hash]').val();
 
     // Value get from cookie
-    const cookieHash = getCookie(COOKIE_NAME);
+    const cookieHash = Cookie.get(COOKIE_NAME);
 
-    if (!!cookieHash) {
-        if (warningHeaderHash !== cookieHash) {
-            // Delete cookie if hash is different, that means the warning header has been changed
-            deleteCookie(COOKIE_NAME);
-            displayWarningHeader($bannerWarningHeader);
+    if ($bannerWarningHeader.exists()) {
+        if (!!cookieHash) {
+            if (warningHeaderHash !== cookieHash) {
+                // Delete cookie if hash is different, that means the warning header has been changed
+                Cookie.delete(COOKIE_NAME);
+                displayWarningHeader($bannerWarningHeader);
+
+            } else {
+                hideWarningHeader($bannerWarningHeader);
+            }
 
         } else {
-            hideWarningHeader($bannerWarningHeader);
+            displayWarningHeader($bannerWarningHeader);
         }
 
-    } else {
-        displayWarningHeader($bannerWarningHeader);
+        onClickWarningHeader($btnWarningHeader, $bannerWarningHeader, warningHeaderHash);
     }
-
-    onClickWarningHeader($btnWarningHeader, $bannerWarningHeader, warningHeaderHash);
 });
 
 function hideWarningHeader($bannerWarningHeader) {
-    $bannerWarningHeader.hide();
+    $bannerWarningHeader.addClass('d-none');
 }
 
 function displayWarningHeader($bannerWarningHeader) {
-    $bannerWarningHeader.css('display', 'flex').removeClass('d-none');
+    $bannerWarningHeader.removeClass('d-none');
 }
 
 function onClickWarningHeader($btnWarningHeader, $bannerWarningHeader, warningHeaderHash) {
     $btnWarningHeader.on('click', function () {
-        createCookie(COOKIE_NAME, warningHeaderHash);
+        Cookie.save(COOKIE_NAME, warningHeaderHash);
 
         hideWarningHeader($bannerWarningHeader);
     });
