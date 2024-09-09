3ab73ae8471117526469ac9f0bb166bc089206cd
CedricRoux <cedric.roux@wiilog.fr>
2024-09-09 14:44:30 +0200

    WIIS-11887 Changes after first tests

--- src/Command/RemoveDuplicateLUCommand.php
+++ src/Command/RemoveDuplicateLUCommand.php
@@ -11,16 +11,17 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use WiiCommon\Helper\Stream;
-
 
 #[AsCommand(
     name: 'app:lu:remove-duplicate',
     description: 'Tool to remove duplicate LU',
 )]
-class RemoveDuplicateLUCommand extends Command {
-    public function __construct(private readonly EntityManagerInterface $entityManager,
-                                private readonly FormatService          $formatService) {
+class RemoveDuplicateLUCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly FormatService $formatService
+    ) {
         parent::__construct();
     }
 
@@ -32,54 +33,109 @@ class RemoveDuplicateLUCommand extends Command {
         $duplicateLUsData = $luRepository->findDuplicateCode();
         $io = new SymfonyStyle($input, $output);
 
-        // if there are no duplicate LUs, show a message in the console output
         if (empty($duplicateLUsData)) {
-            $io->success('Pas d\'UL dupliquée !!');
+            $io->success('Aucun code UL en double trouvé.');
             return Command::SUCCESS;
         } else {
-            $io->warning('Il y a des ULs dupliquées !!');
+            $io->warning('Codes UL en double trouvés.');
         }
 
         foreach ($duplicateLUsData as $duplicateLUData) {
-            do {
-                $lus = $luRepository->findBy(['code' => $duplicateLUData['code']]);
-                $io->section("ULs dupliquées : {$duplicateLUData['code']}");
-                // show the duplicate LUs in the console output
-                $io->table(...$this->createTableConfig($lus, $this->formatService, $luRepository));
+            $lus = $luRepository->findBy(['code' => $duplicateLUData['code']]);
+            $io->section("Code UL dupliqué: {$duplicateLUData['code']} (" . count($lus) . " occurrences)");
+            $io->table(...$this->createTableConfig($lus, $this->formatService, $luRepository));
 
-                // ask the user to choose the LU to rename
-                $chosenLU = $io->choice('Quel UL voulez-vous renommer ? (id)', Stream::from($lus)->map(fn(Pack $lu) => $lu->getId())->toArray());
-                $newCodeIsValid = false;
-                do {
-                    // ask the user to enter the new code
-                    $newCode = $io->ask('Entrez le nouveau code', $duplicateLUData['code'].'_bis');
-
-                    // check if the new code is already used
-                    $newCodeIsValid = count($luRepository->findBy(['code' => $newCode])) === 0;
-                    if ($newCodeIsValid === false) {
-                        $io->error("Le code {$newCode} est déjà utilisé");
-                        $newCode = null;
-                    }
-                } while ($newCodeIsValid === false);
-
-                // update the chosen LU with the new code
-                $lu = $luRepository->find($chosenLU);
-                $lu->setCode($newCode);
-                $this->entityManager->flush();
-
-                $io->success("UL {$chosenLU} renommée en {$newCode}");
-                $io->newLine();
-
-            } while (count($luRepository->findBy(['code' => $duplicateLUData['code']])) > 1);
+            if (strlen($duplicateLUData['code']) < 250) {
+                // Automatically apply the "Add counter" choice
+                $this->applyCounter($lus, $duplicateLUData['code'], $luRepository, $io);
+            } else {
+                // Offer choices to the user
+                $this->offerChoices($lus, $duplicateLUData['code'], $luRepository, $io);
+            }
         }
-
 
         return Command::SUCCESS;
     }
 
-    private function createTableConfig( array $lus, FormatService $formatService, PackRepository $luRepository): array {
+    private function applyCounter(array $lus, string $baseCode, PackRepository $luRepository, SymfonyStyle $io): void
+    {
+        $counter = 1;
+        foreach ($lus as $lu) {
+            $newCodeIsValid = false;
+            while (!$newCodeIsValid) {
+                $newCode = $baseCode . '_' . sprintf("%02d", $counter);
+                $newCodeIsValid = count($luRepository->findBy(['code' => $newCode])) === 0;
+                if (!$newCodeIsValid) {
+                    $counter++;
+                }
 
-        $header = ['Id', 'Dernier mouvement', 'nature', 'Enplacement', 'nb d\'ul enfants'];
+                if (strlen($newCode) > 250) {
+                    $io->error("Le code {$newCode} dépasse 250 caractéres");
+                    $newCodeIsValid = false;
+                    $counter++;
+                    break;
+                }
+            }
+
+            if ($newCodeIsValid) {
+                $lu->setCode($newCode);
+                $this->entityManager->flush();
+                $io->success("UL {$lu->getId()} renommé =>  {$newCode}");
+                $io->newLine();
+            }
+        }
+    }
+
+    private function offerChoices(array $lus, string $baseCode, PackRepository $luRepository, SymfonyStyle $io): void
+    {
+        $validChoice = false;
+        while (!$validChoice) {
+            $choice = $io->choice('Que voulez vous faire ?', ['Ajouter un compteur', 'Renommer completement', 'Passer à UL suivante']);
+
+            if ($choice === 'Ajouter un compteur') {
+                $this->applyCounter($lus, $baseCode, $luRepository, $io);
+                $validChoice = true;
+            } elseif ($choice === 'Renommer completement') {
+                $this->renameCompletely($lus, $baseCode, $luRepository, $io);
+                $validChoice = true;
+            } elseif ($choice === 'Passer à UL suivante') {
+                $io->note("Passer à UL suivante sans renommer UL actuelle.");
+                $validChoice = true;
+                break;
+            }
+        }
+    }
+
+    private function renameCompletely(array $lus, string $baseCode, PackRepository $luRepository, SymfonyStyle $io): void
+    {
+        $newCodeBase = $io->ask('Entrez le nouveau code UL', $baseCode . '_bis');
+        $counter = 1;
+        foreach ($lus as $lu) {
+            $newCodeIsValid = false;
+            while (!$newCodeIsValid) {
+                $newCode = $newCodeBase . '_' . sprintf("%02d", $counter);
+                $newCodeIsValid = count($luRepository->findBy(['code' => $newCode])) === 0;
+                if (!$newCodeIsValid) {
+                    $counter++;
+                }
+
+                if (strlen($newCode) > 255) {
+                    $io->error("Le  code {$newCode} dépasse la limite de 250 caractéres.");
+                    $newCodeIsValid = false;
+                    $counter++;
+                }
+            }
+
+            $lu->setCode($newCode);
+            $this->entityManager->flush();
+            $io->success("UL {$lu->getId()} renommée en {$newCode}");
+            $io->newLine();
+        }
+    }
+
+    private function createTableConfig(array $lus, FormatService $formatService, PackRepository $luRepository): array
+    {
+        $header = ['Id', 'Dernier mouvement', 'Nature', 'Emplacement actuel', 'n\'ombre d\'UL enfants'];
         $table = [];
 
         foreach ($lus as $lu) {
@@ -94,6 +150,7 @@ class RemoveDuplicateLUCommand extends Command {
                 $nbChildren,
             ];
         }
+
         return [$header, $table];
     }
 }
