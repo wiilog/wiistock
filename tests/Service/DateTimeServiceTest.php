29e48319505f7eec33e9f7e97c86e7fbe639aa6f
Benjamin Loupiac <benjamin.loupiac@wiilog.fr>
2024-10-08 15:22:17 +0200

    WIIS-11848 -- tests for getWorkedPeriodBetweenDates

--- tests/Service/DateTimeServiceTest.php
+++ tests/Service/DateTimeServiceTest.php
@@ -3,17 +3,25 @@
 namespace App\Tests\Service;
 
 use App\Service\DateTimeService;
+use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class DateTimeServiceTest extends KernelTestCase
 {
     private DateTimeService $dateTimeService;
+    private EntityManagerInterface $entityManager;
+
+    private String $localPathCacheWorkedDay = './cache/work-period/workedDay';
+
+    private String $localPathCacheFreeDay = './cache/work-period/workFreeDay';
 
     protected function setUp(): void {
         self::bootKernel();
         $container = static::getContainer();
-
         $this->dateTimeService = $container->get(DateTimeService::class);
+        $this->entityManager = $container->get(EntityManagerInterface::class);
     }
 
     /**
@@ -21,6 +29,7 @@ class DateTimeServiceTest extends KernelTestCase
      */
     public function testCalculateMinuteFromWithValidTimes(string $time, int $expectedMinutes): void
     {
+        var_dump(exec("ls ./cache"));
         $this->assertEquals($expectedMinutes, $this->dateTimeService->calculateMinuteFrom($time));
     }
 
@@ -84,6 +93,179 @@ class DateTimeServiceTest extends KernelTestCase
         $this->assertEquals(
             2 * DateTimeService::SECONDS_IN_MINUTE + 5,
             $this->dateTimeService->calculateMinuteFrom('02:05')
+        );
+    }
+
+
+    private function createCacheWorkedPeriod(array $arrayWordedDay, array $arrayFreeDay,): void {
+        file_put_contents($this->localPathCacheWorkedDay, serialize($arrayWordedDay));
+        file_put_contents($this->localPathCacheFreeDay, serialize($arrayFreeDay));
+    }
+    private function deleteCacheWorkedPeriod(): void {
+        if(file_exists($this->localPathCacheWorkedDay)) {
+            unlink($this->localPathCacheWorkedDay);
+        }
+        if(file_exists($this->localPathCacheFreeDay)) {
+            unlink($this->localPathCacheFreeDay);
+        }
+    }
+    private function arrayWorkedPeriod() : array
+    {
+        return array(
+            "monday" => [
+                ["09:00","19:00"],
+            ],
+            "tuesday" => [
+                ["08:00","12:00"],
+                ["13:00","17:00"]
+            ],
+            "wednesday" => [
+                ["08:00","12:00"],
+                ["13:00","17:00"]
+            ],
+            "friday" => [
+                ["08:00","12:00"],
+                ["13:00","16:00"],
+                ["17:00","19:00"]
+            ],
+        );
+    }
+
+    private function arrayWorkedPeriodEmpty() : array
+    {
+        return array();
+    }
+
+    private function arrayFreePeriod() : array
+    {
+        return array(
+            DateTime::createFromFormat('Y-m-d','2024-10-08'),
+            DateTime::createFromFormat('Y-m-d','2024-12-25'),
+        );
+    }
+
+    private function arrayFreePeriodEmpty() : array
+    {
+        return array();
+    }
+
+    /** Get worked period in one day with worked period [08:00 - 12:00;13:00 - 17:00]
+    * Must return 28800000 millisecond (8 hours)
+    * */
+    public function testGetWorkedPeriodBetweenDatesWithBreak() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-09 08:00:00');
+        $this->assertEquals(
+            28800000,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period in one day with worked period [08:00 - 12:00;13:00 - 16:00;17:00 - 19:00]
+     * Must return 32400000 millisecond (9 hours)
+     * */
+    public function testGetWorkedPeriodBetweenDatesWith2Breaks() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-11 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-12 08:00:00');
+        $this->assertEquals(
+            32400000,
+        $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period in one day with worked period [09:00 - 19:00]
+     * Must return 36000000 millisecond (10 hours)
+     * */
+    public function testGetWorkedPeriodBetweenDatesWithoutBreak() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-07 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $this->assertEquals(
+            36000000,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period between two unversed dates with worked period [08:00 - 12:00;13:00 - 17:00]
+     * Must return 28800000 millisecond (8 hours)
+     * */
+    public function testGetWorkedPeriodBetweenUnversedDates() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-09 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $this->assertEquals(
+            28800000,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period between same date and same time with worked period [08:00 - 12:00;13:00 - 17:00]
+     * Must return 0 millisecond
+     * */
+    public function testGetWorkedPeriodBetweenSameDates() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $this->assertEquals(
+            0,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period between date during unworked period (thursday)
+     * Must return 0 millisecond
+     * */
+    public function testGetWorkedPeriodBetweenDatesDuringUnworkedDay() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-10 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-11 08:00:00');
+        $this->assertEquals(
+            0,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period between date during free day period
+     * Must return 0 millisecond
+     * */
+    public function testGetWorkedPeriodBetweenDatesWithFreeDay() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriod(), $this->arrayFreePeriod());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-09 08:00:00');
+        $this->assertEquals(
+            0,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
+        );
+    }
+
+    /** Get worked period between date without worked day
+     * Must return 0 millisecond
+     * */
+    public function testGetWorkedPeriodBetweenDatesWithoutWorkedDay() : void
+    {
+        $this->deleteCacheWorkedPeriod();
+        $this->createCacheWorkedPeriod($this->arrayWorkedPeriodEmpty(), $this->arrayFreePeriodEmpty());
+        $date1 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-08 08:00:00');
+        $date2 = DateTime::createFromFormat('Y-m-d H:i:s','2024-10-09 08:00:00');
+        $this->assertEquals(
+            0,
+            $this->dateTimeService->convertDateIntervalToMilliseconds($this->dateTimeService->getWorkedPeriodBetweenDates($this->entityManager, $date1, $date2))
         );
     }
 }
