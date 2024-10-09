f1a720e689715065e7bc45db6d58c5aec224a290
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-10-09 10:11:42 +0200

    WIIS-11974 fix migrations

--- migrations/Version20241007115708.php
+++ migrations/Version20241007115708.php
@@ -20,9 +20,17 @@ final class Version20241007115708 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE pack RENAME COLUMN last_tracking_id TO last_action_id');
-        $this->addSql('ALTER TABLE pack RENAME COLUMN first_tracking_id TO first_action_id');
-        $this->addSql('ALTER TABLE pack RENAME COLUMN last_drop_id TO last_ongoing_drop_id');
+        if ($schema->getTable("pack")->hasColumn("last_tracking_id")) {
+            $this->addSql('ALTER TABLE pack RENAME COLUMN last_tracking_id TO last_action_id');
+        }
+
+        if ($schema->getTable("pack")->hasColumn("first_tracking_id")) {
+            $this->addSql('ALTER TABLE pack RENAME COLUMN first_tracking_id TO first_action_id');
+        }
+
+        if ($schema->getTable("pack")->hasColumn("last_drop_id")) {
+            $this->addSql('ALTER TABLE pack RENAME COLUMN last_drop_id TO last_ongoing_drop_id');
+        }
     }
 
     public function down(Schema $schema): void
