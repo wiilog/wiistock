47a521e50f271114b28851b3c49ec8df88bd9a7e
RobinWiilog <robin.pluviaux@wiilog.fr>
2024-10-16 10:59:31 +0200

    WIIS-11996

--- src/Messenger/TrackingDelay/CalculateTrackingDelayHandler.php
+++ src/Messenger/TrackingDelay/CalculateTrackingDelayHandler.php
@@ -3,8 +3,10 @@
 namespace App\Messenger\TrackingDelay;
 
 use App\Entity\Pack;
+use App\Service\ExceptionLoggerService;
 use App\Service\TrackingDelayService;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
@@ -13,6 +15,7 @@ class CalculateTrackingDelayHandler {
     public function __construct(
         private EntityManagerInterface $entityManager,
         private TrackingDelayService   $trackingDelayService,
+        private ExceptionLoggerService $loggerService,
     ) {}
 
     public function __invoke(CalculateTrackingDelayMessage $message): void {
@@ -21,8 +24,13 @@ class CalculateTrackingDelayHandler {
         $packRepository = $this->entityManager->getRepository(Pack::class);
         $pack = $packRepository->findOneBy(["code" => $packCode]);
 
-        $this->trackingDelayService->updateTrackingDelay($this->entityManager, $pack);
-
+        throw new \RuntimeException('Tracking delay not implemented');
+        try {
+            $this->trackingDelayService->updateTrackingDelay($this->entityManager, $pack);
+        }
+        catch (Exception $error) {
+            $this->loggerService->sendLog($error);
+        }
         $this->entityManager->flush();
     }
 }
