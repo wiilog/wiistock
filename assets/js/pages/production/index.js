fd3c5d9d3d53bf3e49d353b386ae55ea34c88cd1
Benjamin Loupiac <benjamin.loupiac@wiilog.fr>
2024-10-16 12:12:37 +0200

    WIIS-11993 -- application des parametres dans la modal et dans la liste des OF

--- assets/js/pages/production/index.js
+++ assets/js/pages/production/index.js
@@ -125,6 +125,7 @@ function onProductionRequestTypeChange($select){
         $selectDropLocation.append(new Option(optionData.dropLocationLabel, optionData.dropLocationId, true, true)).trigger(`change`);
     }
     $selectDropLocation.attr('data-other-params-typeDispatchDropLocation', $typeSelect.val() || "")
+    changeAttributByType($select);
 }
 
 function initNewProductionRequest() {
--- public/js/script-wiilog.js
+++ public/js/script-wiilog.js
@@ -1215,6 +1215,47 @@ function onTypeChange($select) {
     }
 }
 
+function changeAttributByType($select) {
+    const $modal = $select.closest('.modal');
+    // find all fixed fields that can be configurable
+    const $fields = $modal.find('[data-displayed-type]');
+
+    // remove required symbol from all fields
+    $fields.find('.required-symbol')
+        .remove();
+    $fields.find('.data')
+        .removeClass('needed')
+        .prop('required', false)
+
+    // find all fields that should be displayed
+    const $fieldsToDisplay = $fields.filter(`[data-displayed-type~="${$select.val()}"]`);
+
+    // find all fields that should be required
+    const $fieldsRequired = $fieldsToDisplay.filter(`[data-required-type~="${$select.val()}"]`);
+
+    // add required symbol to all required fields
+    $fieldsRequired.find('.field-label')
+        .append($('<span class="required-symbol">*</span>'));
+    $fieldsRequired.find('.data')
+        .addClass('needed')
+        .prop('required', true)
+
+    // find all fields that should not be required and remove required attribute
+    const $fieldsNotRequired = $fieldsToDisplay.not($fieldsRequired);
+    $fieldsNotRequired.find('.data')
+        .removeClass('is-invalid');
+    $fieldsNotRequired.find('.invalid-feedback')
+        .remove();
+    $fieldsNotRequired.find('.invalid-feedback')
+        .remove();
+
+    // hide the fields that should not be displayed
+    $fields.not($fieldsToDisplay)
+        .addClass('d-none');
+    // show the fields that should be displayed
+    $fieldsToDisplay.removeClass('d-none');
+}
+
 function getBSAlertModal() {
     return $('#alert-modal');
 }
