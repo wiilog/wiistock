292c61718fe5bda5ed29777d33b662e63366d086
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-10-17 14:43:55 +0200

    WIIS-11996

--- src/Messenger/LoggedHandler.php
+++ src/Messenger/LoggedHandler.php
@@ -3,10 +3,11 @@
 namespace App\Messenger;
 
 use App\Service\ExceptionLoggerService;
+use Exception;
 use Throwable;
 
-abstract class LoggedHandler
-{
+abstract class LoggedHandler {
+
     public function __construct(private ExceptionLoggerService $loggerService) {}
 
     protected function handle(MessageInterface $message): void {
@@ -14,7 +15,10 @@ abstract class LoggedHandler
             $this->process($message);
         }
         catch(Throwable $exception) {
-            $this->loggerService->sendLog($exception);
+            // add message in sent exception
+            $serializedMessage = serialize($message);
+            $sentException = new Exception($serializedMessage, 0, $exception);
+            $this->loggerService->sendLog($sentException);
         }
     }
     abstract protected function process(MessageInterface $message): void;
