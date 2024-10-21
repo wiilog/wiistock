f24a6dba547f96b3c28b506c563b80da00038f51
romain <romain.barot@wiilog.fr>
2024-10-21 15:17:24 +0200

    WIIS-12068

--- migrations/Version20241007121103.php
+++ migrations/Version20241007121103.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
+use Doctrine\DBAL\Logging\Middleware;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Psr\Log\NullLogger;
 use WiiCommon\Helper\Stream;
 
 /**
@@ -24,34 +26,42 @@ final class Version20241007121103 extends AbstractMigration
             $this->addSql("ALTER TABLE dispatch_reference_article ADD COLUMN associated_document_types JSON DEFAULT NULL");
         }
 
-        $associatedDocumentTypesAndReference = $this->connection->iterateAssociative("SELECT JSON_VALUE(reference_article.description, '$.associatedDocumentTypes') AS reference_associated_document_types,
-                    reference_article.reference as reference_reference,
-                    reference_article.id as reference_id
-            FROM reference_article
-            WHERE JSON_LENGTH(reference_article.description) > 0
-            AND JSON_VALUE(reference_article.description, '$.associatedDocumentTypes') NOT IN ('[]', '')");
+        self::migrate($this, 5000);
 
-       foreach ($associatedDocumentTypesAndReference as $associatedDocumentTypeAndReference) {
-           $referenceId = $associatedDocumentTypeAndReference['reference_id'];
-           $associatedDocumentTypes = Stream::explode(',', $associatedDocumentTypeAndReference['reference_associated_document_types'])
-               ->filter()
-               ->toArray();
 
-           if(!empty($associatedDocumentTypes)){
-               $this->addSql("UPDATE dispatch_reference_article SET associated_document_types = :associatedDocumentTypes WHERE dispatch_reference_article.reference_article_id = :referenceId", [
-                   "associatedDocumentTypes" => json_encode($associatedDocumentTypes),
-                   "referenceId" => $referenceId,
-               ]);
-           }
-       }
-
-       $this->addSql("UPDATE reference_article
-            SET description = JSON_REMOVE(reference_article.description, '$.associatedDocumentTypes')
-            WHERE JSON_LENGTH(reference_article.description) > 0");
     }
 
     public function down(Schema $schema): void
     {
 
+    }
+
+    public static function migrate(AbstractMigration $migration, int $limit = null): void
+    {
+        $migration->connection->getConfiguration()->setMiddlewares([new Middleware(new NullLogger())]);
+
+        $queryLimit = $limit ? "LIMIT $limit" : "";
+        $associatedDocumentTypesAndReference = $migration->connection->iterateAssociative("
+            SELECT JSON_VALUE(reference_article.description, '$.associatedDocumentTypes') AS reference_associated_document_types,
+                    reference_article.reference as reference_reference,
+                    reference_article.id as reference_id
+            FROM reference_article
+            WHERE JSON_LENGTH(reference_article.description) > 0
+            AND JSON_VALUE(reference_article.description, '$.associatedDocumentTypes') NOT IN ('[]', '')
+            {$queryLimit}");
+
+        foreach ($associatedDocumentTypesAndReference as $associatedDocumentTypeAndReference) {
+            $referenceId = $associatedDocumentTypeAndReference['reference_id'];
+            $associatedDocumentTypes = Stream::explode(',', $associatedDocumentTypeAndReference['reference_associated_document_types'])
+                ->filter()
+                ->toArray();
+
+            if(!empty($associatedDocumentTypes)){
+                $migration->addSql("UPDATE dispatch_reference_article SET associated_document_types = :associatedDocumentTypes WHERE dispatch_reference_article.reference_article_id = :referenceId", [
+                    "associatedDocumentTypes" => json_encode($associatedDocumentTypes),
+                    "referenceId" => $referenceId,
+                ]);
+            }
+        }
     }
 }
