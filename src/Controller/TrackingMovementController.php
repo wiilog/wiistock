3219ef93811241b702b11acb125e1fa9fca4d4b4
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-11-06 14:31:22 +0100

    WIIS-12096

--- src/Controller/TrackingMovementController.php
+++ src/Controller/TrackingMovementController.php
@@ -389,7 +389,6 @@ class TrackingMovementController extends AbstractController
         $linkedMouvements = $trackingMovementRepository->findBy(['mainMovement' => $trackingMovement]);
 
         if ($userService->hasRightFunction(Menu::TRACA, Action::FULLY_EDIT_TRACKING_MOVEMENTS) && $hasChanged) {
-            $entityManager->remove($trackingMovement);
             $response = $trackingMovementService->persistTrackingMovement(
                 $entityManager,
                 $post->get('pack'),
@@ -414,6 +413,7 @@ class TrackingMovementController extends AbstractController
                 $entityManager->persist($new);
                 $entityManager->flush();
 
+                $entityManager->remove($trackingMovement);
                 $trackingMovement = $new;
             } else {
                 return $this->json($response);
