b97f0f5d5d68572333136ca6d7f6200d116a8907
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-11-06 16:56:46 +0100

    WIIS-12073

--- src/Entity/Emplacement.php
+++ src/Entity/Emplacement.php
@@ -26,7 +26,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\OneToMany;
 
 #[ORM\Entity(repositoryClass: EmplacementRepository::class)]
-#[ORM\Index(fields: ["label"], name: "label_idx")]
+#[ORM\Index(fields: ["label"], name: "IDX_WIILOG_LABEL")]
 class Emplacement implements PairedEntity {
 
     use SensorMessageTrait;
