074cf1786484ac7b87e73c8458cb45f195ca7284
Adrien Bounader <adrien.bounader@wiilog.fr>
2024-12-02 12:17:03 +0100

    WIIS-12134

--- src/Entity/Tracking/PackSplit.php
+++ src/Entity/Tracking/PackSplit.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\Tracking;
 
-use App\Repository\PackSplitRepository;
+use App\Repository\Tracking\PackSplitRepository;
 use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
