3361f03fddeb5a9d29b9472f7367559f6c06dc1d
romain <romain.barot@wiilog.fr>
2024-12-17 16:31:33 +0100

    WIIS-12275

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,7 @@ Encore
     .addEntry('arrival-index', './assets/js/pages/arrival/index.js')
     .addEntry('dispatch-index', '/assets/js/pages/dispatch/index.js')
     .addEntry('dispatch-show', '/assets/js/pages/dispatch/show.js')
+    .addEntry('dispute', '/assets/js/pages/litiges.js')
     .addStyleEntry('pack-common', './assets/scss/utils/pack.scss')
     .autoProvidejQuery()
 
