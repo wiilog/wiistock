a4043983db0f1d19ecaaee2bc68b8222afdc5045
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-01-29 10:03:10 +0100

    WIIS-12269: rename warning-header -> user-message

---
+++ src/Command/UserMessage/ClearUserMessageCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Command\UserMessage;
+
+use App\Entity\Setting;
+use App\Service\CacheService;
+use App\Service\SettingsService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:user-message:clear',
+    description: 'This command clears the user message. It can be used to hide the user message.'
+)]
+class ClearUserMessageCommand extends Command {
+
+    public function __construct(private EntityManagerInterface $entityManager,
+                                private SettingsService        $settingsService,
+                                private CacheService           $cacheService) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $settings = [Setting::USER_MESSAGE_CONFIG];
+
+        $settingMessage = $this->settingsService->persistSetting($this->entityManager, $settings, Setting::USER_MESSAGE_CONFIG);
+        $settingMessage->setValue(null);
+
+        $this->entityManager->flush();
+
+        $this->cacheService->delete(CacheService::COLLECTION_SETTINGS, Setting::USER_MESSAGE_CONFIG);
+
+        return Command::SUCCESS;
+    }
+}
