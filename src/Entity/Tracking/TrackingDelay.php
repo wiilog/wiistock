84c150d8236f24b72298d41079676cb2e3d168d6
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-02-10 09:58:01 +0100

    WIIS-11930: multiple tracking delay calculation by pack

--- src/Entity/Tracking/TrackingDelay.php
+++ src/Entity/Tracking/TrackingDelay.php
@@ -4,6 +4,8 @@ namespace App\Entity\Tracking;
 
 use App\Repository\Tracking\TrackingDelayRepository;
 use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -27,11 +29,25 @@ class TrackingDelay {
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: false)]
     private ?DateTime $calculatedAt = null;
 
-    #[ORM\OneToOne(inversedBy: "trackingDelay", targetEntity: Pack::class)]
-    private ?Pack $pack = null;
-
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?DateTime $limitTreatmentDate = null;
+
+    #[ORM\Column(type: Types::BOOLEAN, nullable: false, options: ['default' => false])]
+    private ?bool $stoppedByMovementEdit = false;
+
+    #[ORM\ManyToOne(targetEntity: Pack::class)]
+    #[ORM\JoinColumn(nullable: false, onDelete: "CASCADE")]
+    private ?Pack $pack = null;
+
+    /**
+     * @var Collection<int, TrackingDelayRecord>
+     */
+    #[ORM\OneToMany(mappedBy: "trackingDelay", targetEntity: TrackingDelayRecord::class, cascade: ['persist', 'remove'], orphanRemoval: true)]
+    private Collection $records;
+
+    public function __construct() {
+        $this->records = new ArrayCollection();
+    }
 
     public function getId(): ?int {
         return $this->id;
@@ -60,15 +76,7 @@ class TrackingDelay {
     }
 
     public function setPack(?Pack $pack): self {
-        if($this->pack && $this->pack->getTrackingDelay() !== $this) {
-            $oldPack = $this->pack;
-            $this->pack = null;
-            $oldPack->setTrackingDelay(null);
-        }
         $this->pack = $pack;
-        if($this->pack && $this->pack->getTrackingDelay() !== $this) {
-            $this->pack->setTrackingDelay($this);
-        }
 
         return $this;
     }
@@ -88,14 +96,60 @@ class TrackingDelay {
         return $this;
     }
 
-    public function getLimitTreatmentDate(): ?DateTime
-    {
+    public function getLimitTreatmentDate(): ?DateTime {
         return $this->limitTreatmentDate;
     }
 
-    public function setLimitTreatmentDate(?DateTime $limitTreatmentDate): self
-    {
+    public function setLimitTreatmentDate(?DateTime $limitTreatmentDate): self {
         $this->limitTreatmentDate = $limitTreatmentDate;
+
+        return $this;
+    }
+
+    public function isStoppedByMovementEdit(): ?bool {
+        return $this->stoppedByMovementEdit;
+    }
+
+    public function setStoppedByMovementEdit(?bool $stoppedByMovementEdit): self {
+        $this->stoppedByMovementEdit = $stoppedByMovementEdit;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, TrackingDelayRecord>
+     */
+    public function getRecords(): Collection {
+        return $this->records;
+    }
+
+    public function addRecord(TrackingDelayRecord $record): self {
+        if (!$this->records->contains($record)) {
+            $this->records[] = $record;
+            $record->setTrackingDelay($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecord(TrackingDelayRecord $record): self {
+        if ($this->records->removeElement($record)) {
+            if ($record->getTrackingDelay() === $this) {
+                $record->setTrackingDelay(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function setRecords(?iterable $records): self {
+        foreach($this->getRecords()->toArray() as $record) {
+            $this->removeRecord($record);
+        }
+
+        $this->records = new ArrayCollection();
+        foreach($records ?? [] as $record) {
+            $this->addRecord($record);
+        }
 
         return $this;
     }
