81960e29835da657c5a497d44ae0651a74a51582
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-02-10 09:58:01 +0100

    WIIS-11930: multiple tracking delay calculation by pack

--- src/Service/PackService.php
+++ src/Service/PackService.php
@@ -831,10 +831,12 @@ class PackService {
                 ->sort(static fn(array $data1, array $data2) => ($data1["remainingTime"] <=> $data2["remainingTime"]));
 
             $firstPackChild = $packChildSortedByDelay->first()["pack"] ?? null;
+
+            /** @var Pack $pack */
             $pack = $firstPackChild ?: $pack;
         }
 
-        $packTrackingDelay = $pack->getTrackingDelay();
+        $packTrackingDelay = $pack->getCurrentTrackingDelay();
 
         $remainingTime = $this->getTrackingDelayRemainingTime($pack);
 
@@ -866,7 +868,7 @@ class PackService {
     }
 
     public function getTrackingDelayRemainingTime(Pack $pack): ?int {
-        $packTrackingDelay = $pack->getTrackingDelay();
+        $packTrackingDelay = $pack->getCurrentTrackingDelay();
         $nature = $pack->getNature();
         $natureTrackingDelay = $nature?->getTrackingDelay();
 
