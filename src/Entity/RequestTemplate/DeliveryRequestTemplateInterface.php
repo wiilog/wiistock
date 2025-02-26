a897331904bd957c2de65215ae17ea65b47e83b9
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-02-26 14:15:32 +0100

    WIIS-12393

--- src/Entity/RequestTemplate/DeliveryRequestTemplateInterface.php
+++ src/Entity/RequestTemplate/DeliveryRequestTemplateInterface.php
@@ -5,9 +5,8 @@ namespace App\Entity\RequestTemplate;
 interface DeliveryRequestTemplateInterface {
     public function getUsage(): DeliveryRequestTemplateUsageEnum;
 
-    const DELIVERY_REQUEST_TEMPLATE_USAGE = [
+    const DELIVERY_REQUEST_TEMPLATE_USAGES = [
         DeliveryRequestTemplateUsageEnum::TRIGGER_ACTION->value => "Actionneur",
         DeliveryRequestTemplateUsageEnum::SLEEPING_STOCK->value => "Stock dormant",
     ];
-
 }
