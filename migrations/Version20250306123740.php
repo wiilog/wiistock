4e8703625491d3d21c38be21c4c72fb946875c55
Benjamin Loupiac <benjamin.loupiac@wiilog.fr>
2025-03-07 09:45:12 +0100

    WIIS-12388 -- fix

--- migrations/Version20250306123740.php
+++ migrations/Version20250306123740.php
@@ -30,16 +30,16 @@ final class Version20250306123740 extends AbstractMigration
                 'menu_label' => 'Références',
             ]);
 
-            $query = $conn->executeQuery('SELECT id FROM translation_source WHERE category_id = :category_id', [
+            $query = $conn->executeQuery('SELECT id AS sourceId FROM translation_source WHERE category_id = :category_id', [
                 'category_id' => $menuId,
             ])
             ->fetchAllAssociative();
 
-            $translationSourceIds = Stream::from($query)->flatten()->toArray();
+            $translationSourceIds = Stream::from($query)->toArray();
 
             foreach ($translationSourceIds as $translationSourceId) {
-                $this->addSql('DELETE FROM translation where source_id = :source_id', ['source_id' => $translationSourceId]);
-                $this->addSql('DELETE FROM translation_source where id = :source_id', ['source_id' => $translationSourceId]);
+                $this->addSql('DELETE FROM translation where source_id = :source_id', ['source_id' => $translationSourceId["sourceId"]]);
+                $this->addSql('DELETE FROM translation_source where id = :source_id', ['source_id' => $translationSourceId["sourceId"]]);
             }
         }
     }
