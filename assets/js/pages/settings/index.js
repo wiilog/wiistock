39aa5b5b30597f57c518e2f518607b6787a58191
romain <romain.barot@wiilog.fr>
2025-03-11 13:55:54 +0100

    WIIS-12515

--- assets/js/pages/settings/index.js
+++ assets/js/pages/settings/index.js
@@ -151,9 +151,7 @@ $(function() {
 
     $(document).on(`change`, `.wii-box.settings-content:not(.d-none) *`, function() {
         editing = true;
-        //alert(editing) //problème ici editing n'est pas toujours mis a vrai lors d'une modification
-    })
-
+    });
 
     document.body.addEventListener(`click`, function(event) {
         const $target = $(event.target);
