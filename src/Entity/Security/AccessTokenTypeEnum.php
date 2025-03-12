5dd9e6ff7061419897970cc2a9f334c8b2ee6b9e
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-03-12 11:33:10 +0100

    WIIS-12386

---
+++ src/Entity/Security/AccessTokenTypeEnum.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Entity\Security;
+
+enum AccessTokenTypeEnum: string {
+    case SLEEPING_STOCK = 'sleeping_stock';
+}
---
+++ src/Repository/Security/TokenRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repository\Security;
+
+use App\Entity\Security\Token;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @extends EntityRepository<Token>
+ */
+class TokenRepository extends EntityRepository {
+}
