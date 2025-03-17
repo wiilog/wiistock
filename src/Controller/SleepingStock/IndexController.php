dc894e7c2e72f460432e6331c34a5525a16c6afa
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-03-17 16:42:17 +0100

    WIIS-12395: add new Authenticator

---
+++ src/Controller/SleepingStock/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\SleepingStock;
+
+use App\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route("/sleeping-stock", name: "sleeping_stock")]
+class IndexController extends AbstractController {
+
+    #[Route("/", name: "index")]
+    public function index(): Response {
+        // TODO
+        return $this->json([
+            "success" => true,
+        ]);
+    }
+
+}
