e351810c4d8bed327811209ac95c5608040eaaf6
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-03-17 16:10:25 +0100

    WIIS-12398

--- assets/js/pages/settings/stock/articles/sleeping-stock.js
+++ assets/js/pages/settings/stock/articles/sleeping-stock.js
@@ -1,6 +1,6 @@
 import EditableDatatable, {MODE_EDIT, SAVE_MANUALLY} from "@app/editatable";
-import AJAX, {GET} from "@app/ajax";
-
+import AJAX, {GET, POST} from "@app/ajax";
+import Flash, {ERROR, INFO, SUCCESS} from "@app/flash";
 
 /**
  * @param {jQuery} $container
@@ -8,6 +8,22 @@ import AJAX, {GET} from "@app/ajax";
 export function initializeSleepingStockSettingPage($container){
     initializeSleepingStockRequestInformations($container)
     initializeSleepingSleepingStockPlan($container)
+
+    const sleepingStockForceSendAlertbuttonClickEvent = 'click.sleepingStockForceSendAlertbuttonClickEvent';
+    $container
+        .find('.sleeping-stock-force-send-alert-button')
+        .off(sleepingStockForceSendAlertbuttonClickEvent)
+        .on(sleepingStockForceSendAlertbuttonClickEvent, () => {
+            AJAX
+                .route(
+                    POST,
+                    'settings_sleeping_stock_plan_force_trigger',
+                )
+                .json()
+                .then(({success, message}) => {
+                    Flash.add(succes ? SUCCESS : ERROR, message)
+                });
+        })
 }
 
 /**
