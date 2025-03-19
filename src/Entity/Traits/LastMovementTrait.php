1bfa5677e88c8f44aa69b54eb3f8e0257188e6fc
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-03-19 16:44:03 +0100

    WIIS-12546

---
+++ src/Entity/Traits/LastMovementTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\MouvementStock;
+
+trait LastMovementTrait {
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?MouvementStock $lastMovement = null;
+
+
+    public function getLastMovement(): ?MouvementStock
+    {
+        return $this->lastMovement;
+    }
+
+    public function setLastMovement(?MouvementStock $lastMovement): static
+    {
+        $this->lastMovement = $lastMovement;
+
+        return $this;
+    }
+}
