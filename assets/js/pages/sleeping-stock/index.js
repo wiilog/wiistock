4000f16e5314025d28e62b7a8aede16fa9e82ae1
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-03-19 11:51:08 +0100

    WIIS-12395

--- assets/js/pages/sleeping-stock/index.js
+++ assets/js/pages/sleeping-stock/index.js
@@ -1,5 +1,7 @@
+import {initDataTable} from "@app/datatable";
+
 $(function() {
-    initDataTable('tableSleepingStockForm', {
+    const table = initDataTable('tableSleepingStockForm', {
         serverSide: false,
         processing: false,
         searching: false,
@@ -12,5 +14,9 @@ $(function() {
             {data: 'quantityStock', name: 'quantityStock', title: Translation.of('Stock', 'Références', 'Général', 'Quantité')},
             {data: 'maxStorageDate', name: 'maxStorageDate', title: Translation.of('Stock', 'Références', 'Email stock dormant', 'Date max de stockage')},
         ],
+    });
+
+    $(document).on('click', '.delete-row', function (event) {
+        table.row($(event.target).closest('tr')).remove().draw();
     });
 });
