e18f643ea2572054797ce3ac731a390cdad399f4
Benjamin Loupiac <benjamin.loupiac@wiilog.fr>
2025-04-10 16:23:19 +0200

    WIIS-12622 -- fix with good category

--- src/Entity/CategoryType.php
+++ src/Entity/CategoryType.php
@@ -50,6 +50,7 @@ class CategoryType {
         self::COLLECT_TRANSPORT,
         self::SHIPPING_REQUEST,
         self::PRODUCTION,
+        self::STOCK_EMERGENCY,
         self::TRACKING_EMERGENCY,
     ];
 
