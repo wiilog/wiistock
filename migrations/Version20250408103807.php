7b62da701effc785789284241b93f3bdd7ffbe40
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-04-14 11:28:32 +0200

    WIIS-12620

--- migrations/Version20250408103807.php
+++ migrations/Version20250408103807.php
@@ -20,37 +20,43 @@ final class Version20250408103807 extends AbstractMigration {
     }
 
     public function up(Schema $schema): void {
-        $emergencyTypeValues = $this->connection->executeQuery('SELECT elements FROM fixed_field_standard WHERE entity_code = :entityCode AND field_code = :fieldCode', [
-            'entityCode' => FixedFieldStandard::ENTITY_CODE_EMERGENCY,
-            'fieldCode' => FixedFieldStandard::FIELD_CODE_EMERGENCY_TYPE,
-        ])->fetchAllAssociative();
+        $emergencyTypeValues = $this->connection
+            ->executeQuery('
+                SELECT elements
+                FROM fixed_field_standard
+                WHERE entity_code = :entityCode AND field_code = :fieldCode
+            ', [
+                'entityCode' => FixedFieldStandard::ENTITY_CODE_EMERGENCY,
+                'fieldCode' => FixedFieldStandard::FIELD_CODE_EMERGENCY_TYPE,
+            ])
+            ->fetchAllAssociative();
 
-        if(!empty($emergencyTypeValues) && isset($emergencyTypeValues[0]['elements'])){
-            $this->addSql("INSERT INTO category_type (label) VALUES (:categoryTypeLabel)", [
-                'categoryTypeLabel' => CategoryType::TRACKING_EMERGENCY
-            ]);
-            $emergencyTypeValuesDecoded = json_decode($emergencyTypeValues[0]['elements'], true);
+        $this->addSql("INSERT INTO category_type (label) VALUES (:categoryTypeLabel)", [
+            'categoryTypeLabel' => CategoryType::TRACKING_EMERGENCY
+        ]);
 
-            foreach ($emergencyTypeValuesDecoded ?? [] as $emergencyTypeValue) {
-                $this->addSql("
-                    INSERT INTO type (category_id, color, label)
-                    VALUES (
-                        (
-                            SELECT category_type.id
-                            FROM category_type
-                            WHERE category_type.label = :categoryTypeLabel
-                            LIMIT 1
-                        ),
-                        :color,
-                        :emergencyLabel
-                    )",
-                    [
-                        'categoryTypeLabel' => CategoryType::TRACKING_EMERGENCY,
-                        'color' => Type::DEFAULT_COLOR,
-                        'emergencyLabel' => $emergencyTypeValue,
-                    ]
-                );
-            }
+        $emergencyTypeValuesDecoded = @json_decode($emergencyTypeValues[0]['elements'], true)
+            ?: [Type::LABEL_STANDARD];
+
+        foreach ($emergencyTypeValuesDecoded as $emergencyTypeValue) {
+            $this->addSql("
+                INSERT INTO type (category_id, color, label)
+                VALUES (
+                    (
+                        SELECT category_type.id
+                        FROM category_type
+                        WHERE category_type.label = :categoryTypeLabel
+                        LIMIT 1
+                    ),
+                    :color,
+                    :emergencyLabel
+                )",
+                [
+                    'categoryTypeLabel' => CategoryType::TRACKING_EMERGENCY,
+                    'color' => Type::DEFAULT_COLOR,
+                    'emergencyLabel' => $emergencyTypeValue,
+                ]
+            );
         }
     }
 
