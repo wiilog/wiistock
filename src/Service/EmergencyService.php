f5a9b21c6d112a36590e35822c57d780c09772d7
GrégoireDolidon <gregoire.dolidon@wiilog.fr>
2025-05-06 16:28:31 +0200

    WIIS-12634

--- src/Service/EmergencyService.php
+++ src/Service/EmergencyService.php
@@ -465,7 +465,7 @@ class EmergencyService {
                          ], [
                              "title" => "Aller vers les arrivages",
                              "hasRight" => $data["emergency_category_label"] === CategoryType::TRACKING_EMERGENCY && $this->userService->hasRightFunction(Menu::QUALI, Action::DISPLAY_EMERGENCY),
-                             "icon" => "fa fa-eye",
+                             "icon" => "fa fa-up-right-from-square",
                              "href" => $this->router->generate('arrivage_index', ["emergency" => $data["id"]]),
                              "attributes" => [
                                  "data-id" => $data["id"],
