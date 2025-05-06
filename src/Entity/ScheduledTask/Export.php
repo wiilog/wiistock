5d5bb60ba84e11269bddfbb22ed17fad3eb954ed
Nassim Boudmea <nassim.boudmea@wiilog.fr>
2025-05-06 16:13:56 +0200

    WIIS-12757

--- src/Entity/ScheduledTask/Export.php
+++ src/Entity/ScheduledTask/Export.php
@@ -32,6 +32,7 @@ class Export extends ScheduledTask {
     const ENTITY_TRUCK_ARRIVAL = "truck_arrival";
     const ENTITY_RECEIPT_ASSOCIATION = "receipt_association";
     const ENTITY_DISPUTE = "dispute";
+    const ENTITY_LOCATION = "location";
 
     const ENTITY_LABELS = [
         self::ENTITY_REFERENCE => "Références",
@@ -46,6 +47,7 @@ class Export extends ScheduledTask {
         self::ENTITY_TRUCK_ARRIVAL => "Arrivages Camion",
         self::ENTITY_RECEIPT_ASSOCIATION => "Associations BR",
         self::ENTITY_DISPUTE => "Litiges",
+        self::ENTITY_LOCATION => "Emplacements",
     ];
 
     const DESTINATION_EMAIL = 1;
