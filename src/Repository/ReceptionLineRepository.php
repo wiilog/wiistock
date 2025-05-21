c6dfcaa25bec881f80540d31b88d441aba343e39
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-05-21 15:17:46 +0200

    WIIS-12804

--- src/Repository/ReceptionLineRepository.php
+++ src/Repository/ReceptionLineRepository.php
@@ -28,34 +28,13 @@ class ReceptionLineRepository extends EntityRepository {
         $exprBuilder = $queryBuilder->expr();
 
         $queryBuilder
-            ->select('line.id AS id')
-            ->addSelect('join_pack.id AS packId')
-            ->addSelect('join_pack.code AS packCode')
-            ->addSelect('join_locationLastOngoingDrop.label AS packLocation')
-            ->addSelect('join_project.code AS packProject')
-            ->addSelect('join_nature.label AS packNature')
-            ->addSelect('join_nature.color AS packColor')
-            ->addSelect('join_receptionReferenceArticle.id AS referenceId')
-            ->addSelect('join_referenceArticle.reference AS reference')
-            ->addSelect('join_referenceArticle.libelle AS label')
-            ->addSelect('join_referenceArticle.typeQuantite AS quantityType')
-            ->addSelect('join_referenceArticle.barCode AS barCode')
-            ->addSelect('join_receptionReferenceArticle.commande AS orderNumber')
-            ->addSelect('join_receptionReferenceArticle.quantiteAR AS quantityToReceive')
-            ->addSelect('join_receptionReferenceArticle.quantite AS receivedQuantity')
-            ->addSelect('join_receptionReferenceArticle.unitPrice AS unitPrice')
-            ->addSelect('IF(join_receptionReferenceArticle.stockEmergencies IS NOT EMPTY, 1, 0) AS emergency')
-            ->addSelect("GROUP_CONCAT(DISTINCT stock_emergency.comment SEPARATOR ', ') AS emergencyComment")
             ->leftJoin('line.pack', 'join_pack')
-            ->leftJoin('join_pack.nature', 'join_nature')
             ->leftJoin('join_pack.lastOngoingDrop', 'join_lastOngoingDrop')
             ->leftJoin('join_lastOngoingDrop.emplacement', 'join_locationLastOngoingDrop')
             ->leftJoin('join_pack.project', 'join_project')
             ->leftJoin('line.receptionReferenceArticles', 'join_receptionReferenceArticle')
             ->leftJoin('join_receptionReferenceArticle.referenceArticle', 'join_referenceArticle')
-            ->leftJoin('join_referenceArticle.articlesFournisseur', 'join_supplierArticles')
-            ->leftJoin('join_supplierArticles.fournisseur', 'join_supplier')
-            ->leftJoin(StockEmergency::class, "stock_emergency", Join::WITH, "stock_emergency.referenceArticle = join_referenceArticle OR stock_emergency.supplier = join_supplier")
+            ->leftJoin('join_receptionReferenceArticle.stockEmergencies', 'stock_emergency')
             ->andWhere('line.reception = :reception')
             ->addOrderBy('IF(join_pack.id IS NULL, 0, 1)') // show receptionLine without pack first
             ->addOrderBy('line.id')
@@ -80,55 +59,8 @@ class ReceptionLineRepository extends EntityRepository {
                 ->setFirstResult($start)
                 ->setMaxResults($length);
         }
-        $queryBuilder = QueryBuilderHelper::setGroupBy($queryBuilder, ["emergencyComment"]);
 
-        $queryResult = $queryBuilder->getQuery()->getResult();
-
-        $result = Stream::from($queryResult)
-            ->keymap(fn(array $row) => [$row["id"], $row], true)
-            ->map(function(array $references, $key) {
-                if (!empty($references)) {
-                    $packId = $references[0]["packId"] ?? null;
-                    $packCode = $references[0]["packCode"] ?? null;
-                    $packLocation = $references[0]["packLocation"] ?? null;
-                    $packProject = $references[0]["packProject"] ?? null;
-                    $packNature = $references[0]["packNature"] ?? null;
-                    $packColor = $references[0]["packColor"] ?? null;
-                }
-
-                return [
-                    "id" => $key,
-                    "pack" => isset($packId)
-                        ? [
-                            "id" => $packId,
-                            "code" => $packCode ?? null,
-                            "location" => $packLocation ?? null,
-                            "project" => $packProject ?? null,
-                            "nature" => $packNature ?? null,
-                            "color" => $packColor ?? null,
-                        ]
-                        : null,
-                    "references" => Stream::from($references)
-                        ->filterMap(fn(array $reference) => (
-                            isset($reference["reference"])
-                                ? [
-                                    "id" => $reference["referenceId"],
-                                    "reference" => $reference["reference"],
-                                    "orderNumber" => $reference["orderNumber"],
-                                    "quantityToReceive" => $reference["quantityToReceive"],
-                                    "receivedQuantity" => $reference["receivedQuantity"],
-                                    "unitPrice" => $reference["unitPrice"],
-                                    "quantityType" => $reference["quantityType"],
-                                    "barCode" => $reference["barCode"],
-                                    "label" => $reference["label"],
-                                    "emergency" => boolval($reference["emergency"]),
-                                    "emergencyComment" => $reference["emergencyComment"],
-                                ]
-                                : null
-                        ))
-                        ->toArray()
-                ];
-            });
+        $result = Stream::from($queryBuilder->getQuery()->getResult());
 
         if ($paginationMode === "units") {
             $total = $result->count();
