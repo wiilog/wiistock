61930c0c72b0ad2825a4b0d619c0c2f2093bba52
Adrien Bounader <adrien.bounader@wiilog.fr>
2025-05-28 16:21:21 +0200

    WIIS-12609

--- cypress/e2e/00_cypressSetup/00_global.cy.js
+++ cypress/e2e/00_cypressSetup/00_global.cy.js
@@ -2,9 +2,12 @@
 describe('Test cypress setup', () => {
 
     it('Test if owasp container is enabled', () => {
-        //cy.visit('/.env', {failOnStatusCode: false}).contains("403");
-
-        cy.request('/.env', {failOnStatusCode: false})
+        cy
+            .request({
+                url: '/.env',
+                followRedirect: false,
+                failOnStatusCode: false,
+            })
             .then((resp) => {
                 expect(resp.status).to.eq(403);
             });
