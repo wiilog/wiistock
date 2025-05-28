4762fb8f063a8cf64bc81ea95ac1245dbc8e3b60
Benjamin Loupiac <benjamin.loupiac@wiilog.fr>
2025-05-28 14:55:04 +0200

    WIIS-12609

--- cypress/e2e/00_cypressSetup/00_global.cy.js
+++ cypress/e2e/00_cypressSetup/00_global.cy.js
@@ -2,6 +2,8 @@
 describe('Test cypress setup', () => {
 
     it('Test if owasp container is enabled', () => {
-        cy.visit('/.env', {failOnStatusCode: false}).contains("403");
+        //cy.visit('/.env', {failOnStatusCode: false}).contains("403");
+        cy.visit('/.env', {failOnStatusCode: false}).as('env')
+        cy.wait('@env').its('response.statusCode').should('eq', 403)
     });
 });
